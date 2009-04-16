17d3d28b91c5f83ecaedef6e66c560cf66460fc2
tequillaymota <tequillaymota@5781a423-4729-0410-aacd-f88070b95415>
2009-04-16 14:32:30 +0000

    Version 6.0.0.5

---
+++ webEdition/we/include/we_language/Finnish_UTF-8/sysinfo.inc.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * webEdition CMS
+ *
+ * This source is part of webEdition CMS. webEdition CMS is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the textfile
+ * webEdition/licenses/webEditionCMS/License.txt
+ *
+ * @category   webEdition
+ * @package    webEdition_language
+ * @copyright  Copyright (c) 2008 living-e AG (http://www.living-e.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL
+ */
+
+/**
+ * Language file: sysinfo.inc.php
+ * Provides language strings for system info.
+ * Language: Deutsch
+ */
+
+$_sysinfo['we_version'] = 'webEdition versio';
+$_sysinfo['server_name'] = 'Server name';
+$_sysinfo['port'] ='Portti' ;
+$_sysinfo['protocol'] = 'Protokolla';
+$_sysinfo['installation_folder'] = 'Asennushakemisto';
+$_sysinfo['we_max_upload_size'] = 'Maksimi lähetettävän tiedoston koko';
+$_sysinfo['php_version'] = 'PHP versio';
+$_sysinfo['mysql_version'] = 'MySql versio';
+$_sysinfo['more_info'] = 'lisätietoja';
+$_sysinfo['back'] = 'paluu';
+$_sysinfo['sysinfo'] = 'Järjestelmätiedot';
+
+$_sysinfo['connection_types'] = 'Päivitä yhteystyypit';
+$_sysinfo['gdlib'] = 'GDlib tuki';
+$_sysinfo['mbstring'] = 'Multibyte String funktiot';
+$_sysinfo['version'] = 'Versio';
+$_sysinfo['available'] = 'saatavilla';
+
+?>
