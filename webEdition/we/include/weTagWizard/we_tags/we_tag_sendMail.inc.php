180e5f9ebf7149f66e66640c49e2700d947d8c60
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2009-06-01 11:27:19 +0000

    Feature Wunsch #2209 und #3703

--- webEdition/we/include/weTagWizard/we_tags/we_tag_sendMail.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_sendMail.inc.php
@@ -11,4 +11,6 @@ $GLOBALS['weTagWizard']['attribute']['id455_from'] = new weTagData_textAttribute
 $GLOBALS['weTagWizard']['attribute']['id456_reply'] = new weTagData_textAttribute('456', 'reply', false, '');
 $GLOBALS['weTagWizard']['attribute']['id457_mimetype'] = new weTagData_selectAttribute('457', 'mimetype', array(new weTagDataOption('text/plain', false, ''), new weTagDataOption('text/html', false, '')), false, '');
 $GLOBALS['weTagWizard']['attribute']['id458_charset'] = new weTagData_textAttribute('458', 'charset', false, '');
+$GLOBALS['weTagWizard']['attribute']['id814_includeimages'] = new weTagData_selectAttribute('814', 'includeimages', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
+
 ?>
