739422cd99420e0cbf59d35a4383a195a4fa4ad1
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2009-06-02 13:00:16 +0000

    Bugs #3631 und #3630 bzw. #3394

--- trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_ifWorkspace.inc.php
+++ trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_ifWorkspace.inc.php
@@ -1,7 +1,7 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/ weTagWizard/classes/weTagData_textAttribute.class.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/ weTagWizard/classes/weTagData_selectorAttribute.class.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/ weTagWizard/classes/weTagData_selectAttribute.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_textAttribute.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_selectorAttribute.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_selectAttribute.class.php');
 
 $GLOBALS['weTagWizard']['weTagData']['needsEndtag'] = true;
 
