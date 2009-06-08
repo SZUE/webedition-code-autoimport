c01ee6f0e7c0dabe6c453c02def5ca4c8d68ec50
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2009-06-08 04:48:01 +0000

    Feature #3164

--- webEdition/we/include/weTagWizard/we_tags/we_tag_flashmovie.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_flashmovie.inc.php
@@ -1,6 +1,8 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_textAttribute.class.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_selectAttribute.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_selectorAttribute.class.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_sqlRowAttribute.class.php');
 
 $GLOBALS['weTagWizard']['weTagData']['needsEndtag'] = false;
 
@@ -8,6 +10,8 @@ $GLOBALS['weTagWizard']['attribute']['id145_name'] = new weTagData_textAttribute
 $GLOBALS['weTagWizard']['attribute']['id146_width'] = new weTagData_textAttribute('146', 'width', false, '');
 $GLOBALS['weTagWizard']['attribute']['id147_height'] = new weTagData_textAttribute('147', 'height', false, '');
 $GLOBALS['weTagWizard']['attribute']['id148_alt'] = new weTagData_textAttribute('148', 'alt', false, '');
+if(defined("FILE_TABLE")) { $GLOBALS['weTagWizard']['attribute']['id820_startid'] = new weTagData_selectorAttribute('820', 'startid',FILE_TABLE, 'folder', false, ''); }
+if(defined("FILE_TABLE")) { $GLOBALS['weTagWizard']['attribute']['id821_parentid'] = new weTagData_selectorAttribute('821', 'parentid',FILE_TABLE, 'folder', false, ''); }
 $GLOBALS['weTagWizard']['attribute']['id633_showcontrol'] = new weTagData_textAttribute('633', 'showcontrol', false, '');
 $GLOBALS['weTagWizard']['attribute']['id150_showflash'] = new weTagData_selectAttribute('150', 'showflash', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
 $GLOBALS['weTagWizard']['attribute']['id628_xml'] = new weTagData_selectAttribute('628', 'xml', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
--- webEdition/we/include/we_tag.inc.php
+++ webEdition/we/include/we_tag.inc.php
@@ -2644,7 +2644,8 @@ function we_tag_flashmovie($attribs, $content)
 	$id = $GLOBALS["we_doc"]->getElement($name, "bdid");
 	$id = $id ? $id : we_getTagAttribute("id", $attribs);
 	$fname = 'we_' . $GLOBALS["we_doc"]->Name . '_img[' . $name . '#bdid]';
-	
+	$startid = we_getTagAttribute("startid", $attribs, "");
+	$parentid = we_getTagAttribute("parentid", $attribs, "0");
 	$showcontrol = we_getTagAttribute("showcontrol", $attribs, "true", true, true);
 	$showflash = we_getTagAttribute("showflash", $attribs, "true", true, true);
 	
@@ -2668,7 +2669,7 @@ function we_tag_flashmovie($attribs, $content)
 		// Create "Edit Flash" button
 		$flash_button = $we_button->create_button(
 				"image:btn_edit_flash", 
-				"javascript:we_cmd('openDocselector','" . $id . "', '" . FILE_TABLE . "', 'document.forms[\'we_form\'].elements[\'" . $fname . "\'].value', '', 'opener.setScrollTo();opener.top.we_cmd(\'reload_editpage\');opener._EditorFrame.setEditorIsHot(true);', '" . session_id() . "', 0, 'application/x-shockwave-flash', " . (we_hasPerm(
+				"javascript:we_cmd('openDocselector','" . ($id != "" ? $id : $startid) . "', '" . FILE_TABLE . "', 'document.forms[\'we_form\'].elements[\'" . $fname . "\'].value', '', 'opener.setScrollTo();opener.top.we_cmd(\'reload_editpage\');opener._EditorFrame.setEditorIsHot(true);', '" . session_id() . "'," .$parentid. ", 'application/x-shockwave-flash', " . (we_hasPerm(
 						"CAN_SELECT_OTHER_USERS_FILES") ? 0 : 1) . ")", 
 				true);
 		
