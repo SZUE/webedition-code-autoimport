5eb1d5bd6680578d90cafbc339fbe948ac762f0c
mars-dev <mars-dev@5781a423-4729-0410-aacd-f88070b95415>
2009-07-30 22:06:39 +0000

    bugfix #3527 (added customer, added versions, fixed schedpro)

--- webEdition/we/include/we_defines.inc.php
+++ webEdition/we/include/we_defines.inc.php
@@ -107,13 +107,13 @@ define("FOLDER_ONLY", 1);
 define("WE_UB", "SER_MODULE");
 
 $MNEMONIC_EDITPAGES = array(
-	'0' => 'properties', '1' => 'edit', '2' => 'information', '3' => 'preview', '10' => 'validation'
+	'0' => 'properties', '1' => 'edit', '2' => 'information', '3' => 'preview', '8' => 'schedpro', '10' => 'validation', '17' => 'versions'
 );
-if (isset($_pro_modules) && in_array("schedpro", $_pro_modules)) { //	schedpro only when installed
-	$MNEMONIC_EDITPAGES['8'] = 'schedpro';
-}
 if (isset($_we_active_modules) && in_array('shop', $_we_active_modules)) {
 	$MNEMONIC_EDITPAGES['11'] = 'variants';
+}
+if (isset($_we_active_modules) && in_array('customer', $_we_active_modules)) {
+	$MNEMONIC_EDITPAGES['14'] = 'customer';
 }
 
 $GLOBALS["BIG_USER_MODULE"] = defined("BIG_USER_MODULE") ? BIG_USER_MODULE : "";
