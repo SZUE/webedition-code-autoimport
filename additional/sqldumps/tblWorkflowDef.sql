215f511506685e2151041fc02af8053b05fa9b75
holger_meyer <holger_meyer@5781a423-4729-0410-aacd-f88070b95415>
2009-10-27 16:11:17 +0000

    #3914

--- additional/sqldumps/tblWorkflowDef.sql
+++ additional/sqldumps/tblWorkflowDef.sql
@@ -5,6 +5,7 @@ CREATE TABLE tblWorkflowDef (
   Folders varchar(255) NOT NULL default '',
   DocType bigint(20) NOT NULL default '0',
   Objects varchar(255) NOT NULL default '',
+  ObjectFileFolders varchar(255) NOT NULL default '',
   Categories varchar(255) NOT NULL default '',
   ObjCategories varchar(255) NOT NULL default '',
   `Status` tinyint(1) NOT NULL default '0',
