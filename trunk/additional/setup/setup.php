7daf35e90c4bce9eb83b53f5bb855e2b61a0c157
tequillaymota <tequillaymota@5781a423-4729-0410-aacd-f88070b95415>
2009-11-03 10:14:27 +0000

    bugfix #0003798

--- trunk/additional/setup/setup.php
+++ trunk/additional/setup/setup.php
@@ -30,7 +30,7 @@ if(isset($_REQUEST["phpinfo"])) {
 	phpinfo();
 	exit();
 }
-if(PHP_VERSION < "5.2.4") {
+if(version_compare(PHP_VERSION,"5.2.4","<")) {
 	die('<html><head><title>webEdition setup</title></head><body><div style="font-family:sans-serif, sans; font-size:10pt; border:1px solid red; text-align:center; padding:10px; margin:100px;"><b>PHP Version mismatch</b><br /><br />The PHP Version currently used  on this server is too old to run webEdition.<br />webEdition needs at least PHP Version 5.2.4 or newer, please ask your administrator to update your PHP installation!</div></body></html>');
 }
 
@@ -145,7 +145,7 @@ function step_requirements() {
 	global $errors;
 	$output = "Checking if all system requirements are met. Some additional tests are performed as they are needed for webEdition to be fully functional but are not essential to run webEdition.<br /><br /><b>Basic Requirements:</b><ul style=\"list-style-position:outside;\">";
 	$errors = false;
-	if(PHP_VERSION < "5.2.4") {
+	if(version_compare(PHP_VERSION,"5.2.4","<")) {
 		$output.=tpl_error("PHP Version 5.2.4 or newer required!");
 		$errors = true;
 	} else {
