bcec8d2ec6c7ae4bd7b4f241c1916b7927cb7ead
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2009-11-29 19:37:48 +0000

    Bug #3984

--- trunk/webEdition/we/include/we_language/Russian_UTF-8/modules/object.inc.php
+++ trunk/webEdition/we/include/we_language/Russian_UTF-8/modules/object.inc.php
@@ -19,69 +19,69 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 
-$l_contentTypes["objectFile"] = "пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_contentTypes["object"] = "пїЅпїЅпїЅпїЅпїЅ";
+$l_contentTypes["objectFile"] = "Object";// TRANSLATE
+$l_contentTypes["object"] = "Class";// TRANSLATE
 
-$l_object["attributes"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["objectname"] = "пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["objectpath"] = "пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["class"] = "пїЅпїЅпїЅпїЅпїЅ";
-$l_object["classname"] = "пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["class_id"] = "ID of the class"; // TRANSLATE
-$l_object["default"] = "Default Value";
-$l_object["checked"] = "checked"; // TRANSLATE
-$l_object["name"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ";
-$l_object["allFields"] = "пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ";
-$l_object["search_term"] = "пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["search_field"] = "пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["defaultdir"] = "Default Directory"; // TRANSLATE
-$l_object["rootdir"] = "Workspace"; // TRANSLATE
+$l_object["attributes"] = "Attributes";// TRANSLATE
+$l_object["objectname"] = "Object name";// TRANSLATE
+$l_object["objectpath"] = "Object path";// TRANSLATE
+$l_object["class"] = "Class";// TRANSLATE
+$l_object["classname"] = "Class name";// TRANSLATE
+$l_object["class_id"] = "ID of the class";// TRANSLATE
+$l_object["default"] = "Default Value";// TRANSLATE
+$l_object["checked"] = "checked";// TRANSLATE
+$l_object["name"] = "Automatic name";// TRANSLATE
+$l_object["allFields"] = "All fields";// TRANSLATE
+$l_object["search_term"] = "Search term";// TRANSLATE
+$l_object["search_field"] = "Search field";// TRANSLATE
+$l_object["defaultdir"] = "Default Directory";// TRANSLATE
+$l_object["rootdir"] = "Workspace";// TRANSLATE
 
-$l_object["no_root_ws_select"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ!";
+$l_object["no_root_ws_select"] = "The root directory cannot be choosen!";// TRANSLATE
 
-$l_object["objectFile_field"] = "пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["multiObjectFile_field"] = "Multi object"; // TRANSLATE
-$l_object["checkbox_field"] = "Checkbox"; // TRANSLATE
-$l_object["meta_field"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["input_field"] = "пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["int_field"] = "Integer"; // TRANSLATE
-$l_object["float_field"] = "Float"; // TRANSLATE
-$l_object["date_field"] = "пїЅпїЅпїЅпїЅ";
-$l_object["textarea_field"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["img_field"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["binary_field"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["link_field"] = "пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["href_field"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["shopVat_field"] = "пїЅпїЅпїЅпїЅ пїЅпїЅпїЅ";
+$l_object["objectFile_field"] = "Object";// TRANSLATE
+$l_object["multiObjectFile_field"] = "Multi object";// TRANSLATE
+$l_object["checkbox_field"] = "Checkbox";// TRANSLATE
+$l_object["meta_field"] = "Select";// TRANSLATE
+$l_object["input_field"] = "Text input";// TRANSLATE
+$l_object["int_field"] = "Integer";// TRANSLATE
+$l_object["float_field"] = "Float";// TRANSLATE
+$l_object["date_field"] = "Date";// TRANSLATE
+$l_object["textarea_field"] = "Textarea";// TRANSLATE
+$l_object["img_field"] = "Image";// TRANSLATE
+$l_object["binary_field"] = "Binary document";// TRANSLATE
+$l_object["link_field"] = "Link";// TRANSLATE
+$l_object["href_field"] = "Href";// TRANSLATE
+$l_object["shopVat_field"] = "VAT field";// TRANSLATE
 $l_object["multiobject_recursion"] = "In a Multiobject, an object can not be included in itself";// TRANSLATE
 
-$l_object["we_new_doc_after_save"] = "New Object after saving"; // TRANSLATE
-$l_object["objectFile_response_save_ok"] = "пїЅпїЅпїЅпїЅпїЅпїЅ '%s' пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!";
-$l_object["objectFile_response_publish_ok"] = "пїЅпїЅпїЅпїЅпїЅпїЅ '%s' пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!";
-$l_object["objectFile_response_unpublish_ok"] = "пїЅпїЅпїЅпїЅпїЅпїЅ '%s' пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!";
-$l_object["fieldNameNotValid"] = "Invalid field name\\nValid characters are alpha-numeric, capital and small (a-z, A-Z, 0-9) and underscore."; // TRANSLATE
-$l_object["fieldNameNotTitleDesc"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ!";
-$l_object["fieldNameEmpty"] = "пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ!";
-$l_object["length"] = "пїЅпїЅпїЅпїЅпїЅ";
-$l_object["type"] = "пїЅпїЅпїЅ";
-$l_object["default"] = "пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["max_objects"] = "max. Objects"; // TRANSLATE
-$l_object["no_maximum"] = "leave empty for no limit"; // TRANSLATE
+$l_object["we_new_doc_after_save"] = "New Object after saving";// TRANSLATE
+$l_object["objectFile_response_save_ok"] = "The object '%s' has been successfully saved!";// TRANSLATE
+$l_object["objectFile_response_publish_ok"] = "The object '%s' has been successfully published!";// TRANSLATE
+$l_object["objectFile_response_unpublish_ok"] = "The object '%s' has been successfully unpublished!";// TRANSLATE
+$l_object["fieldNameNotValid"] = "Invalid field name\\nValid characters are alpha-numeric, capital and small (a-z, A-Z, 0-9) and underscore.";// TRANSLATE
+$l_object["fieldNameNotTitleDesc"] = "The fieldnames Title and Description are not allowed!";// TRANSLATE
+$l_object["fieldNameEmpty"] = "The fieldname is empty!";// TRANSLATE
+$l_object["length"] = "Length";// TRANSLATE
+$l_object["type"] = "Type";// TRANSLATE
+$l_object["default"] = "Default";// TRANSLATE
+$l_object["max_objects"] = "max. Objects";// TRANSLATE
+$l_object["no_maximum"] = "leave empty for no limit";// TRANSLATE
 
-$l_object["DefaultOwners"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
+$l_object["DefaultOwners"] = "Standard user";// TRANSLATE
 
-$l_object["copyObject"] = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ";
-$l_object["copyClass"] = "Copy class"; // TRANSLATE
+$l_object["copyObject"] = "Copy object";// TRANSLATE
+$l_object["copyClass"] = "Copy class";// TRANSLATE
 
-$l_object["new_field"] = "пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ";
+$l_object["new_field"] = "NewField";// TRANSLATE
 
-$l_object["behaviour"] = "пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ";
-$l_object["behaviour_all"] = "пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ!";
-$l_object["behaviour_no"] = "пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ!";
+$l_object["behaviour"] = "Display";// TRANSLATE
+$l_object["behaviour_all"] = "If you do not specify a workspace, all objects of this class will always be visible!";// TRANSLATE
+$l_object["behaviour_no"] = " If you do not specify a workspace, no objects of this class will be visible!";// TRANSLATE
 
-$l_object["generated_template_for_objectFile"] = "пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
+$l_object["generated_template_for_objectFile"] = "No matching template found.";// TRANSLATE
 
-$l_object["no_workspace_defined"] = "пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ. пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ.";
+$l_object["no_workspace_defined"] = "There are no workspaces defined for this object. You can select special workspaces for an object in its class.";// TRANSLATE
 
-$l_object["use_thumbnail_preview"] = "Thumbnail preview in objects"; // TRANSLATE
+$l_object["use_thumbnail_preview"] = "Thumbnail preview in objects";// TRANSLATE
 ?>
