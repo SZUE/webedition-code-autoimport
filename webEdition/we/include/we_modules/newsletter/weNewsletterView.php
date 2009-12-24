478ebbf3c21fd1e8821d4452bf821e3c5d69dae2
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2009-12-24 11:35:09 +0000

    Bug #4062

--- webEdition/we/include/we_modules/newsletter/weNewsletterView.php
+++ webEdition/we/include/we_modules/newsletter/weNewsletterView.php
@@ -2483,7 +2483,7 @@ class weNewsletterView {
 		$_clean = $this->getCleanMail($this->newsletter->Reply);
 		
 		$phpmail = new we_util_Mailer($this->newsletter->Test,$this->newsletter->Subject,$this->newsletter->Sender,$this->newsletter->Reply,$this->newsletter->isEmbedImages);
-		if(!$this->newsletter->use_base_href) {$phpmail->setIsUseBaseHref($this->newsletter->use_base_href);}
+		if(!$this->View->settings["use_base_href"]) {$phpmail->setIsUseBaseHref($this->View->settings["use_base_href"]);}
 		$phpmail->setCharSet($this->newsletter->Charset!="" ? $this->newsletter->Charset : $GLOBALS["_language"]["charset"]);
 		$phpmail->addHTMLPart($content);
 		$phpmail->addTextPart(trim($plain));
