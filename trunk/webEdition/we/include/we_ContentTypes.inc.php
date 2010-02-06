00490170f9abc2b126165d19e6c5101852b20ebe
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-02-06 14:26:32 +0000

    Bug #4130

--- trunk/webEdition/we/include/we_ContentTypes.inc.php
+++ trunk/webEdition/we/include/we_ContentTypes.inc.php
@@ -41,7 +41,7 @@ $GLOBALS["WE_CONTENT_TYPES"]["text/html"] = array(
         <head>
                 <title></title>
                 <meta http-equiv="Content-Type" content="text/html; ' . (isset(
-				$GLOBALS["_language"]["charset"]) ? $GLOBALS["_language"]["charset"] : "") . '">
+				$GLOBALS["_language"]["charset"]) ? $GLOBALS["_language"]["charset"] : "UTF-8") . '">
         </head>
         <body>
         </body>
@@ -167,7 +167,8 @@ $GLOBALS["WE_CONTENT_TYPES"]["text/xml"] = array(
 	
 		"Extension" => ".xml", 
 		"Permission" => 'NEW_TEXT', 
-		"DefaultCode" => '<?xml version="1.0" encoding="ISO-8859-1" ?>', 
+		"DefaultCode" => '<?xml version="1.0" encoding="'. (isset(
+				$GLOBALS["_language"]["charset"]) ? $GLOBALS["_language"]["charset"] : "UTF-8") .'" ?>', 
 		"IsRealFile" => "1", 
 		"IsWebEditionFile" => "1", 
 		"Icon" => "link.gif"
