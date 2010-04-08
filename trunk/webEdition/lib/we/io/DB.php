85a7c254d066ff0597293d3889150516772d21e1
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-04-08 03:36:25 +0000

    Bugs #4260 und #4262 Nachtrag

--- trunk/webEdition/lib/we/io/DB.php
+++ trunk/webEdition/lib/we/io/DB.php
@@ -44,7 +44,7 @@ class we_io_DB
 	 */
 	static function newAdapter()
 	{
-		$db = Zend_Db::factory('Pdo_Mysql', array('host' => DB_HOST, 'username' => DB_USER, 'password' => DB_PASSWORD, 'dbname' => DB_DATABASE, 'charset'= 'UTF-8'));
+		$db = Zend_Db::factory('Pdo_Mysql', array('host' => DB_HOST, 'username' => DB_USER, 'password' => DB_PASSWORD, 'dbname' => DB_DATABASE, 'charset'=> 'UTF-8'));
 		return $db;
 	}
 
