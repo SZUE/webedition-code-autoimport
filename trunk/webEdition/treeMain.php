705cd1d5655b0580f9dc0677fde78ad62c561d60
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-04-21 04:59:57 +0000

    Bug #4287

--- trunk/webEdition/treeMain.php
+++ trunk/webEdition/treeMain.php
@@ -22,9 +22,10 @@
 		print $_REQUEST["code"];
 		exit();
 	}
-
+	
 	include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we_classes/weMainTree.inc.php");
-
+	protect();
+	
 	$Tree =  new weMainTree("webEdition.php","top","top.resize.left.tree","top.load");
 
 	print $Tree->getHTMLContruct("if(top.treeResized){top.treeResized();}");
