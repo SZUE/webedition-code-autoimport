d9dc8d555b9c25cddbd17fa4b80e170825b1ad3e
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-05-15 13:08:24 +0000

    Einfügen von Zend für Update auf 1.10.4, wiedereinführen von php-Mailer wegen abwärtskompatibilität, #4342

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,5 +1,5 @@
 <?php
 define("WE_VERSION", "6.0.0.9");
 //define("WE_VERSION_SUPP", "");
-define("WE_ZFVERSION","1.9.6"); // recommended version of the Zend Framework (bundled with webEdition)
+define("WE_ZFVERSION","1.10.4"); // recommended version of the Zend Framework (bundled with webEdition)
 ?>
