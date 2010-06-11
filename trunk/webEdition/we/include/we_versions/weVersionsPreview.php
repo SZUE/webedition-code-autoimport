91651d468349e4a2cead87430c42531b8d4a507a
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-06-11 07:56:35 +0000

    Bug #4386

--- trunk/webEdition/we/include/we_versions/weVersionsPreview.php
+++ trunk/webEdition/we/include/we_versions/weVersionsPreview.php
@@ -19,6 +19,7 @@
  */
 
 include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we.inc.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we_versions/weVersions.class.inc.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_language/' . $GLOBALS['WE_LANGUAGE'] . '/versions.inc.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_language/' . $GLOBALS['WE_LANGUAGE'] . '/date.inc.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_language/' . $GLOBALS['WE_LANGUAGE'] . '/modules/customerFilter.inc.php');
