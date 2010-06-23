08bd13f89344e36111e2e42740e4f53fa1701633
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-06-23 04:42:44 +0000

    6.0.0.9 -> 6.1.0.0 und Nachtrag #4383

--- trunk/webEdition/we/include/we_tag.inc.php
+++ trunk/webEdition/we/include/we_tag.inc.php
@@ -6386,14 +6386,14 @@ function we_tag_url($attribs, $content)
 	if (isset($urls[$id])) { // do only work you have never done before
 		return $urls[$id];
 	}
-	if ($id == 0) {
+	if ($id == '0') {
 		$url = "/";
 	} else {
 	    if ($id=='self' || $id=='top'){
-			$doc = we_getDocForTag($docAttr, true); // check if we should use the top document or the  included document
-			$id = $doc->ID;
-		}
-		$row = getHash("SELECT Path,IsFolder,IsDynamic FROM " . FILE_TABLE . " WHERE ID=".abs($id)."", new DB_WE());
+			$doc = we_getDocForTag($id, true); // check if we should use the top document or the  included document
+			$testid = $doc->ID;
+		} else {$testid = $id;}
+		$row = getHash("SELECT Path,IsFolder,IsDynamic FROM " . FILE_TABLE . " WHERE ID=".abs($testid)."", new DB_WE());
 		$url = isset($row["Path"]) ? ($row["Path"] . ($row["IsFolder"] ? "/" : "")) : "";
 	}
 	$urls[$id] = $url;
--- trunk/webEdition/we/include/we_version.php
+++ trunk/webEdition/we/include/we_version.php
@@ -1,5 +1,5 @@
 <?php
-define("WE_VERSION", "6.0.0.9");
-define("WE_VERSION_SUPP", "");
+define("WE_VERSION", "6.1.0.0");
+define("WE_VERSION_SUPP","");
 define("WE_ZFVERSION","1.10.4"); // recommended version of the Zend Framework (bundled with webEdition)
 ?>
