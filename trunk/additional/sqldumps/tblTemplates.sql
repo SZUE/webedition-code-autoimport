58759cdc4d261762661f5c1795b160e13c77577b
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-07-09 09:51:53 +0000

    Bug #4431 erster Versuch

--- trunk/additional/sqldumps/tblTemplates.sql
+++ trunk/additional/sqldumps/tblTemplates.sql
@@ -19,7 +19,7 @@ CREATE TABLE tblTemplates (
   ModifierID bigint(20) NOT NULL default '0',
   MasterTemplateID bigint(20) NOT NULL default '0',
   IncludedTemplates varchar(255) NOT NULL default '',
-  CacheType ENUM( '', 'none', 'tag', 'document', 'full' ) DEFAULT 'none' NOT NULL,
+  `CacheType` enum('','none','tag','document','full') NOT NULL default 'none',
   CacheLifeTime int(5) NOT NULL default '0',
   PRIMARY KEY  (ID),
   KEY MasterTemplateID (MasterTemplateID),
