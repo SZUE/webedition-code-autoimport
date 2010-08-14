6ea08e4a30404ee1f9c7a6da761be9ce93195d43
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-08-14 10:25:19 +0000

    Nachtrag #4520

--- trunk/additional/lang_iso/Deutsch/browser.inc.php
+++ trunk/additional/lang_iso/Deutsch/browser.inc.php
@@ -18,11 +18,11 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 
-$GLOBALS["l_browser"]["save_link_as_IE"] = 'Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Ziel speichern unter..."';
-$GLOBALS["l_browser"]["save_link_as_FF"] = 'Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Ziel speichern unter..."';
-$GLOBALS["l_browser"]["save_link_as_SM"] = 'Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Link-Ziel speichern unter..."';
-$GLOBALS["l_browser"]["save_link_as_SAFARI"] = 'Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Verkn¸pfte Datei laden"';
-$GLOBALS["l_browser"]["save_link_as_DEFAULT"] = 'Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Ziel speichern unter..."';
+$GLOBALS["l_browser"]["save_link_as_IE"] = 'Schlieﬂen Sie dieses Fenster erst, nachdem der Download erfolgreich beendet ist!<br/><br/>Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Ziel speichern unter..."';
+$GLOBALS["l_browser"]["save_link_as_FF"] = 'Schlieﬂen Sie dieses Fenster erst, nachdem der Download erfolgreich beendet ist!<br/><br/>Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Ziel speichern unter..."';
+$GLOBALS["l_browser"]["save_link_as_SM"] = 'Schlieﬂen Sie dieses Fenster erst, nachdem der Download erfolgreich beendet ist!<br/><br/>Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Link-Ziel speichern unter..."';
+$GLOBALS["l_browser"]["save_link_as_SAFARI"] = 'Schlieﬂen Sie dieses Fenster erst, nachdem der Download erfolgreich beendet ist!<br/><br/>Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Verkn¸pfte Datei laden"';
+$GLOBALS["l_browser"]["save_link_as_DEFAULT"] = 'Schlieﬂen Sie dieses Fenster erst, nachdem der Download erfolgreich beendet ist!<br/><br/>Klicken Sie mit der rechten Maustaste auf den Link und w‰hlen Sie "Ziel speichern unter..."';
 
 
 ?>
