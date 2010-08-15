61d3ed9e1eb708191f9f874eccc7685b3e724141
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2010-08-15 19:04:24 +0000

    Bug #4549: Index

--- additional/sqldumps/tblAnzeigePrefs.sql
+++ additional/sqldumps/tblAnzeigePrefs.sql
@@ -2,7 +2,8 @@ CREATE TABLE tblAnzeigePrefs (
   ID int(15) NOT NULL auto_increment,
   strDateiname varchar(255) NOT NULL default '',
   strFelder text NOT NULL,
-  PRIMARY KEY  (ID)
+  PRIMARY KEY  (ID),
+  KEY `strDateiname` (`strDateiname`)
 ) ENGINE=MyISAM;
 /* query separator */
 INSERT INTO tblAnzeigePrefs VALUES (1,'edit_shop_properties','a:2:{s:14:"customerFields";a:0:{}s:19:"orderCustomerFields";a:0:{}}');
