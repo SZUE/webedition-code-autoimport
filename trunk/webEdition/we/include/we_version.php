7482f91b232753b0f9afb6c73a794e159e49fbff
tequillaymota <tequillaymota@5781a423-4729-0410-aacd-f88070b95415>
2010-08-16 13:25:47 +0000

    versionsnummer vorübergehend zurückgesetzt

--- trunk/webEdition/we/include/we_version.php
+++ trunk/webEdition/we/include/we_version.php
@@ -1,5 +1,5 @@
 <?php
-define("WE_VERSION", "6.1.0.1");
+define("WE_VERSION", "6.1.0.0");
 define("WE_VERSION_SUPP","svn");
 define("WE_ZFVERSION","1.10.6"); // recommended version of the Zend Framework (bundled with webEdition)
 define("WE_SVNREV","0000");
