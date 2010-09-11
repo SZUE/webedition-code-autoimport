a71b9e06aee5a04af8cbe5417335254dfdd0d321
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-09-11 14:45:45 +0000

    Features #4702, #4706, #4710

--- trunk/webEdition/we/include/we_language/Deutsch_UTF-8/sysinfo.inc.php
+++ trunk/webEdition/we/include/we_language/Deutsch_UTF-8/sysinfo.inc.php
@@ -43,11 +43,21 @@ $_sysinfo["safe_mode warning"] = 'Der PHP Safe Mode kann u.U. zu Probelem bei In
 $_sysinfo["zend_framework warning"] = 'Sie verwenden derzeit eine andere Version des Zend Framework als die für webEdition ursprünglich vorhergesehene Version '.WE_ZFVERSION.'.';
 $_sysinfo["suhosin warning"] = 'Wegen der vielfältigen Konfigurationsmöglichkeiten kann bei Nutzung dieser PHP Erweiterung die volle Funktionsfähigkeit von webEdition leider nicht garantiert werden.';
 $_sysinfo["dbversion warning"] = 'Der verwendete DB-Server meldet die Version %s, webEdition benötigt jedoch mindestens die MySQL-Server Version 5.0. webEdition mag mit der genutzten Version funktionieren, dies kann jedoch nicht für neue webEdition Versionen (z.B. nach Updates) garantiert werden.  Spätestens ab webEdition Version 7 wird MySQL Version 5 benötigt. Außerdem: die auf dem Server installierte MySQL Version ist veraltet. Für diese Version gibt es keine Updates mehr, dies kann die Sicherheit des gesamten Systems beeinträchtigen.';
+$_sysinfo["pcre warning"] = 'Versionen vor 7.0 können zu ernsten Problemen führen';
+$_sysinfo["pcre_unkown"] = 'Nicht detektierbar';
+$_sysinfo["exif warning"] = 'EXIF-Metadaten für Bilder sind nicht verfügbar';
+$_sysinfo['sdk_db warning'] = 'SDK Operationen und WE-APPS mit Datenbanknutzung sind nicht verfügbar';
+$_sysinfo['phpext warning'] = 'nicht verfügbar: ';
+$_sysinfo['phpext warning2'] = 'Die Funktion von webEdition kann nicht garantiert werden!';
 
 $_sysinfo['connection_types'] = 'Update-Verbindungstypen';
 $_sysinfo['gdlib'] = 'GDlib Unterstützung';
 $_sysinfo['mbstring'] = 'Multibyte String Funktionen';
 $_sysinfo['version'] = 'Version';
 $_sysinfo['available'] = 'verfügbar';
+$_sysinfo['exif'] = 'EXIF Unterstützung';
+$_sysinfo['pcre'] = 'PCRE-Extension';
+$_sysinfo['sdk_db'] = 'SDK/Apps DB Unterstützung';
+$_sysinfo['phpext'] = 'Minimal notwendige PHP-Extensions';
 
 ?>
