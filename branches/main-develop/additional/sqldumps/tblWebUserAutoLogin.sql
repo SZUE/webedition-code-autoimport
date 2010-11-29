170f2c001761ef3539568a03b43d3cc3372559a2
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2010-11-29 15:57:24 +0000

    Feature #4872, Feature #4870 Nachtrag

--- branches/main-develop/additional/sqldumps/tblWebUserAutoLogin.sql
+++ branches/main-develop/additional/sqldumps/tblWebUserAutoLogin.sql
@@ -3,6 +3,6 @@ CREATE TABLE tblWebUserAutoLogin (
   WebUserID bigint(20) NOT NULL default '0',
   LastIp varchar(40)NOT NULL DEFAULT '',
   LastLogin varchar(24) NOT NULL default '',
-  PRIMARY KEY  (AutoLoginID,UserID),
+  PRIMARY KEY  (AutoLoginID,WebUserID),
   KEY `LastLogin` (`LastLogin`)
 ) ENGINE=MyISAM;
