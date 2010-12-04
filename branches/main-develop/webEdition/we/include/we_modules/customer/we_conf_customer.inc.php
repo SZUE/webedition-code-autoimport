a13b1c9e0bf7ebc20c61939bc20955a4bc0ee09c
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2010-12-04 18:36:46 +0000

    some reformating

--- branches/main-develop/webEdition/we/include/we_modules/customer/we_conf_customer.inc.php
+++ branches/main-develop/webEdition/we/include/we_modules/customer/we_conf_customer.inc.php
@@ -18,18 +18,14 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 
+define('CUSTOMER_TABLE',TBL_PREFIX . 'tblWebUser');
+define('CUSTOMER_ADMIN_TABLE',TBL_PREFIX . 'tblWebAdmin');
+define('CUSTOMER_FILTER_TABLE',TBL_PREFIX . 'tblcustomerfilter');
+define('CUSTOMER_AUTOLOGIN_TABLE',TBL_PREFIX . 'tblWebUserAutoLogin');
+define('CUSTOMER_SESSION_TABLE',TBL_PREFIX . 'tblWebUserSessions');
+define('CUSTOMER_AUTOLOGIN_LIFETIME',TBL_PREFIX . '31536000');
+define('CUSTOMER_SESSION_LIFETIME',TBL_PREFIX . '300');
+define('WE_CUSTOMER_MODULE_DIR',$_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we_modules/customer/');
+define('WE_CUSTOMER_MODULE_PATH','/webEdition/we/include/we_modules/customer/');
 
-
-
-
-define("CUSTOMER_TABLE",TBL_PREFIX . "tblWebUser");
-define("CUSTOMER_ADMIN_TABLE",TBL_PREFIX . "tblWebAdmin");
-define("CUSTOMER_FILTER_TABLE",TBL_PREFIX . "tblcustomerfilter");
-define("CUSTOMER_AUTOLOGIN_TABLE",TBL_PREFIX . "tblWebUserAutoLogin");
-define("CUSTOMER_SESSION_TABLE",TBL_PREFIX . "tblWebUserSessions");
-define("CUSTOMER_AUTOLOGIN_LIFETIME",TBL_PREFIX . "31536000");
-define("CUSTOMER_SESSION_LIFETIME",TBL_PREFIX . "300");
-define("WE_CUSTOMER_MODULE_DIR",$_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we_modules/customer/");
-define("WE_CUSTOMER_MODULE_PATH","/webEdition/we/include/we_modules/customer/");
-
-@include(WE_CUSTOMER_MODULE_DIR."we_conf_customerpro.inc.php");
+@include(WE_CUSTOMER_MODULE_DIR.'we_conf_customerpro.inc.php');
