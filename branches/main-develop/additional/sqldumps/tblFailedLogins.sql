59a2a624dfde30703f18878c9d68b98b99b21ae2
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2010-12-12 17:29:09 +0000

    Feature #4837

--- branches/main-develop/additional/sqldumps/tblFailedLogins.sql
+++ branches/main-develop/additional/sqldumps/tblFailedLogins.sql
@@ -2,6 +2,8 @@ CREATE TABLE tblFailedLogins (
   ID bigint(20) NOT NULL default '0',
   Username varchar(64) NOT NULL default '',
   `Password` varchar(32) NOT NULL default '',
-  IP varchar(15) NOT NULL default '',
-  LoginDate int(11) NOT NULL default '0'
+  IP varchar(40) NOT NULL default '',
+  LoginDate int(11) NOT NULL default '0',
+  PRIMARY KEY (`ID`),
+  KEY IP (IP,LoginDate)
 ) ENGINE=MyISAM;
