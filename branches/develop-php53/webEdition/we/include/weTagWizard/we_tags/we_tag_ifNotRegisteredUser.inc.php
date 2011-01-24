811cd9db54647b356ca4a21b402a8d9a04127e3d
arminSchulz <arminSchulz@5781a423-4729-0410-aacd-f88070b95415>
2011-01-24 06:51:25 +0000

    Commit after Merge from main-develop

--- branches/develop-php53/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotRegisteredUser.inc.php
+++ branches/develop-php53/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotRegisteredUser.inc.php
@@ -7,5 +7,5 @@ $GLOBALS['weTagWizard']['weTagData']['needsEndtag'] = true;
 
 $GLOBALS['weTagWizard']['attribute']['id239_permission'] = new weTagData_sqlColAttribute('239', 'permission', CUSTOMER_TABLE, false, array(), '');
 $GLOBALS['weTagWizard']['attribute']['id240_match'] = new weTagData_textAttribute('240', 'match', false, '');
-$GLOBALS['weTagWizard']['attribute']['id745_cfilter'] = new weTagData_selectAttribute('745', 'cfilter', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
-?>
+$GLOBALS['weTagWizard']['attribute']['id705_userid'] = new weTagData_textAttribute('705', 'userid', false, '');
+$GLOBALS['weTagWizard']['attribute']['id745_allowNoFilter'] = new weTagData_selectAttribute('745', 'allowNoFilter', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
