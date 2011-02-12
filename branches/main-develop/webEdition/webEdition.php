f846e4b9aefc528e4bd72d59ffff15a3575bd990
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2011-02-12 12:51:38 +0000

    Test für automatische Generierung neue Version

--- branches/main-develop/webEdition/webEdition.php
+++ branches/main-develop/webEdition/webEdition.php
@@ -18,7 +18,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 
-
 include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/"."we.inc.php");
 include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/"."we_html_tools.inc.php");
 include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we_language/".$GLOBALS["WE_LANGUAGE"]."/global.inc.php");
