2b9dbb2b4b768c1fa91eaa7f4f70c47796eb7c12
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2011-02-23 23:32:44 +0000

    Feature #4925

--- webEdition/we/include/we_db.inc.php
+++ webEdition/we/include/we_db.inc.php
@@ -164,13 +164,9 @@ class DB_WE extends DB_Sql {
 		$this->Errno = mysql_errno();
 		$this->Error = mysql_error();
 		if (!$this->Query_ID) {
+			trigger_error('MYSQL-ERROR'."\n".'Fehler: ' . $this->Errno ."\n". 'Detail: ' . $this->Error ."\n". 'Query: ' . $Query_String . "\n", E_USER_WARNING);
 			if (defined('WE_SQL_DEBUG') && WE_SQL_DEBUG == 1) {
-				error_log('
-MYSQL-ERROR
-Fehler: ' . $this->Errno . '
-Detail: ' . $this->Error . '
-Query: ' . $Query_String . '
-');
+				error_log('MYSQL-ERROR - Fehler: ' . $this->Errno . ' Detail: ' . $this->Error . ' Query: ' . $Query_String);
 			}
 			$this->halt('Invalid SQL: ' . $Query_String);
 		}
