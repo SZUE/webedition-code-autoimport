e9cbf96bbee078087e3f59d3dbc9b639db3a0276
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2011-03-13 20:33:19 +0000

    Feature #3613 - kleinere Anpassungen

--- branches/main-develop/webEdition/we/include/we_classes/we_wysiwyg.class.inc.php
+++ branches/main-develop/webEdition/we/include/we_classes/we_wysiwyg.class.inc.php
@@ -1173,14 +1173,18 @@ tinyMCE.init({
 
 	// Theme options
 	theme_advanced_buttons1 : "bold,italic,underline,sub,sup,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-	theme_advanced_buttons2 : "pastetext,pasteword,cleanup,|,search,replace,|,bullist,numlist,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-	theme_advanced_buttons4 : "styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,|,insertfile,insertimage",
+	theme_advanced_buttons2 : "insertdate,inserttime,pastetext,pasteword,removeformat,cleanup,|,search,replace,|,bullist,numlist,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,help,code,|,forecolor,backcolor",
+	theme_advanced_buttons3 : "tablecontrols,|,hr,visualaid,|,charmap,emotions,iespell,media,advhr,|,ltr,rtl,|,fullscreen,preview,print",
+	theme_advanced_buttons4 : "styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,blockquote,|,insertfile,insertimage",
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
 	theme_advanced_statusbar_location : "bottom",
 	theme_advanced_resizing : true,
-
+	theme_advanced_source_editor_height : "300",
+	theme_advanced_source_editor_width : "500",
+	plugin_preview_height : "300"
+	plugin_preview_width : "500",
+	theme_advanced_disable : "",
 	// Skin options
 	skin : "o2k7",
 	skin_variant : "silver",
