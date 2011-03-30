a7277becb1a4f4c19a9ee105beacc8811fe27ecf
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2011-03-30 20:58:30 +0000

    zu Feature #5132

--- branches/main-develop/webEdition/we/include/we_global.inc.php
+++ branches/main-develop/webEdition/we/include/we_global.inc.php
@@ -3847,7 +3847,7 @@ function g_l($name, $specific) {
 		//get local variable
 		if($tmp !== false){
 			$cache["l_$name"]=${"l_$name"};
-			return $tmp;
+			return mb_convert_encoding($tmp, 'HTML-ENTITIES', "UTF-8");
 		}else{
 				trigger_error('Requested lang entry '."l_$name$specific".' not found in '.$file.'!',E_USER_WARNING);
 			return false;
