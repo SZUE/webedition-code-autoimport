26f77e97fb6757e4f68db9a03b12467c78d7ad93
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2011-04-03 04:54:37 +0000

    Bug 5165 und 5067

--- trunk/webEdition/we/include/we_modules/glossary/weGlossaryReplace.php
+++ trunk/webEdition/we/include/we_modules/glossary/weGlossaryReplace.php
@@ -116,7 +116,7 @@
 			$link = $cache->get('link');
 			unset($cache);
 			// first check if there is a body tag inside the sourcecode
-			preg_match("/<body.*>(.*)<\/body>/siU", $src, $matches);
+			preg_match("/<body.*>(.*)<\/body>/si", $src, $matches);
 
 			if(isset($matches[1])) {
 				// take the code between the body-tags
@@ -126,7 +126,7 @@
 				// take the whole code
 				$srcBody = $replBody = $src;
 
-			}
+			}//p_r($srcBody);
 			/*
 			This is the fastest variant
 			*/
@@ -164,7 +164,7 @@
 			*/
 
 			$replBody = str_replace("@@@we@@@", "'", $replBody);
-			if(isset($matches[0])) {
+			if(isset($matches[1])) {
 				return str_replace($srcBody, $replBody, $src);
 
 			} else {
