0f093ae802214bdd94ae06f44db51e7ea2e3962d
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2011-04-15 23:24:28 +0000

    BugFix #5162

--- webEdition/we/include/we_classes/listview/listviewBase.class.php
+++ webEdition/we/include/we_classes/listview/listviewBase.class.php
@@ -284,7 +284,7 @@ class listviewBase{
 				}
 				$newdate=$year.'-'.$month.'-'.$day;
 			}
-			$attribs['href'] = we_tag('url',array('id'=>($urlID?$urlID:'self'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString('we_lv_calendar_'.$this->name.'='.$this->calendar_struct['calendar'].'&we_lv_datefield_'.$this->name.'='.$this->calendar_struct['datefield'].'&we_lv_date_'.$this->name.'='.$newdate));
+			$attribs['href'] = we_tag('url',array('id'=>($urlID?$urlID:'top'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString('we_lv_calendar_'.$this->name.'='.$this->calendar_struct['calendar'].'&we_lv_datefield_'.$this->name.'='.$this->calendar_struct['datefield'].'&we_lv_date_'.$this->name.'='.$newdate));
 			if($only){
 			    $this->close_a = false;
 			    return (isset($attribs[$only]) ? $attribs[$only] : '');
@@ -295,7 +295,7 @@ class listviewBase{
 		else if($this->hasPrevPage()){
 
 			$foo = $this->start - $this->rows;
-			$attribs['href'] = we_tag('url',array('id'=>($urlID?$urlID:'self'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString('we_lv_start_'.$this->name.'='.$foo));
+			$attribs['href'] = we_tag('url',array('id'=>($urlID?$urlID:'top'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString('we_lv_start_'.$this->name.'='.$foo));
 
 			if($only){
 			    $this->close_a = false;
@@ -404,7 +404,7 @@ class listviewBase{
 				}
 				$newdate=$year."-".$month."-".$day;
 			}
-			$attribs["href"] = we_tag('url',array('id'=>($urlID?$urlID:'self'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString("we_lv_calendar_".$this->name."=".$this->calendar_struct["calendar"]."&we_lv_datefield_".$this->name."=".$this->calendar_struct["datefield"]."&we_lv_date_".$this->name."=$newdate"));
+			$attribs["href"] = we_tag('url',array('id'=>($urlID?$urlID:'top'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString("we_lv_calendar_".$this->name."=".$this->calendar_struct["calendar"]."&we_lv_datefield_".$this->name."=".$this->calendar_struct["datefield"]."&we_lv_date_".$this->name."=$newdate"));
 			if($only){
 			    $this->close_a = false;
 			    return (isset($attribs[$only]) ? $attribs[$only] : "");
@@ -415,7 +415,7 @@ class listviewBase{
 		else if($this->hasNextPage()){
 
 			$foo = $this->start + $this->rows;
-			$attribs["href"] = we_tag('url',array('id'=>($urlID?$urlID:'self'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString("we_lv_start_".$this->name."=$foo"));
+			$attribs["href"] = we_tag('url',array('id'=>($urlID?$urlID:'top'))).'?'. htmlspecialchars(listviewBase::we_makeQueryString("we_lv_start_".$this->name."=$foo"));
 			if($only){
 			    $this->close_a = false;
 			    return (isset($attribs[$only]) ? $attribs[$only] : "");
