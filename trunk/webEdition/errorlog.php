0276328808610fd062e4fb8a474136de7f62f339
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2011-04-24 05:21:19 +0000

    Nachtrag 5180

--- trunk/webEdition/errorlog.php
+++ trunk/webEdition/errorlog.php
@@ -40,47 +40,47 @@
   </colgroup>
   <tr bgcolor="#f7f7f7" valign="top">
   	<td nowrap="nowrap" style="border-bottom: 1px solid #265da6; border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>#'.$_infoArr['ID'].'</b></font></td>
-    <td  style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.$_infoArr['Date'].'</font></td>
+    <td  style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.htmlentities($_infoArr['Date']).'</font></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-bottom: 1px solid #265da6; border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Error type:</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.$_infoArr['Type'].'</i></font></td>
+    <td style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.htmlentities($_infoArr['Type']).'</i></font></td>
   </tr>
   <tr valign="top">
     <td  style="border-bottom: 1px solid #265da6; border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Error message:</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.$_infoArr['Text'].'</i></font></pre></td>
+    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.htmlentities($_infoArr['Text']).'</i></font></pre></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-bottom: 1px solid #265da6; border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Script name:</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.$_infoArr['File'].'</i></font></td>
+    <td style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.htmlentities($_infoArr['File']).'</i></font></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-bottom: 1px solid #265da6; border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Line number:</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.$_infoArr['Line'].'</i></font></td>
+    <td style="border-bottom: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><i>'.htmlentities($_infoArr['Line']).'</i></font></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-bottom: 1px solid #265da6;border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Backtrace</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.$_infoArr['Backtrace'].'
+    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.htmlentities($_infoArr['Backtrace']).'
       </font></pre></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-bottom: 1px solid #265da6;border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Request</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.$_infoArr['Request'].'
+    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.htmlentities($_infoArr['Request']).'
       </font></pre></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-bottom: 1px solid #265da6;border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Session</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.$_infoArr['Session'].'
+    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.htmlentities($_infoArr['Session']).'
       </font></pre></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Server</b></font></td>
-    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.$_infoArr['Server'].'
+    <td style="border-bottom: 1px solid #265da6;"><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.htmlentities($_infoArr['Server']).'
       </font></pre></td>
   </tr>
   <tr valign="top">
     <td nowrap="nowrap" style="border-bottom: 1px solid #265da6;border-right: 1px solid #265da6;"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Global</b></font></td>
-    <td ><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.$_infoArr['Global'].'
+    <td ><pre><font face="Verdana, Arial, Helvetica, sans-serif" size="2">'.htmlentities($_infoArr['Global']).'
       </font></pre></td>
   </tr>
   
@@ -111,15 +111,15 @@
 			$q="TRUNCATE TABLE `".ERROR_LOG_TABLE."`";
 			$db->query($q);
 		}
-		$q="SELECT * FROM ".ERROR_LOG_TABLE." ORDER By Date DESC ";
+		$q="SELECT * FROM ".ERROR_LOG_TABLE." ORDER BY ID DESC ";
 		$db->query($q);
-		$count = 15;
+		$count = 1;
 	
 		$nextprev = "";
 		$size = $db->num_rows();
 		if ($size>0){
 			
-			$start = (isset($_REQUEST['start']) ? $_REQUEST['start'] : 0);
+			$start = (isset($_REQUEST['start']) ? abs($_REQUEST['start']) : 0);
 			$start = $start < 0 ? 0 : $start;
 			$start = $start>$size ? $size : $start;
 
@@ -131,22 +131,22 @@
 
 			$ind = 0;
 			$nextprev = '<table style="margin-top: 10px;" border="0" cellpadding="0" cellspacing="0"><tr><td>';
-			if($start<$size){
-				$nextprev .= $we_button->create_button("back", '/webEdition/errorlog.php' . "?start=".$back); //bt_back
+			if($start>0){
+				$nextprev .= $we_button->create_button("next", '/webEdition/errorlog.php' . "?start=".$back); //bt_back
 			}else{
-				$nextprev .= $we_button->create_button("back", "", false, 100, 22, "", "", true);
+				$nextprev .= $we_button->create_button("next", "", false, 100, 22, "", "", true);
 			}
 
-			$nextprev .= getPixel(23,1)."</td><td align='center' class='defaultfont' width='120'><b>".($start+1)."&nbsp;-&nbsp;";
+			$nextprev .= getPixel(23,1)."</td><td align='center' class='defaultfont' width='120'><b>".($size-$start);
 
-			$nextprev .= ($start+$count);
+			//$nextprev .= "&nbsp;-&nbsp".($size-$start+$count);
 
-			$nextprev .= "&nbsp;".$GLOBALS["l_global"]["from"]." ".($size+1)."</b></td><td>".getPixel(23,1);
+			$nextprev .= "&nbsp;".$GLOBALS["l_global"]["from"]." ".($size)."</b></td><td>".getPixel(23,1);
 
-			if($next > 0){
-				$nextprev .= $we_button->create_button("next",'/webEdition/errorlog.php' . "?start=".$next); //bt_next
+			if($next < $size){
+				$nextprev .= $we_button->create_button("back",'/webEdition/errorlog.php' . "?start=".$next); //bt_next
 			}else{
-				$nextprev .= $we_button->create_button("next", "", "", 100, 22, "", "", true);
+				$nextprev .= $we_button->create_button("back", "", "", 100, 22, "", "", true);
 			}
 			$nextprev .= "</td></tr></table>";
 			$_parts[] = array(
@@ -154,7 +154,7 @@
 					  'html'=> $nextprev,
 					  'space'=>$_space_size
 				  );
-			$q="SELECT * FROM ".ERROR_LOG_TABLE." ORDER By Date DESC LIMIT ".$start.",".$count;
+			$q="SELECT * FROM ".ERROR_LOG_TABLE." ORDER By ID DESC LIMIT ".$start.",".$count;
 			$db->query($q);
 		  
 			while ($db->next_record()){
