890b7fdeadf1349940705c68a9cc7a67a4108b26
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2011-04-26 22:03:27 +0000

    Fix #5225

--- trunk/webEdition/we/include/we_tags/we_tag_icon.inc.php
+++ trunk/webEdition/we/include/we_tags/we_tag_icon.inc.php
@@ -19,17 +19,13 @@
  */
 
 function we_tag_icon($attribs, $content){
-	$foo = attributFehltError($attribs, "id", "a");
-	if ($foo)
-		return $foo;
-	$xml = we_getTagAttribute("xml", $attribs, "");
-	$id = we_getTagAttribute("id", $attribs);
-	$row = getHash("SELECT Path,IsFolder,IsDynamic FROM " . FILE_TABLE . " WHERE ID=".abs($id)."", new DB_WE());
+	if (($foo=attributFehltError($attribs, 'id', 'icon'))) return $foo;
+	$xml = we_getTagAttribute('xml', $attribs, '');
+	$id = we_getTagAttribute('id', $attribs);
+	$row = getHash('SELECT Path,IsFolder,IsDynamic FROM ' . FILE_TABLE . ' WHERE ID='.abs($id), new DB_WE());
 	if (count($row)) {
-		$url = $row["Path"] . ($row["IsFolder"] ? "/" : "");
-		return getHtmlTag('link', array(
-			'rel' => 'shortcut icon', 'href' => $url, 'xml' => $xml
-		));
+		$url = $row['Path'] . ($row['IsFolder'] ? '/' : '');
+		return getHtmlTag('link', array('rel' => 'shortcut icon', 'href' => $url, 'xml' => $xml));
 	}
-	return "";
+	return '';
 }
