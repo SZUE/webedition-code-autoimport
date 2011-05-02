7163e9c48633b06d0d2a5cb724b4e8b6eae8aa85
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2011-05-02 18:36:33 +0000

    Fix #5243

--- additional/sqldumps/dump/complete.sql
+++ additional/sqldumps/dump/complete.sql
@@ -355,11 +355,13 @@ CREATE TABLE tblNewsletterLog (
 CREATE TEMPORARY TABLE IF NOT EXISTS _newNewsPref(
   pref_name varchar(30) NOT NULL default '',
   pref_value longtext NOT NULL,
-	PRIMARY KEY name (pref_name(30))
+  PRIMARY KEY name (pref_name(30))
 )ENGINE = MYISAM;
-
-INSERT INTO _newNewsPref SELECT DISTINCT * FROM tblNewsletterPrefs GROUP BY pref_name;
+/* query separator */
+INSERT IGNORE INTO _newNewsPref SELECT DISTINCT * FROM tblNewsletterPrefs GROUP BY pref_name;
+/* query separator */
 TRUNCATE tblNewsletterPrefs;
+/* query separator */
 INSERT INTO tblNewsletterPrefs SELECT * FROM _newNewsPref;
 /* query separator */
 
@@ -1103,7 +1105,9 @@ CREATE TEMPORARY TABLE IF NOT EXISTS _delKeys(
   ID bigint
 )ENGINE = MEMORY;
 
+/* query separator */
 INSERT INTO _delKeys SELECT s.ID FROM tblsearchtool s, tblsearchtool t WHERE s.Path=t.Path AND s.ID>t.ID;
+/* query separator */
 DELETE FROM tblsearchtool WHERE ID IN (SELECT ID FROM _delKeys);
 /* query separator */
 
