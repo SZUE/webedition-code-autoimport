84d058b5d2434dabfb48d8f24810688e7b553b06
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2011-06-13 06:12:22 +0000

    Bug #5328

--- webEdition/we/include/we_classes/weSelectorQuery.class.inc.php
+++ webEdition/we/include/we_classes/weSelectorQuery.class.inc.php
@@ -351,7 +351,11 @@ class weSelectorQuery {
 	 * @return string
 	 */
 	function getUserExtraQuery($table, $useCreatorID=true){
-		$userExtraSQL = makeOwnersSql(false) . " ";
+		if ($table != NAVIGATION_TABLE) {
+			$userExtraSQL = makeOwnersSql(false) . " ";
+		} else {
+			$userExtraSQL = " ";
+		}
 
 		if(get_ws($table)) {
 			$userExtraSQL .= getWsQueryForSelector($table);
