e8bf2a7cf7a80924cfa8cdf7b8ddc7d0f43de6f5
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2011-10-06 16:17:55 +0000

    Bug #5603

--- webEdition/we/include/weTagWizard/we_tags/we_tag_ifShopPayVat.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_ifShopPayVat.inc.php
@@ -1,3 +1,4 @@
 <?php
-
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_textAttribute.class.php');
 $GLOBALS['weTagWizard']['weTagData']['needsEndtag'] = true;
+$GLOBALS['weTagWizard']['attribute']['id892_customerid'] = new weTagData_textAttribute('892', 'customerid', false, '');
