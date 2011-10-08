71adc8588e0786534c15b2403d118a692ce7fad6
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2011-10-08 04:28:24 +0000

    Nachtrag #5603

--- webEdition/we/include/weTagWizard/we_tags/we_tag_ifShopPayVat.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_ifShopPayVat.inc.php
@@ -1,4 +1,4 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_textAttribute.class.php');
 $GLOBALS['weTagWizard']['weTagData']['needsEndtag'] = true;
-$GLOBALS['weTagWizard']['attribute']['id892_namefrom'] = new weTagData_textAttribute('892', 'namefrom', false, '');
+$GLOBALS['weTagWizard']['attribute']['id892_customerfrom'] = new weTagData_textAttribute('892', 'customerfrom', false, '');
