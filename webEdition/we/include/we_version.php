e72ea28baf4298254e6b03dd3862e335bc154f28
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2011-10-20 03:21:23 +0000

    Version 6.2.4

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,5 +1,5 @@
 <?php
-define("WE_VERSION", "6.2.3.0");
+define("WE_VERSION", "6.2.4.0");
 define("WE_VERSION_SUPP","release");
 define("WE_ZFVERSION","1.11.7"); // recommended version of the Zend Framework (bundled with webEdition)
 define("WE_SVNREV","");
