9707c0153ab92c084c8d3b5b5b1faa65bc9f9710
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2011-12-25 17:17:24 +0000

    Fix #5845

--- branches/main-develop/webEdition/we/include/we_modules/messaging/messaging_edit_folder.php
+++ branches/main-develop/webEdition/we/include/we_modules/messaging/messaging_edit_folder.php
@@ -22,12 +22,11 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 
-include_once($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we.inc.php');
-include_once(WE_MESSAGING_MODULE_DIR . "we_messaging.inc.php");
-
 if (!preg_match('|^([a-f0-9]){32}$|i',$_REQUEST['we_transaction'])) {
 	exit();
 }
+include_once($_SERVER['DOCUMENT_ROOT'].'/webEdition/we/include/we.inc.php');
+
 $messaging = new we_messaging($_SESSION["we_data"][$_REQUEST['we_transaction']]);
 $messaging->set_login_data($_SESSION["user"]["ID"], $_SESSION["user"]["Username"]);
 $messaging->init($_SESSION["we_data"][$_REQUEST['we_transaction']]);
