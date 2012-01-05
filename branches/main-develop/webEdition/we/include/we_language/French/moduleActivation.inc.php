941c38511e54c214a89454477775cb7104557b4b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-01-05 21:04:08 +0000

    new lang files

--- branches/main-develop/webEdition/we/include/we_language/French/moduleActivation.inc.php
+++ branches/main-develop/webEdition/we/include/we_language/French/moduleActivation.inc.php
@@ -2,5 +2,5 @@
 /** Generated language file of webEdition CMS */
 $l_moduleActivation=array(
 	'content'=>'You can select which Extra to use, by activating them in the <a href="javascript:top.opener.top.we_cmd(`openPreferences`, `setting_active_integrated_modules`);">preferences</a>',
-	'headline'=>'The Extra `%s` ist currently not activated',
+	'headline'=>'L`extra `%s` n`est actuelement pas activé',
 );
