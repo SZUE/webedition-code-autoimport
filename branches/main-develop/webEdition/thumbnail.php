e0c882bce96e1b6c6ad375c39a5822ad9e8dcd37
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-01-11 22:11:56 +0000

    Fix thumbnail

--- branches/main-develop/webEdition/thumbnail.php
+++ branches/main-develop/webEdition/thumbnail.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * webEdition CMS
  *
@@ -21,40 +22,32 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
-include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we_inc_min.inc.php');
-
+include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we.inc.php');
+//include_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we_inc_min.inc.php');
 //we_html_tools::protect();
 
-if(!isset($_REQUEST['id']) || $_REQUEST['id']==''||
-				!isset($_REQUEST['path']) || $_REQUEST['path']==''||
-				!isset($_REQUEST['size']) || $_REQUEST['size']==''||
-				!isset($_REQUEST['extension']) || $_REQUEST['extension']=='') {
+if(!isset($_REQUEST['id']) || $_REQUEST['id'] == '' ||
+	!isset($_REQUEST['path']) || $_REQUEST['path'] == '' ||
+	!isset($_REQUEST['size']) || $_REQUEST['size'] == '' ||
+	!isset($_REQUEST['extension']) || $_REQUEST['extension'] == ''){
 	exit();
 }
 
 $imageId = $_REQUEST['id'];
 $imagePath = $_REQUEST['path'];
 $imageSizeW = $_REQUEST['size'];
-if (isset($_REQUEST['size2'])){
-	$imageSizeH =$_REQUEST['size2'];
-} else {
-	$imageSizeH =$imageSizeW ;
-}
+$imageSizeH = (isset($_REQUEST['size2']) ? $_REQUEST['size2'] : $imageSizeW);
 
-$whiteList = array();
-$ct=new we_base_ContentTypes();
-$exts = $ct->getExtension('image/*');
-if(!empty($exts)){
-	$whiteList = makeArrayFromCSV($exts);
-}
 
-if (!in_array(strtolower($_REQUEST['extension']), $whiteList)) {
+$ct = new we_base_ContentTypes();
+$whiteList = $ct->getExtension('image/*');
+
+if(!in_array(strtolower($_REQUEST['extension']), $whiteList)){
 	exit();
 }
 
 $imageExt = substr($_REQUEST['extension'], 1, strlen($_REQUEST['extension']));
 
 $thumbpath = we_image_edit::createPreviewThumb($imagePath, $imageId, $imageSizeW, $imageSizeH, substr($_REQUEST['extension'], 1));
-header("Content-type: image/" . $imageExt );
+header("Content-type: image/" . $imageExt);
 readfile($_SERVER['DOCUMENT_ROOT'] . $thumbpath);
