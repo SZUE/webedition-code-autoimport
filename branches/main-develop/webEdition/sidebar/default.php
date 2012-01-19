6fb57b58c713dfd8f7a5697d467f0d623dd292fe
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-01-19 00:01:29 +0000

    sidebar auf htmltop

--- branches/main-develop/webEdition/sidebar/default.php
+++ branches/main-develop/webEdition/sidebar/default.php
@@ -21,94 +21,78 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
 we_html_tools::protect();
-
-?><html>
-
-<head>
-	<title>webEdition</title>
-	<meta http-equiv="expires" content="0">
-	<meta http-equiv="pragma" content="no-cache">
-	<meta http-equiv="content-type" content="text/html; charset=<?php echo $GLOBALS['WE_BACKENDCHARSET']; ?>">
-
-	<link href="/webEdition/css/global.php?WE_LANGUAGE=<?php echo $GLOBALS["WE_LANGUAGE"]; ?>&amp;WE_BACKENDCHARSET=<?php echo $GLOBALS["WE_BACKENDCHARSET"]; ?>" rel="styleSheet" type="text/css" />
+we_html_tools::htmlTop('sideBar');
+?>
+<link href="/webEdition/css/global.php?WE_LANGUAGE=<?php echo $GLOBALS["WE_LANGUAGE"]; ?>&amp;WE_BACKENDCHARSET=<?php echo $GLOBALS["WE_BACKENDCHARSET"]; ?>" rel="styleSheet" type="text/css" />
 
 </head>
 <body class="weSidebarBody">
 
-<table>
-<?php
+	<table>
+		<?php
 
-	function showSidebarText(&$textArray) {
-		for($i = 0; $i < sizeof($textArray); $i++) {
-			$text = &$textArray[$i];
+		function showSidebarText(&$textArray){
+			for($i = 0; $i < sizeof($textArray); $i++){
+				$text = &$textArray[$i];
 
-			$link = "%s";
-			if(isset($text['link']) && $text['link'] != "") {
-				if(stripos($text['link'],'javascript:')===0) {
-					$link = "<a href=\"" . $text['link'] . "\">%s</a>";
-
-				} else {
-					$link = "<a href=\"" . $text['link'] . "\" target=\"_blank\">%s</a>";
-
+				$link = "%s";
+				if(isset($text['link']) && $text['link'] != ""){
+					if(stripos($text['link'], 'javascript:') === 0){
+						$link = "<a href=\"" . $text['link'] . "\">%s</a>";
+					} else{
+						$link = "<a href=\"" . $text['link'] . "\" target=\"_blank\">%s</a>";
+					}
 				}
 
-			}
+				$icon = "";
+				if(isset($text['icon']) && $text['icon'] != ""){
+					$icon = sprintf($link, "<img src=\"/webEdition/sidebar/img/" . $text['icon'] . "\" width=\"42\" height=\"42\" border=\"0\" />");
+				}
 
-			$icon = "";
-			if(isset($text['icon']) && $text['icon'] != "") {
-				$icon = sprintf($link, "<img src=\"/webEdition/sidebar/img/" . $text['icon'] . "\" width=\"42\" height=\"42\" border=\"0\" />");
-
-			}
-
-			$headline = "";
-			if(isset($text['headline']) && $text['headline'] != "") {
-				$headline = sprintf($link, $text['headline']);
-
-			}
-?>
-	<tr>
-		<td colspan="2"><?php we_html_tools::pPixel(1,5);?></td>
-	</tr>
-	<tr>
-<?php
-		if($icon == "") {
-?>
-		<td class="defaultfont" valign="top" colspan="2">
-			<strong><?php echo $headline; ?></strong><br />
-			<?php we_html_tools::pPixel(1,4);?>
-			<br />
-			<?php echo $text['text']; ?>
-		</td>
-<?php
-		} else {
-?>
-		<td class="defaultfont" valign="top" width="52"><?php echo $icon; ?></td>
-		<td class="defaultfont" valign="top">
-			<strong><?php echo $headline; ?></strong><br />
-			<?php we_html_tools::pPixel(1,4);?><br />
-			<?php echo $text['text']; ?>
-		</td>
-<?php
+				$headline = "";
+				if(isset($text['headline']) && $text['headline'] != ""){
+					$headline = sprintf($link, $text['headline']);
+				}
+				?>
+				<tr>
+					<td colspan="2"><?php we_html_tools::pPixel(1, 5); ?></td>
+				</tr>
+				<tr>
+				<?php
+				if($icon == ""){
+					?>
+						<td class="defaultfont" valign="top" colspan="2">
+							<strong><?php echo $headline; ?></strong><br />
+						<?php we_html_tools::pPixel(1, 4); ?>
+							<br />
+						<?php echo $text['text']; ?>
+						</td>
+			<?php
+		} else{
+			?>
+						<td class="defaultfont" valign="top" width="52"><?php echo $icon; ?></td>
+						<td class="defaultfont" valign="top">
+							<strong><?php echo $headline; ?></strong><br />
+						<?php we_html_tools::pPixel(1, 4); ?><br />
+						<?php echo $text['text']; ?>
+						</td>
+			<?php
 		}
-?>
-	</tr>
-<tr>
-<?php
-		}
+		?>
+				</tr>
+				<tr>
+					<?php
+				}
+			}
 
-	}
+			showSidebarText(g_l('sidebar', '[default]'));
 
-	showSidebarText(g_l('sidebar','[default]'));
-
-	if(we_hasPerm("ADMINISTRATOR")) {
-		showSidebarText(g_l('sidebar','[admin]'));
-
-	}
-
-?>
-</table>
+			if(we_hasPerm("ADMINISTRATOR")){
+				showSidebarText(g_l('sidebar', '[admin]'));
+			}
+			?>
+	</table>
 
 </body>
 </html>
