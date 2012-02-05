8f550d3ea7a7b24fe4118dd67839516dfabe8d8f
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-02-05 14:01:37 +0000

    Fix from merge

--- branches/main-develop/webEdition/we/include/we_editors/file_in_workspace_footer.inc.php
+++ branches/main-develop/webEdition/we/include/we_editors/file_in_workspace_footer.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * webEdition CMS
  *
@@ -21,26 +22,20 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
 include_once ($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we_inc_min.inc.php');
 
 
-	$_messageTbl = new we_html_table(	array(	"border"      => 0,
-											"cellpadding" => 0,
-											"cellspacing" => 0),
-									2,
-									4);
-	//	spaceholder
-	$_messageTbl->setColContent(0,0, we_html_tools::getPixel(20,7));
-	$_messageTbl->setColContent(1,1, we_html_element::htmlImg(array("src" => IMAGE_DIR . "alert.gif")));
-	$_messageTbl->setColContent(1,2, we_html_tools::getPixel(5,2));
-	$_messageTbl->setCol(1,3, array("class" => "defaultfont"), g_l('alert','['.FILE_TABLE.'][not_im_ws]'));
+$_messageTbl = new we_html_table(array("border" => 0, "cellpadding" => 0, "cellspacing" => 0), 2, 4);
+//	spaceholder
+$_messageTbl->setColContent(0, 0, we_html_tools::getPixel(20, 7));
+$_messageTbl->setColContent(1, 1, we_html_element::htmlImg(array("src" => IMAGE_DIR . "alert.gif")));
+$_messageTbl->setColContent(1, 2, we_html_tools::getPixel(5, 2));
+$_messageTbl->setCol(1, 3, array("class" => "defaultfont"), g_l('alert', '[' . FILE_TABLE . '][not_im_ws]'));
 
 
-	$_head = we_html_element::htmlHead(we_html_element::jsElement('top.toggleBusy(0);''));
-	$_body = we_html_element::htmlBody(	array(	"background" => "/webEdition/images/edit/editfooterback.gif",
-												"bgcolor"    => "white"),
-										$_messageTbl->getHtml());
+$_head = we_html_element::htmlHead(we_html_element::jsElement('top.toggleBusy(0);'));
+$_body = we_html_element::htmlBody(array('background' => IMAGE_DIR . 'edit/editfooterback.gif',
+		'bgcolor' => 'white'), $_messageTbl->getHtml());
 
 
-	print we_html_element::htmlHtml($_head . STYLESHEET . "\n" . $_body);
+print we_html_element::htmlHtml($_head . STYLESHEET . $_body);
