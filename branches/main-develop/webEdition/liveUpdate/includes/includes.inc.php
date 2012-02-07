12af2202a785e4a4df73a2508deeb54d89fca353
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-02-07 21:10:40 +0000

    update fix

--- branches/main-develop/webEdition/liveUpdate/includes/includes.inc.php
+++ branches/main-develop/webEdition/liveUpdate/includes/includes.inc.php
@@ -24,6 +24,7 @@
 
 define('LIVEUPDATE_DIR', $_SERVER['DOCUMENT_ROOT'] . '/webEdition/liveUpdate/');
 
+include_once ($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we.inc.php');
 require_once(LIVEUPDATE_DIR . 'classes/liveUpdateHttp.class.php');
 require_once(LIVEUPDATE_DIR . 'classes/liveUpdateResponse.class.php');
 require_once(LIVEUPDATE_DIR . 'classes/liveUpdateFrames.class.php');
