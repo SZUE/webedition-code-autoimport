498822d367ffc9b0c3e79bf8fb17cb715a3abe5c
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-02-08 19:56:16 +0000

    Fix 5937

--- branches/main-develop/webEdition/we/include/we_tags/we_tag_var.inc.php
+++ branches/main-develop/webEdition/we/include/we_tags/we_tag_var.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * webEdition CMS
  *
@@ -21,102 +22,84 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
 function we_tag_var($attribs, $content){
-	if (($foo = attributFehltError($attribs, "name", "var")))
+	if(($foo = attributFehltError($attribs, "name", "var")))
 		return $foo;
 	$docAttr = weTag_getAttribute("doc", $attribs);
 	$name = weTag_getAttribute("name", $attribs);
 	//$_name_orig=weTag_getAttribute("_name_orig", $attribs);
 	$type = weTag_getAttribute("type", $attribs);
-  $htmlspecialchars = weTag_getAttribute("htmlspecialchars", $attribs, false, true); // #3771
+	$htmlspecialchars = weTag_getAttribute("htmlspecialchars", $attribs, false, true); // #3771
 	$doc = we_getDocForTag($docAttr, false);
 
-	switch ($type) {
+	switch($type){
 		case "session" :
-		    $return = (isset($_SESSION[$name])) ? $_SESSION[$name] : "";
-		    if ($htmlspecialchars) $return = htmlspecialchars($return); // #3771
-			return $return;
+			$return = (isset($_SESSION[$name])) ? $_SESSION[$name] : "";
+			return $htmlspecialchars ? htmlspecialchars($return) : $return;
 		case "request" :
 			$return = removePHP(isset($_REQUEST[$name]) ? $_REQUEST[$name] : "");
-		    if ($htmlspecialchars) $return = htmlspecialchars($return); // #3771
-			return $return;
+			return $htmlspecialchars ? htmlspecialchars($return) : $return;
 		case "post" :
 			$return = removePHP(isset($_POST[$name]) ? $_POST[$name] : "");
-		    if ($htmlspecialchars) $return = htmlspecialchars($return); // #3771
-			return $return;
+			return $htmlspecialchars ? htmlspecialchars($return) : $return;
 		case "get" :
 			$return = removePHP(isset($_GET[$name]) ? $_GET[$name] : "");
-		    if ($htmlspecialchars) $return = htmlspecialchars($return); // #3771
-			return $return;
+			return $htmlspecialchars ? htmlspecialchars($return) : $return;
 		case "global" :
-		    $return = (isset($GLOBALS[$name])) ? $GLOBALS[$name] : "";
-		    if ($htmlspecialchars) $return = htmlspecialchars($return); // #3771
-			return $return;
+			$return = (isset($GLOBALS[$name])) ? $GLOBALS[$name] : "";
+			return $htmlspecialchars ? htmlspecialchars($return) : $return;
 		case 'multiobject' :
 			$data = unserialize($doc->getField($attribs, $type, true));
-			if (isset($data['objects']) && sizeof($data['objects']) > 0) {
-				$out = implode(",", $data['objects']);
-			} else {
-				$out = "";
+			if(isset($data['objects']) && sizeof($data['objects']) > 0){
+				return implode(",", $data['objects']);
+			} else{
+				return '';
 			}
-			return $out;
 
 		case "property" :
-			if (isset($GLOBALS['we_obj'])) {
+			if(isset($GLOBALS['we_obj'])){
 				return $GLOBALS['we_obj']->$name;
-			} else {
+			} else{
 				return $doc->$name;
 			}
 		case 'shopVat' :
-			if (defined('SHOP_TABLE')) {
+			if(defined('SHOP_TABLE')){
 
 				$vatId = $GLOBALS['we_doc']->getElement(WE_SHOP_VAT_FIELD_NAME);
 				return weShopVats::getVatRateForSite($vatId);
 			}
-		case 'link' : return $doc->getField($attribs, $type, false);break; // bugfix #3634
+			break;
+		case 'link' :
+			return $doc->getField($attribs, $type, false);
+		// bugfix #3634
 		default :
 			$normVal = $doc->getField($attribs, $type, true);
 			// bugfix 7557
 			// wenn die Abfrage im Aktuellen Objekt kein Erg?bnis liefert
 			// wird in den eingebundenen Objekten ?berpr?ft ob das Feld existiert
-			if ($type == "select" && $normVal == "") {
-				if (isset($doc->DefArray) && is_array($doc->DefArray)) {
-					foreach ($doc->DefArray as $_glob_key => $_val) {
+			if($type == "select" && $normVal == ""){
+				if(isset($doc->DefArray) && is_array($doc->DefArray)){
+					foreach($doc->DefArray as $_glob_key => $_val){
 
-						if (substr($_glob_key, 0, 7) == "object_") {
+						if(substr($_glob_key, 0, 7) == "object_"){
 
 							$normVal = we_document::getFieldByVal(
-									$doc->getElement($name),
-									$type,
-									$attribs,
-									false,
-									$GLOBALS['we_doc']->ParentID,
-									$GLOBALS['we_doc']->Path,
-									$GLOBALS['DB_WE'],
-									substr($_glob_key, 7));
+									$doc->getElement($name), $type, $attribs, false, $GLOBALS['we_doc']->ParentID, $GLOBALS['we_doc']->Path, $GLOBALS['DB_WE'], substr($_glob_key, 7));
 						}
 
-						if ($normVal != "")
+						if($normVal != "")
 							break;
 					}
-				} else {
+				} else{
 
-					if (isset($doc->elements) && is_array($doc->elements)) {
-						foreach ($doc->elements as $_glob_key => $_val) {
+					if(isset($doc->elements) && is_array($doc->elements)){
+						foreach($doc->elements as $_glob_key => $_val){
 
-							if (substr($_glob_key, 0, 10) == "we_object_") {
+							if(substr($_glob_key, 0, 10) == "we_object_"){
 								$normVal = we_document::getFieldByVal(
-										$doc->getElement($name),
-										$type,
-										$attribs,
-										false,
-										$GLOBALS['we_doc']->ParentID,
-										$GLOBALS['we_doc']->Path,
-										$GLOBALS['DB_WE'],
-										substr($_glob_key, 10));
+										$doc->getElement($name), $type, $attribs, false, $GLOBALS['we_doc']->ParentID, $GLOBALS['we_doc']->Path, $GLOBALS['DB_WE'], substr($_glob_key, 10));
 							}
-							if ($normVal != "")
+							if($normVal != "")
 								break;
 						}
 					}
@@ -128,5 +111,4 @@ function we_tag_var($attribs, $content){
 			return $normVal;
 	}
 	return $var;
-
 }
