05fa9af18b43c056d08d3d259dd9b973fc209a75
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-03-23 00:40:26 +0000

    eol

--- additional/scripts/eol-style/eol.sh
+++ additional/scripts/eol-style/eol.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 DIR=`pwd`/../../..
-find ${DIR} -name \*.php -exec svn propset svn:eol-style native {} \;
-find ${DIR} -name \*.html -exec svn propset svn:eol-style native {} \;
-find ${DIR} -name \*.js -exec svn propset svn:eol-style native {} \;
-find ${DIR} -name \*.java -exec svn propset svn:eol-style native {} \;
-find ${DIR} -name \*.xml -exec svn propset svn:eol-style native {} \;
+DIR=`readlink -f ${DIR}`
+
+find ${DIR} -name \*.php -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} -name \*.html -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} -name \*.js -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} -name \*.css -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} -name \*.java -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} -name \*.xml -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} -name .htaccess -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} -name \*.txt -exec svn propset svn:eol-style native {} \;|tee -a log.txt
