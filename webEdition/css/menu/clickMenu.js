d71106aa349517e428defe9c1c9f1620bb6a3a7a
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2012-05-21 09:33:31 +0000

    #6441

--- webEdition/css/menu/clickMenu.js
+++ webEdition/css/menu/clickMenu.js
@@ -1,31 +1,20 @@
-/* ================================================================
-This copyright notice must be untouched at all times.
-
-The original version of this scriptt and the associated (x)html
-is available at http://www.stunicholls.com/menu/simple.html
-Copyright (c) 2005-2007 Stu Nicholls. All rights reserved.
-This script and the associated (x)html may be modified in any
-way to fit your requirements.
-=================================================================== */
-
 var menuActive = false;
 
 function topMenuClick(elem){
 	var itemsState = (menuActive) ? "top_div" : "top_div click";
-	elem.className = itemsState;
-	var elemsDivTop = document.getElementById("nav").getElementsByTagName("DIV");
 	menuActive = (menuActive) ? false : true;
-	for (var x=0; x < elemsDivTop.length; x++){
-		elemsDivTop[x].className = itemsState;
+	var liElems = elem.parentNode.parentNode.childNodes;
+	for (var i = 0; i < liElems.length; i++){
+		liElems[i].firstChild.className = itemsState;
 	}
 }
 
-function topMenuHover(elem){
-	var left = elem.getElementsByTagName("DIV")[0].getElementsByTagName("UL")[0].offsetLeft;
+function topMenuHover(elem){ //<li top
+	var left = elem.firstChild.childNodes[1].offsetLeft;
+	var liElems = elem.parentNode.childNodes;	
 	if(left < -1000) {
-		var elemsDivTop = document.getElementById("nav").getElementsByTagName("DIV");
-		for (var x = 0; x < elemsDivTop.length; x++){
-			elemsDivTop[x].className = "top_div";
+		for (var i = 0; i < liElems.length; i++){
+			liElems[i].firstChild.className = "top_div";
 		}
 		menuActive = false;
 	}
--- webEdition/css/menu/clickMenu_IE8.js
+++ webEdition/css/menu/clickMenu_IE8.js
@@ -1,13 +1,3 @@
-/* ================================================================
-This copyright notice must be untouched at all times.
-
-The original version of this scriptt and the associated (x)html
-is available at http://www.stunicholls.com/menu/simple.html
-Copyright (c) 2005-2007 Stu Nicholls. All rights reserved.
-This script and the associated (x)html may be modified in any
-way to fit your requirements.
-=================================================================== */
-
 var menuActive = false;
 
 function topMenuClick(elem){
@@ -20,11 +10,11 @@ function topMenuClick(elem){
 }
 
 function topMenuHover(elem){
-	var left = elem.getElementsByTagName("DIV")[0].getElementsByTagName("UL")[0].offsetLeft;
+	var left = elem.firstChild.childNodes[1].offsetLeft;
+	var liElems = elem.parentNode.childNodes;	
 	if(left < -1000) {
-		var elemsDivTop = document.getElementById("nav").getElementsByTagName("DIV");
-		for (var x = 0; x < elemsDivTop.length; x++){
-			elemsDivTop[x].className = "top_div";
+		for (var i = 0; i < liElems.length; i++){
+			liElems[i].firstChild.className = "top_div";
 		}
 		menuActive = false;
 	}
