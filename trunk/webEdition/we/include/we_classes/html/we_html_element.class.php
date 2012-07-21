469bdfd5632efdfcad81b5e012a0e587c9575f13
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-07-21 09:40:52 +0000

    Miniänderung

--- trunk/webEdition/we/include/we_classes/html/we_html_element.class.php
+++ trunk/webEdition/we/include/we_classes/html/we_html_element.class.php
@@ -400,4 +400,11 @@ abstract class we_html_element{
 		return $url . (strstr($url, '?') ? '&amp;' : '?') . $cache;
 	}
 
+	static function htmlIFrame($name, $src, $style){
+		return self::htmlDiv(array('style' => $style, 'name' => $name . 'Div')
+				, we_baseElement::getHtmlCode(
+					new we_baseElement('iframe', true, array('name' => $name, 'frameBorder' => 0, 'src' => $src, 'style' => 'border:0px;width:100%;height:100%;overflow: hidden;'))
+				));
+	}
+
 }
