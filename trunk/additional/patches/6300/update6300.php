0ba1282669098e1f5cfc4c95d3d8d72058bbdbfd
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2012-07-26 11:33:55 +0000

    1und1 Menü

--- trunk/additional/patches/6300/update6300.php
+++ trunk/additional/patches/6300/update6300.php
@@ -9,6 +9,7 @@ if(!is_readable("../../we/include/conf/we_conf.inc.php")) {
 }
 //die Vorbilder includen mehr, aber wozu?
 include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we.inc.php");
+include_once(WE_INCLUDES_PATH."we_classes/base/weFile.class.php");
 
 function up6300_updatePrefs(){
 	$db = new DB_WE();
