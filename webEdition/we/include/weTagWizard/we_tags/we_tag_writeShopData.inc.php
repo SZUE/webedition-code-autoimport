0406a75cc949e1ee2c2ae19b4ce09a278e4329e9
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-08-07 20:29:41 +0000

    Fix #6655

--- webEdition/we/include/weTagWizard/we_tags/we_tag_writeShopData.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_writeShopData.inc.php
@@ -8,6 +8,7 @@ $this->Module = 'shop';
 $this->Attributes[] = new weTagData_textAttribute('shopname', true, '');
 $this->Attributes[] = new weTagData_textAttribute('pricename', true, '');
 $this->Attributes[] = new weTagData_selectAttribute('netprices', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
+$this->Attributes[] = new weTagData_selectAttribute('usevat', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
 $this->Attributes[] = new weTagData_textAttribute('shipping', false, '');
 $this->Attributes[] = new weTagData_textAttribute('shippingisnet', false, '');
 $this->Attributes[] = new weTagData_textAttribute('shippingvatrate', false, '');
