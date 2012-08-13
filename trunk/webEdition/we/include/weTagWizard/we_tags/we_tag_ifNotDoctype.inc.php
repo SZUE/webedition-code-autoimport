4016b07d68196b6be98aebd142bebc12a28e669c
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-08-13 18:55:26 +0000

    Fix #6798

--- trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotDoctype.inc.php
+++ trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotDoctype.inc.php
@@ -6,3 +6,6 @@ $this->NeedsEndTag = true;
 $this->Groups[] = 'if_tags';
 //$this->Module = '';
 $this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
+
+$this->Attributes[] = new weTagData_textAttribute('doctypes', true, '');
+$this->Attributes[] = new weTagData_selectAttribute('doc', array(new weTagDataOption('top', false, ''), new weTagDataOption('self', false, ''), new weTagDataOption('listview', false, '')), false, '');
