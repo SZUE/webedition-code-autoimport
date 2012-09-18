7f5e738bd14577e59e660648ebdf28085f87fabc
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2012-09-18 10:14:15 +0000

    #6865 (Teil 2: Shop-Modul)

--- webEdition/we/include/we_modules/shop/edit_shop_properties.php
+++ webEdition/we/include/we_modules/shop/edit_shop_properties.php
@@ -881,7 +881,7 @@ if(isset($_REQUEST["SendMail"])){
 foreach(weShopStatusMails::$StatusFields as $field){
 	if(isset($_REQUEST[$field])){
 		list($year, $month, $day) = explode('.', $_REQUEST[$field]);
-		$DateOrder = $year . "-" . $month . "-" . $day . " 00:00:00";
+		$DateOrder = $day . "-" . $month . "-" . $year . " 00:00:00";
 		$GLOBALS['DB_WE']->query('UPDATE ' . SHOP_TABLE . ' SET ' . $field . '="' . $GLOBALS['DB_WE']->escape($DateOrder) . '" WHERE IntOrderID = ' . intval($_REQUEST["bid"]));
 		$weShopStatusMails->checkAutoMailAndSend(substr($field, 4), $_REQUEST["bid"], $_customer);
 	}
