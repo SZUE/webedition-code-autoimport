e43446ada62b38a5b3027512e0fb41f603356782
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-10-30 22:35:39 +0000

    Fix #5368

--- trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_conditionAdd.inc.php
+++ trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_conditionAdd.inc.php
@@ -8,7 +8,7 @@ $this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
 
 $this->Attributes[] = new weTagData_textAttribute('field', true, '');
 $this->Attributes[] = new weTagData_textAttribute('value', false, '');
-$this->Attributes[] = new weTagData_choiceAttribute('compare', array(new weTagDataOption('=', false, ''), new weTagDataOption('!=', false, ''), new weTagDataOption('&lt;', false, ''), new weTagDataOption('&gt;', false, ''), new weTagDataOption('&lt;=', false, ''), new weTagDataOption('&gt;=', false, ''), new weTagDataOption('like', false, '')), false,false, '');
+$this->Attributes[] = new weTagData_choiceAttribute('compare', array(new weTagDataOption('=', false, ''), new weTagDataOption('!=', false, ''), new weTagDataOption('&lt;', false, ''), new weTagDataOption('&gt;', false, ''), new weTagDataOption('&lt;=', false, ''), new weTagDataOption('&gt;=', false, ''), new weTagDataOption('LIKE', false, '')), false,false, '');
 $this->Attributes[] = new weTagData_textAttribute('var', false, '');
 $this->Attributes[] = new weTagData_selectAttribute('type', array(new weTagDataOption('global', false, ''), new weTagDataOption('request', false, ''), new weTagDataOption('sessionfield', false, ''), new weTagDataOption('document', false, ''), new weTagDataOption('now', false, '')), false, '');
 $this->Attributes[] = new weTagData_selectAttribute('property', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
