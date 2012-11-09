449d3c3b7ed7cede6ad46e38d3a84586720d467c
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2012-11-09 05:21:16 +0000

    Bugs #6998 und #6997 Nachtrag

--- webEdition/lib/we/ui/controls/SelectObjectfield.php
+++ webEdition/lib/we/ui/controls/SelectObjectfield.php
@@ -95,7 +95,7 @@ class we_ui_controls_SelectObjectfield extends we_ui_controls_Select{
 	 * @return string
 	 */
 	public function getOptionsHTML(){
-		include (WE_INCLUDES_PATH . 'we_active_integrated_modules.inc.php');
+		include ($_SERVER['DOCUMENT_ROOT'] . "/webEdition/we/include/conf/we_active_integrated_modules.inc.php");
 		if(in_array('object', $GLOBALS['_we_active_integrated_modules'])){
 
 			if(file_exists(WE_MODULES_PATH . 'object/we_conf_object.inc.php')){
