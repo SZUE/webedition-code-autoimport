e43b0c97a2c0c28e74f33ba05f5608ab3a982031
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2012-11-19 12:31:31 +0000

    Bug #7024

--- webEdition/we/include/we_modules/object/we_object.inc.php
+++ webEdition/we/include/we_modules/object/we_object.inc.php
@@ -486,7 +486,7 @@ class we_object extends we_document{
 
 		////// resave the line O to O.....
 		$this->DB_WE->query('DELETE FROM ' . $ctable . ' WHERE OF_ID=0 OR ID=0');
-		$this->DB_WE->query('INSERT INTO ' . $ctable . ' OF_ID=0');
+		$this->DB_WE->query('INSERT INTO ' . $ctable . ' SET OF_ID=0');
 		////// resave the line O to O.....
 
 		unset($this->elements);
