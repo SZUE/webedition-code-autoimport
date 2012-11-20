7aaf16ac0027a362849bc79b3f511c29c3c868d6
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-11-20 22:00:21 +0000

    Feature #6484

--- webEdition/we/include/weTagWizard/we_tags/we_tag_ifFieldEmpty.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_ifFieldEmpty.inc.php
@@ -8,4 +8,6 @@ $this->Groups[] = 'if_tags';
 $this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
 
 $this->Attributes[] = new weTagData_textAttribute('match', true, '');
-$this->Attributes[] = new weTagData_selectAttribute('type', array(new weTagDataOption('img', false, ''), new weTagDataOption('flashmovie', false, ''), new weTagDataOption('quicktime', false, ''), new weTagDataOption('binary', false, ''), new weTagDataOption('href', false, ''), new weTagDataOption('object', false, ''), new weTagDataOption('multiobject', false, ''), new weTagDataOption('calendar', false, ''), new weTagDataOption('checkbox', false, '')), false, '');
+$this->Attributes[] = new weTagData_selectAttribute('type', array(new weTagDataOption('img', false, ''), new weTagDataOption('flashmovie', false, ''), new weTagDataOption('quicktime', false, ''), new weTagDataOption('binary', false, ''), new weTagDataOption('href', false, ''), new weTagDataOption('object', false, ''), new weTagDataOption('multiobject', false, ''), new weTagDataOption('calendar', false, ''), new weTagDataOption('checkbox', false, ''),
+ new weTagDataOption('int', false, ''), new weTagDataOption('float', false, '')
+), false, '');
