4e9f0e22b502365e187102edcfb51486fc78d154
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-11-24 11:54:10 +0000

    mofifier added

--- trunk/webEdition/we/include/we_classes/database/we_database_base.class.php
+++ trunk/webEdition/we/include/we_classes/database/we_database_base.class.php
@@ -605,16 +605,14 @@ abstract class we_database_base{
 		if(!$this->_connect()){
 			return false;
 		}
-		$query = '';
 		if(is_array($table)){
+			$query = array();
 			foreach($table as $key => $value){
-				if(is_numeric($key)){
-					$query.= $value . ' ' . $mode . ',';
-				} else{
-					$query.= $key . ' ' . $value . ',';
-				}
+				$query[] = (is_numeric($key) ?
+						$value . ' ' . $mode :
+						$key . ' ' . $value);
 			}
-			$query = substr($query, 0, -1);
+			$query = impode(',', $query);
 		} else{
 			$query = $table . ' ' . $mode;
 		}
@@ -666,7 +664,7 @@ abstract class we_database_base{
 		printf("<b>MySQL Error</b>: %s (%s)<br>\n", $this->Errno, $this->Error);
 	}
 
-	function isColExist($tab, $col){
+	public function isColExist($tab, $col){
 		if($tab == '' || $col == ''){
 			return false;
 		}
@@ -674,7 +672,7 @@ abstract class we_database_base{
 		return (bool) count(getHash('SHOW COLUMNS FROM ' . $this->escape($tab) . ' LIKE "' . $col . '"', $this));
 	}
 
-	function isTabExist($tab){
+	public function isTabExist($tab){
 		if($tab == ''){
 			return false;
 		}
@@ -682,7 +680,7 @@ abstract class we_database_base{
 		return ($this->next_record());
 	}
 
-	function addTable($tab, $cols, $keys = array()){
+	public function addTable($tab, $cols, $keys = array()){
 		if(!is_array($cols) || !count($cols)){
 			return;
 		}
@@ -690,7 +688,7 @@ abstract class we_database_base{
 		foreach($cols as $name => $type){
 			$cols_sql[] = "`" . $name . "` " . $type;
 		}
-		if(count($keys)){
+		if(!empty($keys)){
 			foreach($keys as $key){
 				$cols_sql[] = $key;
 			}
@@ -707,11 +705,11 @@ abstract class we_database_base{
 		return $this->query('CREATE TABLE ' . $this->escape($tab) . ' (' . implode(',', $cols_sql) . ') ENGINE = MYISAM ' . $charset_collation . ';');
 	}
 
-	function delTable($tab){
+	public function delTable($tab){
 		$this->query('DROP TABLE IF EXISTS ' . $this->escape($tab));
 	}
 
-	function addCol($tab, $col, $typ, $pos = ''){
+	public function addCol($tab, $col, $typ, $pos = ''){
 		$col = trim($col, '`');
 		if($this->isColExist($tab, $col)){
 			return false;
@@ -719,7 +717,7 @@ abstract class we_database_base{
 		return $this->query('ALTER TABLE ' . $this->escape($tab) . ' ADD `' . $col . '` ' . $typ . (($pos != '') ? ' ' . $pos : ''));
 	}
 
-	function changeColType($tab, $col, $newtyp){
+	public function changeColType($tab, $col, $newtyp){
 		$col = trim($col, '`');
 		if(!$this->isColExist($tab, $col)){
 			return false;
@@ -728,11 +726,11 @@ abstract class we_database_base{
 		return $this->query('ALTER TABLE ' . $this->escape($tab) . ' CHANGE `' . $col . '` `' . $col . '` ' . $newtyp);
 	}
 
-	function getColTyp($tab, $col){
+	public function getColTyp($tab, $col){
 		return f('SHOW COLUMNS FROM ' . $this->escape($tab) . ' LIKE "' . $col . '"', 'Type', $this);
 	}
 
-	function delCol($tab, $col){
+	public function delCol($tab, $col){
 		if(!$this->isColExist($tab, $col)){
 			return;
 		}
@@ -746,7 +744,7 @@ abstract class we_database_base{
 			false;
 	}
 
-	function getTableKeyArray($tab){
+	public function getTableKeyArray($tab){
 		$myarray = array();
 		$zw = $this->getTableCreateArray($tab);
 		if(!$zw){
@@ -770,7 +768,7 @@ abstract class we_database_base{
 	 * @param string $key full key definition what is used in a create statement
 	 * @return string|boolean extracted key name
 	 */
-	function isKeyExistAtAll($tab, $key){
+	public function isKeyExistAtAll($tab, $key){
 		$matches = array();
 		preg_match('|.*KEY *`?([^( `]*)`? \(|', $key, $matches);
 		$key = $matches[1];
@@ -792,7 +790,7 @@ abstract class we_database_base{
 	 * @param string $key full key definition what is used in a create statement
 	 * @return boolean true, if the exact definition is met, false otherwise
 	 */
-	function isKeyExist($tab, $key){
+	public function isKeyExist($tab, $key){
 		$zw = $this->getTableCreateArray($tab);
 		if($zw){
 			foreach($zw as $v){
@@ -808,7 +806,7 @@ abstract class we_database_base{
 	 * @param string $tab tablename
 	 * @param string $fullKey full key definition what is used in a create statement
 	 */
-	function addKey($tab, $fullKey){
+	public function addKey($tab, $fullKey){
 		$this->query('ALTER TABLE ' . $this->escape($tab) . ' ADD ' . $fullKey);
 	}
 
@@ -817,7 +815,7 @@ abstract class we_database_base{
 	 * @param string $tab tablename
 	 * @param string $keyname ONLY the keyname is wanted here
 	 */
-	function delKey($tab, $keyname){
+	public function delKey($tab, $keyname){
 		$this->query('ALTER TABLE ' . $this->escape($tab) . ' DROP ' . ($keyname == 'PRIMARY' ? 'PRIMARY KEY' : 'INDEX `' . $keyname . '`'));
 	}
 
@@ -827,7 +825,7 @@ abstract class we_database_base{
 	 * @param string $oldcol old col-name
 	 * @param string $newcol new col-name
 	 */
-	function renameCol($tab, $oldcol, $newcol){
+	public function renameCol($tab, $oldcol, $newcol){
 		$this->query('ALTER TABLE ' . $this->escape($tab) . ' CHANGE `' . $oldcol . '` `' . $newcol . '`');
 	}
 
@@ -837,7 +835,7 @@ abstract class we_database_base{
 	 * @param string $colName the name of the col to move
 	 * @param string $newPos the new position (possible: FIRST, AFTER colname)
 	 */
-	function moveCol($tab, $colName, $newPos){
+	public function moveCol($tab, $colName, $newPos){
 		//get the old col def, use for alter table.
 		$zw = $this->getTableCreateArray($tab);
 		if(!$zw){
