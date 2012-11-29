6cbab74475f84f9a7ce3ed02b1d2ee22e67f7642
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-11-29 10:59:24 +0000

    new lang files

--- webEdition/we/include/we_language/French/dynamicControls.inc.php
+++ webEdition/we/include/we_language/French/dynamicControls.inc.php
@@ -1,6 +1,6 @@
 <?php
 /** Generated language file of webEdition CMS */
 $l_dynamicControls=array(
-	'expand_group'=>'Click here to expand this group ...',
-	'fold_group'=>'Cliquez ici, pour fermer ce groupe ...',
+	'expand_group'=>'Cliquez ici pour ouvrir ce groupe ...',
+	'fold_group'=>'Cliquez ici pour fermer ce groupe ...',
 );
