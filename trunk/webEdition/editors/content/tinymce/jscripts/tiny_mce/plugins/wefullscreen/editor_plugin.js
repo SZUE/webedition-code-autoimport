ead2dd6155eca0e4947675121e4c816d2967b277
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-12-04 17:22:27 +0000

    delete unneeded files

--- trunk/webEdition/editors/content/tinymce/jscripts/tiny_mce/plugins/wefullscreen/editor_plugin.js
+++ trunk/webEdition/editors/content/tinymce/jscripts/tiny_mce/plugins/wefullscreen/editor_plugin.js
@@ -1,1 +1,73 @@
-(function(){tinymce.create('tinymce.plugins.WefullscreenPlugin',{init:function(d,e){d.addCommand('mceWefullscreen',function(){var a=d.selection;d.windowManager.open({file:e+'/../../../../../wysiwyg/fullscreenEditDialog.php?we_dialog_args[editor]=tinyMce'+'&we_dialog_args[editname]=tinyMce'+'&we_dialog_args[outsideWE]='+wefullscreenVars["outsideWE"]+'&we_dialog_args[xml]='+wefullscreenVars["xml"]+'&we_dialog_args[removeFirstParagraph]='+wefullscreenVars["removeFirstParagraph"]+'&we_dialog_args[baseHref]='+wefullscreenVars["baseHref"]+'&we_dialog_args[charset]='+wefullscreenVars["charset"]+'&we_dialog_args[cssClasses]='+wefullscreenVars["cssClasses"]+'&we_dialog_args[bgcolor]='+wefullscreenVars["bgcolor"]+'&we_dialog_args[language]='+wefullscreenVars["language"]+'&we_dialog_args[screenWidth]='+wefullscreenVars["screenWidth"]+'&we_dialog_args[screenHeight]='+wefullscreenVars["screenHeight"]+'&we_dialog_args[className]='+wefullscreenVars["className"]+'&we_dialog_args[fontnames]='+wefullscreenVars["fontnames"]+'&we_dialog_args[propString]='+wefullscreenVars["propString"],width:screen.availWidth-20,height:screen.availHeight-70,inline:1},{plugin_url:e,})});d.addButton('wefullscreen',{title:tinyMceGL.wefullscreen.tooltip,cmd:'mceWefullscreen',image:e+'/img/fullscreen.gif'});d.onNodeChange.add(function(a,b,n,c){b.setDisabled('wefullscreen',false)})},getInfo:function(){return{longname:'Wefullscreen plugin',author:'webEdition e.V',authorurl:'http://www.webedition.org',infourl:'http://www.webedition.org'}}});tinymce.PluginManager.add('wefullscreen',tinymce.plugins.WefullscreenPlugin)})();
+(function() {
+
+	tinymce.create('tinymce.plugins.WefullscreenPlugin', {
+
+		init : function(ed, url) {
+			ed.addCommand('mceWefullscreen', function() {
+				var se = ed.selection;
+				ed.windowManager.open({
+
+					file : url + '/../../../../../wysiwyg/fullscreenEditDialog.php?we_dialog_args[editor]=tinyMce' +
+						'&we_dialog_args[editname]=tinyMce' +
+						'&we_dialog_args[outsideWE]='+ wefullscreenVars["outsideWE"] +
+						'&we_dialog_args[xml]='+ wefullscreenVars["xml"] +
+						'&we_dialog_args[removeFirstParagraph]='+ wefullscreenVars["removeFirstParagraph"] +
+						'&we_dialog_args[baseHref]='+ wefullscreenVars["baseHref"] +
+						'&we_dialog_args[charset]='+ wefullscreenVars["charset"] +
+						'&we_dialog_args[cssClasses]='+ wefullscreenVars["cssClasses"] +
+						'&we_dialog_args[bgcolor]='+ wefullscreenVars["bgcolor"] +
+						'&we_dialog_args[language]='+ wefullscreenVars["language"] +
+						'&we_dialog_args[screenWidth]='+ wefullscreenVars["screenWidth"] +
+						'&we_dialog_args[screenHeight]='+ wefullscreenVars["screenHeight"] +
+						'&we_dialog_args[className]='+ wefullscreenVars["className"] +
+						'&we_dialog_args[fontnames]='+ wefullscreenVars["fontnames"] +
+						'&we_dialog_args[propString]='+ wefullscreenVars["propString"],
+
+					width : screen.availWidth-20,
+					height : screen.availHeight - 70,
+					inline : 1
+				}, {
+
+					plugin_url : url, // Plugin absolute URL
+				});
+
+				//ed.execCommand("mceSetContent", false, "das ist der text");
+
+			});
+
+			// Register wefullscreen button
+			ed.addButton('wefullscreen', {
+				title : tinyMceGL.wefullscreen.tooltip,
+				cmd : 'mceWefullscreen',
+				image : url + '/img/fullscreen.gif'
+			});
+
+			// Add a node change handler, selects the button in the UI when a image is selected
+			ed.onNodeChange.add(function(ed, cm, n, co) {
+				cm.setDisabled('wefullscreen', false);
+			});
+		},
+
+
+
+		/**
+		 * Returns information about the plugin as a name/value array.
+
+		 * The current keys are longname, author, authorurl, infourl and version.
+		 * @return {Object} Name/value array containing information about the plugin.
+		 */
+
+		getInfo : function() {
+			return {
+				longname : 'Wefullscreen plugin',
+				author : 'webEdition e.V',
+				authorurl : 'http://www.webedition.org',
+				infourl : 'http://www.webedition.org'
+				//version : tinymce.majorVersion + "." + tinymce.minorVersion
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('wefullscreen', tinymce.plugins.WefullscreenPlugin);
+})();
--- trunk/webEdition/editors/content/tinymce/jscripts/tiny_mce/plugins/wespellchecker/editor_plugin.js
+++ trunk/webEdition/editors/content/tinymce/jscripts/tiny_mce/plugins/wespellchecker/editor_plugin.js
@@ -1,1 +1,53 @@
-(function(){tinymce.create('tinymce.plugins.WespellcheckerPlugin',{init:function(d,e){d.addCommand('mceWespellchecker',function(){var a=d.selection;d.windowManager.open({file:e+'/../../../../../wysiwyg/spellcheck.php?we_dialog_args[editor]=tinyMce&we_dialog_args[editname]=tinyMce',width:500+parseInt(d.getLang('wespellchecker.delta_width',0)),height:490+parseInt(d.getLang('wespellchecker.delta_height',0)),inline:1},{plugin_url:e,some_custom_arg:'custom arg'})});d.addButton('wespellchecker',{title:tinyMceGL.wespellchecker.tooltip,cmd:'mceWespellchecker',image:e+'/img/spellcheck.gif'});d.onNodeChange.add(function(a,b,n,c){b.setDisabled('wespellchecker',false)})},getInfo:function(){return{longname:'Wespellchecker plugin',author:'webEdition e.V',authorurl:'http://www.webedition.org',infourl:'http://www.webedition.org'}}});tinymce.PluginManager.add('wespellchecker',tinymce.plugins.WespellcheckerPlugin)})();
+(function() {
+
+	tinymce.create('tinymce.plugins.WespellcheckerPlugin', {
+
+		init : function(ed, url) {
+			ed.addCommand('mceWespellchecker', function() {
+
+				var se = ed.selection;
+
+				ed.windowManager.open({
+					file : url + '/../../../../../wysiwyg/spellcheck.php?we_dialog_args[editor]=tinyMce&we_dialog_args[editname]=tinyMce',
+					width : 500 + parseInt(ed.getLang('wespellchecker.delta_width', 0)),
+					height : 490 + parseInt(ed.getLang('wespellchecker.delta_height', 0)),
+					inline : 1
+				}, {
+					plugin_url : url, // Plugin absolute URL
+					some_custom_arg : 'custom arg' // Custom argument
+				});
+			});
+
+			// Register wespellchecker button
+			ed.addButton('wespellchecker', {
+				title : tinyMceGL.wespellchecker.tooltip,
+				cmd : 'mceWespellchecker',
+				image : url + '/img/spellcheck.gif'
+			});
+
+			// Add a node change handler, selects the button in the UI when a image is selected
+			ed.onNodeChange.add(function(ed, cm, n, co) {
+				cm.setDisabled('wespellchecker', false);
+			});
+		},
+
+		/**
+		 * Returns information about the plugin as a name/value array.
+		 * The current keys are longname, author, authorurl, infourl and version.
+		 *
+		 * @return {Object} Name/value array containing information about the plugin.
+		 */
+		getInfo : function() {
+			return {
+				longname : 'Wespellchecker plugin',
+				author : 'webEdition e.V',
+				authorurl : 'http://www.webedition.org',
+				infourl : 'http://www.webedition.org'
+				//version : tinymce.majorVersion + "." + tinymce.minorVersion
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('wespellchecker', tinymce.plugins.WespellcheckerPlugin);
+})();
