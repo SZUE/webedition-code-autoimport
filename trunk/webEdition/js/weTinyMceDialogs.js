64d71b57cb64bba7a591d1fa2e361fc4fde75dfa
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2012-12-05 09:30:33 +0000

    tinyMce: fehlende Datei

---
+++ trunk/webEdition/js/weTinyMceDialogs.js
@@ -0,0 +1,33 @@
+/**
+ * webEdition CMS
+ *
+ * $Rev: 5112 $
+ * $Author: mokraemer $
+ * $Date: 2012-11-09 20:02:22 +0100 (Fr, 09 Nov 2012) $
+ *
+ * This source is part of webEdition CMS. webEdition CMS is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the textfile
+ * webEdition/licenses/webEditionCMS/License.txt
+ *
+ * @category   webEdition
+ * @package    webEdition_base
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL
+ */
+
+var tinyMceDialog = null;
+
+function weRegisterTinyMcePopup(win){
+	if(tinyMceDialog !== null){
+		try{
+			tinyMceDialog.close();
+		}catch(err){}
+	} 
+	tinyMceDialog = win;
+}
