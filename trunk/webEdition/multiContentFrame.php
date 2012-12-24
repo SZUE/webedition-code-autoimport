e8126036041a7af8c594036f8d67282238c98785
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-12-24 17:34:38 +0000

    iframe entfernt

--- trunk/webEdition/multiContentFrame.php
+++ trunk/webEdition/multiContentFrame.php
@@ -47,7 +47,6 @@ we_html_tools::htmlTop();
 
 	function startMultiEditor() {
 		we_cmd('start_multi_editor'<?php print $_cmd_string; ?>);
-
 	}
 	//-->
 </script>
@@ -58,7 +57,7 @@ include(WEBEDITION_PATH . 'multiEditor/EditorFrameController.inc.php');
 <body onresize="setFrameSize()">
 	<div style="position:absolute;top:0px;bottom:0px;right:0px;left:0px;overflow: hidden;background-color: white;">
 		<div style="position:absolute;top:0px;height:22px;width:100%;background-color: Silver;" id="multiEditorDocumentTabsFrameDiv">
-			<?php include(WEBEDITION_PATH.'multiEditor/multiTabs.php');?>
+			<?php include(WEBEDITION_PATH.'multiEditor/multiTabs.inc.php');?>
 		</div>
 		<div style="position:absolute;top:22px;bottom:0px;left:0px;right:0px;overflow: hidden;" id="multiEditorEditorFramesetsDiv">
 			<iframe frameBorder="0" src="<?php print WEBEDITION_DIR ?>multiEditor/multiEditorFrameset.php" onload="startMultiEditor()" name="multiEditorEditorFramesets" style="border:0px;width:100%;height:100%;overflow: hidden;"></iframe>
