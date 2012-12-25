e7b60b7db2af7b83a23208d51118a5432974ee60
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-12-25 00:36:26 +0000

    Nachtrag multieditor

--- trunk/webEdition/multiContentFrame.php
+++ trunk/webEdition/multiContentFrame.php
@@ -57,11 +57,16 @@ include(WEBEDITION_PATH . 'multiEditor/EditorFrameController.inc.php');
 <body onresize="setFrameSize()">
 	<div style="position:absolute;top:0px;bottom:0px;right:0px;left:0px;overflow: hidden;background-color: white;">
 		<div style="position:absolute;top:0px;height:22px;width:100%;background-color: Silver; border-top: 1px solid #000000;" id="multiEditorDocumentTabsFrameDiv">
-			<?php include(WEBEDITION_PATH.'multiEditor/multiTabs.inc.php');?>
+			<?php include(WEBEDITION_PATH . 'multiEditor/multiTabs.inc.php'); ?>
 		</div>
-		<div style="position:absolute;top:22px;bottom:0px;left:0px;right:0px;overflow: hidden;" id="multiEditorEditorFramesetsDiv">
-			<?php include(WEBEDITION_PATH.'multiEditor/multiEditorFrameset.inc.php');?>
+		<div style="position:absolute;top:22px;bottom:0px;left:0px;right:0px;overflow: hidden;" id="multiEditorEditorFramesetsDiv"><?php
+			$count = (isset($_SESSION) && isset($_SESSION['weS']['we_mode']) && $_SESSION['weS']['we_mode'] == 'seem') ? 1 : 16;
 
+			for($i = 0; $i < $count; $i++){
+				//'overflow:hidden;' removed to fix bug #6540
+				echo '	<iframe frameBorder="0" style="' . ($i == 0 ? '' : 'display:none;') . 'margin:0px;border:0px;width:100%;height:100%;" src="' . HTML_DIR . 'blank_editor.html" name="multiEditFrame_' . $i . '" id="multiEditFrame_' . $i . '"  noresize ></iframe>';
+			}
+			?>
 		</div>
 	</div>
 	<?php
