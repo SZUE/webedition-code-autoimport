56ddcf8f5b4ca724560b2e817b6fab7e6bf12158
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2012-12-31 15:31:49 +0000

    new lang files

--- trunk/webEdition/we/include/we_language/Dutch/buttons/modules/schedule.inc.php
+++ trunk/webEdition/we/include/we_language/Dutch/buttons/modules/schedule.inc.php
@@ -1,6 +1,11 @@
 <?php
 /** Generated language file of webEdition CMS */
 $l_buttons_modules_schedule=array(
+	'saveInScheduler'=>array(
+		'alt'=>'Document is saved in the schedule table and will be published by the scheduler.',
+		'value'=>'Save in Scheduler',
+		'width'=>150,
+	),
 	'schedule_button'=>array(
 		'alt'=>'Geplande acties',
 		'value'=>'Planner',
