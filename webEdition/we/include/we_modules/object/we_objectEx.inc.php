804bd757e499091aa53de6536ac05430fbdc2085
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2013-01-02 04:46:43 +0000

    Bessere Fehlermeldung in we_ObjectEx

--- webEdition/we/include/we_modules/object/we_objectEx.inc.php
+++ webEdition/we/include/we_modules/object/we_objectEx.inc.php
@@ -454,7 +454,7 @@ class we_objectEx extends we_object{
 					$this->DB_WE->query('ALTER TABLE ' . $ctable . ' MODIFY COLUMN ' . $ovalname . ' ' . $type . ' AFTER ' . $last);
 					$last = $ovalname;
 				} else{
-					t_e('Field not found: Table ' . $this->Text . ' Field: ' . $ovalname);
+					t_e('warning', 'we_ObjectEx::setOrder '.$ctable.' Field not found: Field: ' . $ovalname);
 				}
 			}
 		}
@@ -473,8 +473,14 @@ class we_objectEx extends we_object{
 				}
 			}	
 			if(count($neworder)!= count($consider)){
-				t_e('warning', 'we_ObjectEx::setOrder: Order-Array ('.count($neworder).': '.implode(',',$neworder).') has different length than Fields-Array('.count($consider).': '.implode(',',$consider).') ');
-			} else {
+if(count($neworder)> count($consider)){
+					$thedifference= array_diff($neworder,$consider);
+					t_e('warning', 'we_ObjectEx::setOrder: '.$ctable.' Order-Array ('.count($neworder).') has larger length than generated Fields Array ('.count($consider).'), Missing: ('.implode(',',$thedifference).') Order-Array:('.implode(',',$neworder).') Fields-Array:('.implode(',',$consider).') ');
+				} else {
+					$thedifference= array_diff($consider,$neworder);
+					t_e('warning', 'we_ObjectEx::setOrder: '.$ctable.' Order-Array ('.count($neworder).') has smaller length than generated Fields Array ('.count($consider).'), Missing: ('.implode(',',$thedifference).') Order-Array:('.implode(',',$neworder).') Fields-Array:('.implode(',',$consider).') ');
+	
+				}			} else {
 				$neworder=array_flip($neworder);
 				$theorder=array();
 				foreach($consider as $ck => $cv){
