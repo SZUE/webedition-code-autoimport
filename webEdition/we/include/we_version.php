43e7cbd526ec6c0e7d42d15f96bf454df1d17211
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2013-01-03 03:30:24 +0000

    Vorbereitung release 6.3.5

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,6 +1,6 @@
 <?php
-define('WE_VERSION', '6.3.3.1');
-define('WE_VERSION_SUPP','nightly-build');
+define('WE_VERSION', '6.3.4.0');
+define('WE_VERSION_SUPP','release');
 define('WE_ZFVERSION','1.11.11'); // recommended version of the Zend Framework (bundled with webEdition)
 define('WE_SVNREV','');
 define('WE_VERSION_SUPP_VERSION','0');
