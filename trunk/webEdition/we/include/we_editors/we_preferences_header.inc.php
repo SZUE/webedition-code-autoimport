a4dc416518c21d3719cc28be35d8da433ac18cc6
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-01-08 01:15:31 +0000

    zu Feature #4544

--- trunk/webEdition/we/include/we_editors/we_preferences_header.inc.php
+++ trunk/webEdition/we/include/we_editors/we_preferences_header.inc.php
@@ -22,52 +22,20 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
-
 $tabname = isset($_REQUEST["tabname"]) && $_REQUEST["tabname"] != "" ? $_REQUEST["tabname"] : "setting_ui";
+include_once(WE_INCLUDES_PATH . 'we_editors/we_preferences_config.inc.php');
 
 
 // generate the tabs
 
 $we_tabs = new we_tabs();
 
-$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_ui]'), ($tabname == "setting_ui" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('ui');"));
-
-
-if(we_hasPerm("EDIT_SETTINGS_DEF_EXT")){
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_extensions]'), ($tabname == "setting_extensions" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('extensions');"));
+foreach($GLOBALS['tabs'] as $name => $perm){
+	if(empty($perm) || we_hasPerm($perm)){
+		$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab][' . $name . ']'), ($tabname == 'setting_' . $name ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('" . $name . "');"));
+	}
 }
 
-
-$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_editor]'), ($tabname == "setting_editor" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('editor');"));
-
-
-if(we_hasPerm("ADMINISTRATOR")){
-
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_proxy]'), ($tabname == "setting_proxy" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('proxy');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_advanced]'), ($tabname == "setting_advanced" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('advanced');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_system]'), ($tabname == "setting_system" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('system');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_seolinks]'), ($tabname == "setting_seolinks" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('seolinks');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[module_activation][headline]'), ($tabname == "setting_active_integrated_modules" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('active_integrated_modules');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_language]'), ($tabname == "setting_language" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('language');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_countries]'), ($tabname == "setting_countries" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('countries');"));
-
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_error_handling]'), ($tabname == "tab_error_handling" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('error_handling');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[backup]'), ($tabname == "setting_backup" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('backup');"));
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[validation]'), ($tabname == "setting_validation" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('validation');"));
-
-
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_email]'), ($tabname == "setting_email" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('email');"));
-}
-// add message_reporting tab
-$we_tabs->addTab(new we_tab("#", g_l('prefs', '[message_reporting][headline]'), ($tabname == "setting_message_reporting" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('message_reporting');"));
-
-if(we_hasPerm("FORMMAIL")){
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_formmail]'), ($tabname == "setting_recipients" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('recipients');"));
-}
-if(we_hasPerm("ADMINISTRATOR")){
-	$we_tabs->addTab(new we_tab("#", g_l('prefs', '[tab_versions]'), ($tabname == "setting_versions" ? 'TAB_ACTIVE' : 'TAB_NORMAL'), "top.we_cmd('versions');"));
-}
 $we_tabs->onResize('naviDiv');
 $tab_head = $we_tabs->getHeader('', 1);
 
