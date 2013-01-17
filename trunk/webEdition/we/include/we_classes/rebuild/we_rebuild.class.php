70d0a565002dc7b90cfc0a2322f3241c8086fa8d
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-01-17 16:59:36 +0000

    Fix #7097

--- trunk/webEdition/we/include/we_classes/rebuild/we_rebuild.class.php
+++ trunk/webEdition/we/include/we_classes/rebuild/we_rebuild.class.php
@@ -200,7 +200,7 @@ abstract class we_rebuild{
 		}
 		$data = self::getTemplates(true, $maintable, $tmptable);
 
-		$GLOBALS['DB_WE']->query('SELECT ID,ClassName,Path FROM ' . FILE_TABLE . ' WHERE IsFolder=1 OR Published > 0 ORDER BY ID');
+		$GLOBALS['DB_WE']->query('SELECT ID,ClassName,Path FROM ' . FILE_TABLE . ' WHERE IsFolder=1 OR Published > 0 ORDER BY IsFolder DESC, LENGTH(Path)');
 		while($GLOBALS['DB_WE']->next_record()) {
 			$data[] = array(
 				'id' => $GLOBALS['DB_WE']->f('ID'),
@@ -430,7 +430,7 @@ abstract class we_rebuild{
 			($_folders_query ? ' AND ' . $_folders_query . ' ' : '') .
 			($_template_query ? ' AND ' . $_template_query . ' ' : '');
 
-		$GLOBALS['DB_WE']->query('SELECT ID,ClassName,Path FROM ' . FILE_TABLE . ' WHERE IsDynamic=0 AND Published > 0 AND ContentType="text/webedition" ' . $query . ' ORDER BY ID');
+		$GLOBALS['DB_WE']->query('SELECT ID,ClassName,Path FROM ' . FILE_TABLE . ' WHERE IsDynamic=0 AND Published > 0 AND ContentType="text/webedition" ' . $query . ' ORDER BY LENGTH(Path)');
 		while($GLOBALS['DB_WE']->next_record()) {
 			$data[] = array(
 				'id' => $GLOBALS['DB_WE']->f('ID'),
