1296cf057a79ef1a13515c803034633a871c2f98
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-01-21 22:10:20 +0000

    Fix ZF version

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,7 +1,7 @@
 <?php
 define('WE_VERSION', '6.3.5.1');
 define('WE_VERSION_SUPP','nightly-build');
-define('WE_ZFVERSION','1.11.11'); // recommended version of the Zend Framework (bundled with webEdition)
+define('WE_ZFVERSION','1.12.1'); // recommended version of the Zend Framework (bundled with webEdition)
 define('WE_SVNREV','');
 define('WE_VERSION_SUPP_VERSION','0');
 define('WE_VERSION_BRANCH','trunk');
