f145ccbba558222a98a1218bbd37c1fc9a3df032
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-01-24 08:44:12 +0000

    Fix #7273

--- webEdition/we/include/weTagWizard/we_tags/we_tag_field.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_field.inc.php
@@ -1,35 +1,35 @@
-<?php
-//NOTE you are inside the constructor of weTagData.class.php
+	<?php
+	//NOTE you are inside the constructor of weTagData.class.php
 
-$this->NeedsEndTag = false;
-//$this->Groups[] = 'input_tags';
-//$this->Module = '';
-$this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
+	$this->NeedsEndTag = false;
+	//$this->Groups[] = 'input_tags';
+	//$this->Module = '';
+	$this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
 
-$name = new weTagData_choiceAttribute('name', array(new weTagDataOption('WE_PATH', false, ''), new weTagDataOption('WE_ID', false, ''), new weTagDataOption('WE_TEXT', false, ''), new weTagDataOption('wedoc_CreationDate', false, ''), new weTagDataOption('wedoc_ModDate', false, ''), new weTagDataOption('wedoc_Published', false, ''), new weTagDataOption('wedoc_ParentID', false, ''), new weTagDataOption('wedoc_Text', false, ''), new weTagDataOption('WE_SHOPVARIANTS', false, '')), false,false, '');
-$classid = (defined("OBJECT_TABLE") ? new weTagData_selectorAttribute('classid',OBJECT_TABLE, 'object', false, ''): null);
-$hyperlink = new weTagData_selectAttribute('hyperlink', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
-$tid = (defined("TEMPLATES_TABLE") ? new weTagData_selectorAttribute('tid',TEMPLATES_TABLE, 'text/weTmpl', false, ''): null);
-$href = new weTagData_textAttribute('href', false, '');
-$target = new weTagData_choiceAttribute('target', array(new weTagDataOption('_top', false, ''), new weTagDataOption('_parent', false, ''), new weTagDataOption('_self', false, ''), new weTagDataOption('_blank', false, '')), false,false, '');
-$class = new weTagData_textAttribute('class', false, '');
-$style = new weTagData_textAttribute('style', false, '');
-$format = new weTagData_textAttribute('format', false, '');
-$num_format = new weTagData_choiceAttribute('num_format', array(new weTagDataOption('german', false, ''), new weTagDataOption('french', false, ''), new weTagDataOption('english', false, ''), new weTagDataOption('swiss', false, '')), false,false, '');
-$thumbnail = new weTagData_sqlRowAttribute('thumbnail',THUMBNAILS_TABLE, false, 'Name', '', '', '');
-$id = (defined("FILE_TABLE") ? new weTagData_selectorAttribute('id',FILE_TABLE, 'text/webedition', false, ''):null);
-$parentidname = new weTagData_textAttribute('parentidname', false, '');
-$winprops = new weTagData_textAttribute('winprops', false, '');
-$alt = new weTagData_textAttribute('alt', false, '');
-$max = new weTagData_textAttribute('max', false, '');
-$src = new weTagData_textAttribute('src', false, '');
-$width = new weTagData_textAttribute('width', false, '');
-$height = new weTagData_textAttribute('height', false, '');
-$border = new weTagData_textAttribute('border', false, '');
-$hspace = new weTagData_textAttribute('hspace', false, '');
-$vspace = new weTagData_textAttribute('vspace', false, '');
-$align = new weTagData_selectAttribute('align', array(new weTagDataOption('left', false, ''), new weTagDataOption('right', false, ''), new weTagDataOption('top', false, ''), new weTagDataOption('bottom', false, ''), new weTagDataOption('absmiddle', false, ''), new weTagDataOption('middle', false, ''), new weTagDataOption('texttop', false, ''), new weTagDataOption('baseline', false, ''), new weTagDataOption('absbottom', false, '')), false, '');
-//$only = new weTagData_textAttribute('only', false, '');
+	$name = new weTagData_choiceAttribute('name', array(new weTagDataOption('WE_PATH', false, ''), new weTagDataOption('WE_ID', false, ''), new weTagDataOption('WE_TEXT', false, ''), new weTagDataOption('wedoc_CreationDate', false, ''), new weTagDataOption('wedoc_ModDate', false, ''), new weTagDataOption('wedoc_Published', false, ''), new weTagDataOption('wedoc_ParentID', false, ''), new weTagDataOption('wedoc_Text', false, ''), new weTagDataOption('WE_SHOPVARIANTS', false, '')), false,false, '');
+	$classid = (defined("OBJECT_TABLE") ? new weTagData_selectorAttribute('classid',OBJECT_TABLE, 'object', false, ''): null);
+	$hyperlink = new weTagData_selectAttribute('hyperlink', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
+	$tid = (defined("TEMPLATES_TABLE") ? new weTagData_selectorAttribute('tid',TEMPLATES_TABLE, 'text/weTmpl', false, ''): null);
+	$href = new weTagData_textAttribute('href', false, '');
+	$target = new weTagData_choiceAttribute('target', array(new weTagDataOption('_top', false, ''), new weTagDataOption('_parent', false, ''), new weTagDataOption('_self', false, ''), new weTagDataOption('_blank', false, '')), false,false, '');
+	$class = new weTagData_textAttribute('class', false, '');
+	$style = new weTagData_textAttribute('style', false, '');
+	$format = new weTagData_textAttribute('format', false, '');
+	$num_format = new weTagData_choiceAttribute('num_format', array(new weTagDataOption('german', false, ''), new weTagDataOption('french', false, ''), new weTagDataOption('english', false, ''), new weTagDataOption('swiss', false, '')), false,false, '');
+	$thumbnail = new weTagData_sqlRowAttribute('thumbnail',THUMBNAILS_TABLE, false, 'Name', '', '', '');
+	$id = (defined("FILE_TABLE") ? new weTagData_selectorAttribute('id',FILE_TABLE, 'text/webedition', false, ''):null);
+	$parentidname = new weTagData_textAttribute('parentidname', false, '');
+	$winprops = new weTagData_textAttribute('winprops', false, '');
+	$alt = new weTagData_textAttribute('alt', false, '');
+	$max = new weTagData_textAttribute('max', false, '');
+	$src = new weTagData_textAttribute('src', false, '');
+	$width = new weTagData_textAttribute('width', false, '');
+	$height = new weTagData_textAttribute('height', false, '');
+	$border = new weTagData_textAttribute('border', false, '');
+	$hspace = new weTagData_textAttribute('hspace', false, '');
+	$vspace = new weTagData_textAttribute('vspace', false, '');
+	$align = new weTagData_selectAttribute('align', array(new weTagDataOption('left', false, ''), new weTagDataOption('right', false, ''), new weTagDataOption('top', false, ''), new weTagDataOption('bottom', false, ''), new weTagDataOption('absmiddle', false, ''), new weTagDataOption('middle', false, ''), new weTagDataOption('texttop', false, ''), new weTagDataOption('baseline', false, ''), new weTagDataOption('absbottom', false, '')), false, '');
+	//$only = new weTagData_textAttribute('only', false, '');
 $htmlspecialchars = new weTagData_selectAttribute('htmlspecialchars', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
 $seeMode = new weTagData_selectAttribute('seeMode', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
 $xml = new weTagData_selectAttribute('xml', array(new weTagDataOption('true', false, ''), new weTagDataOption('false', false, '')), false, '');
@@ -37,7 +37,7 @@ $win2iso = new weTagData_selectAttribute('win2iso', array(new weTagDataOption('t
 $listviewname = new weTagData_textAttribute('listviewname', false, '');
 $striphtml = new weTagData_selectAttribute('striphtml', array(new weTagDataOption('false', false, ''), new weTagDataOption('true', false, '')), false, '');
 $only = new weTagData_selectAttribute('only', array(new weTagDataOption('name', false, ''), new weTagDataOption('src', false, ''), new weTagDataOption('parentpath', false, ''), new weTagDataOption('filename', false, ''), new weTagDataOption('extension', false, ''), new weTagDataOption('filesize', false, '')), false, '');
-$onlyImg = new weTagData_selectAttribute('only', array(new weTagDataOption('name', false, ''), new weTagDataOption('src', false, ''), new weTagDataOption('parentpath', false, ''), new weTagDataOption('filename', false, ''), new weTagDataOption('extension', false, ''), new weTagDataOption('filesize', false, ''),new weTagDataOption('width', false, ''),new weTagDataOption('height', false, ''),new weTagDataOption('src', false, ''),new weTagDataOption('alt', false, '')), false, '');
+$onlyImg = new weTagData_selectAttribute('only', array(new weTagDataOption('name', false, ''), new weTagDataOption('src', false, ''), new weTagDataOption('parentpath', false, ''), new weTagDataOption('filename', false, ''), new weTagDataOption('extension', false, ''), new weTagDataOption('filesize', false, ''),new weTagDataOption('width', false, ''),new weTagDataOption('height', false, ''),new weTagDataOption('alt', false, '')), false, '');
 $outputlanguage = new weTagData_textAttribute('outputlanguage', false, '');
 $doc = new weTagData_selectAttribute('doc', array(new weTagDataOption('self', false, ''), new weTagDataOption('top', false, '')), false, '');
 $triggerid = (defined("FILE_TABLE") ? new weTagData_selectorAttribute('triggerid',FILE_TABLE, 'text/webedition', false, ''): null);
