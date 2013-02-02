d0da5b0e30123df068c6c1b22946d67c3a69343f
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-02-02 17:26:23 +0000

    Nachtrag tblprefs

--- additional/sqldumps/tblPrefs.sql
+++ additional/sqldumps/tblPrefs.sql
@@ -2,7 +2,8 @@ CREATE TABLE ###TBLPREFIX###tblPrefs (
   userID bigint(20) unsigned NOT NULL default '0',
   `key` varchar(100) NOT NULL default '',
 	value text NOT NULL,
-  PRIMARY KEY (`userID`,`key`)
+  PRIMARY KEY (`userID`,`key`),
+	KEY lookup (`key`)
 ) ENGINE=MyISAM;
 
 /* query separator */
