b5f0e8fc17481c9f9787090beba2a52664ba05a6
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-02-04 22:25:45 +0000

    nachtrag pdf

--- webEdition/we/include/we_classes/helpers/we_helpers_pdf2text.class.php
+++ webEdition/we/include/we_classes/helpers/we_helpers_pdf2text.class.php
@@ -22,6 +22,7 @@
  * @category   webEdition
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
+ * Filters from "tcpdf_filters.php", Nicola Asuni - Tecnick.com LTD - Manor Coach House, Church Hill, Aldershot, Hants, GU12 4RQ, UK - www.tecnick.com
  */
 //define('DEBUG', 'fontout|page|tree|line'); //line|fontout|page|tree
 //define('DEBUG', 'tree'); //line|fontout|page|tree
@@ -136,7 +137,7 @@ class we_helpers_pdf2text{
 				foreach($str as $cur){
 					$out.=chr(hexdec($cur));
 				}
-				return CheckAndConvertISOfrontend(mb_convert_encoding($out, 'UTF-8','UTF-16'));
+				return CheckAndConvertISOfrontend(mb_convert_encoding($out, 'UTF-8', 'UTF-16'));
 		}
 	}
 
@@ -211,12 +212,269 @@ class we_helpers_pdf2text{
 	}
 
 	private static function getStream($elem){
-		if(!isset($elem['stream'])){
-			print_r($elem);
+		/* if(!isset($elem['stream'])){
+		  print_r($elem);
+		  } */
+		switch($elem['Filter']){
+
+			case '/Fl':
+			case '/FlateDecode':
+				return @gzuncompress($elem['stream']);
+			case 'ASCIIHexDecode':
+			case 'AHx':
+				return self::decodeFilterASCIIHexDecode($elem['stream']);
+			case 'ASCII85Decode':
+			case 'A85':
+				return self::decodeFilterASCII85Decode($elem['stream']);
+			case 'LZWDecode':
+			case 'LZW':
+				return self::decodeFilterLZWDecode($elem['stream']);
+			case 'RunLengthDecode':
+			case 'RL':
+				return self::decodeFilterRunLengthDecode($elem['stream']);
+			case 'CCITTFaxDecode':
+			case 'CCF':
+			//can be ignored - used for images only
+			case 'DCTDecode':
+			case 'DCT':
+			//can be ignored - used for images only
+
+			default:
+				return $elem['stream'];
 		}
-		return (!self::DEFLATE_ALL && $elem['Filter'] == self::FILTER_GZ ?
-				@gzuncompress($elem['stream']) :
-				$elem['stream']);
+	}
+
+	/**
+	 * ASCIIHexDecode
+	 * Decodes data encoded in an ASCII hexadecimal representation, reproducing the original binary data.
+	 * @param $data (string) Data to decode.
+	 * @return Decoded data string.
+	 * @public
+	 * @since 1.0.000 (2011-05-23)
+	 */
+	private static function decodeFilterASCIIHexDecode($data){
+		// intialize string to return
+		$decoded = '';
+		// all white-space characters shall be ignored
+		$data = preg_replace('/[\s]/', '', $data);
+		// check for EOD character: GREATER-THAN SIGN (3Eh)
+		$eod = strpos($data, '>');
+		if($eod !== false){
+			// remove EOD and extra data (if any)
+			$data = substr($data, 0, $eod);
+			$eod = true;
+		}
+		// get data length
+		$data_length = strlen($data);
+		if(($data_length % 2) != 0){
+			// odd number of hexadecimal digits
+			if($eod){
+				// EOD shall behave as if a 0 (zero) followed the last digit
+				$data = substr($data, 0, -1) . '0' . substr($data, -1);
+			} else{
+				$this->Error('decodeASCIIHex: invalid code');
+			}
+		}
+		// check for invalid characters
+		if(preg_match('/[^a-fA-F\d]/', $data) > 0){
+			$this->Error('decodeASCIIHex: invalid code');
+		}
+		// get one byte of binary data for each pair of ASCII hexadecimal digits
+		$decoded = pack('H*', $data);
+		return $decoded;
+	}
+
+	/**
+	 * ASCII85Decode
+	 * Decodes data encoded in an ASCII base-85 representation, reproducing the original binary data.
+	 * @param $data (string) Data to decode.
+	 * @return Decoded data string.
+	 * @since 1.0.000 (2011-05-23)
+	 */
+	private static function decodeFilterASCII85Decode($data){
+		// intialize string to return
+		$decoded = '';
+		// all white-space characters shall be ignored
+		$data = preg_replace('/[\s]/', '', $data);
+		// remove start sequence 2-character sequence <~ (3Ch)(7Eh)
+		if(strpos($data, '<~') !== false){
+			// remove EOD and extra data (if any)
+			$data = substr($data, 2);
+		}
+		// check for EOD: 2-character sequence ~> (7Eh)(3Eh)
+		$eod = strpos($data, '~>');
+		if($eod !== false){
+			// remove EOD and extra data (if any)
+			$data = substr($data, 0, $eod);
+		}
+		// data length
+		$data_length = strlen($data);
+		// check for invalid characters
+		if(preg_match('/[^\x21-\x75,\x74]/', $data) > 0){
+			$this->Error('decodeASCII85: invalid code');
+		}
+		// z sequence
+		$zseq = chr(0) . chr(0) . chr(0) . chr(0);
+		// position inside a group of 4 bytes (0-3)
+		$group_pos = 0;
+		$tuple = 0;
+		$pow85 = array((85 * 85 * 85 * 85), (85 * 85 * 85), (85 * 85), 85, 1);
+		$last_pos = ($data_length - 1);
+		// for each byte
+		for($i = 0; $i < $data_length; ++$i){
+			// get char value
+			$char = ord($data[$i]);
+			if($char == 122){ // 'z'
+				if($group_pos == 0){
+					$decoded .= $zseq;
+				} else{
+					$this->Error('decodeASCII85: invalid code');
+				}
+			} else{
+				// the value represented by a group of 5 characters should never be greater than 2^32 - 1
+				$tuple += (($char - 33) * $pow85[$group_pos]);
+				if($group_pos == 4){
+					$decoded .= chr($tuple >> 24) . chr($tuple >> 16) . chr($tuple >> 8) . chr($tuple);
+					$tuple = 0;
+					$group_pos = 0;
+				} else{
+					++$group_pos;
+				}
+			}
+		}
+		if($group_pos > 1){
+			$tuple += $pow85[($group_pos - 1)];
+		}
+		// last tuple (if any)
+		switch($group_pos){
+			case 4:{
+					$decoded .= chr($tuple >> 24) . chr($tuple >> 16) . chr($tuple >> 8);
+					break;
+				}
+			case 3:{
+					$decoded .= chr($tuple >> 24) . chr($tuple >> 16);
+					break;
+				}
+			case 2:{
+					$decoded .= chr($tuple >> 24);
+					break;
+				}
+			case 1:{
+					$this->Error('decodeASCII85: invalid code');
+					break;
+				}
+		}
+		return $decoded;
+	}
+
+	/**
+	 * LZWDecode
+	 * Decompresses data encoded using the LZW (Lempel-Ziv-Welch) adaptive compression method, reproducing the original text or binary data.
+	 * @param $data (string) Data to decode.
+	 * @return Decoded data string.
+	 * @public
+	 * @since 1.0.000 (2011-05-23)
+	 */
+	private static function decodeFilterLZWDecode($data){
+		// intialize string to return
+		$decoded = '';
+		// data length
+		$data_length = strlen($data);
+		// convert string to binary string
+		$bitstring = '';
+		for($i = 0; $i < $data_length; ++$i){
+			$bitstring .= sprintf('%08b', ord($data{$i}));
+		}
+		// get the number of bits
+		$data_length = strlen($bitstring);
+		// initialize code length in bits
+		$bitlen = 9;
+		// initialize dictionary index
+		$dix = 258;
+		// initialize the dictionary (with the first 256 entries).
+		$dictionary = array();
+		for($i = 0; $i < 256; ++$i){
+			$dictionary[$i] = chr($i);
+		}
+		// previous val
+		$prev_index = 0;
+		// while we encounter EOD marker (257), read code_length bits
+		while(($data_length > 0) AND (($index = bindec(substr($bitstring, 0, $bitlen))) != 257)) {
+			// remove read bits from string
+			$bitstring = substr($bitstring, $bitlen);
+			// update number of bits
+			$data_length -= $bitlen;
+			if($index == 256){ // clear-table marker
+				// reset code length in bits
+				$bitlen = 9;
+				// reset dictionary index
+				$dix = 258;
+				$prev_index = 256;
+				// reset the dictionary (with the first 256 entries).
+				$dictionary = array();
+				for($i = 0; $i < 256; ++$i){
+					$dictionary[$i] = chr($i);
+				}
+			} elseif($prev_index == 256){
+				// first entry
+				$decoded .= $dictionary[$index];
+				$prev_index = $index;
+			} else{
+				// check if index exist in the dictionary
+				if($index < $dix){
+					// index exist on dictionary
+					$decoded .= $dictionary[$index];
+					$dic_val = $dictionary[$prev_index] . $dictionary[$index]{0};
+					// store current index
+					$prev_index = $index;
+				} else{
+					// index do not exist on dictionary
+					$dic_val = $dictionary[$prev_index] . $dictionary[$prev_index]{0};
+					$decoded .= $dic_val;
+				}
+				// update dictionary
+				$dictionary[$dix] = $dic_val;
+				++$dix;
+				// change bit length by case
+				if($dix == 2047){
+					$bitlen = 12;
+				} elseif($dix == 1023){
+					$bitlen = 11;
+				} elseif($dix == 511){
+					$bitlen = 10;
+				}
+			}
+		}
+		return $decoded;
+	}
+
+	private static function decodeFilterRunLengthDecode($data){
+		// intialize string to return
+		$decoded = '';
+		// data length
+		$data_length = strlen($data);
+		$i = 0;
+		while($i < $data_length) {
+			// get current byte value
+			$byte = ord($data{$i});
+			if($byte == 128){
+				// a length value of 128 denote EOD
+				break;
+			} elseif($byte < 128){
+				// if the length byte is in the range 0 to 127
+				// the following length + 1 (1 to 128) bytes shall be copied literally during decompression
+				$decoded .= substr($data, ($i + 1), ($byte + 1));
+				// move to next block
+				$i += ($byte + 2);
+			} else{
+				// if length is in the range 129 to 255,
+				// the following single byte shall be copied 257 - length (2 to 128) times during decompression
+				$decoded .= str_repeat($data{($i + 1)}, (257 - $byte));
+				// move to next block
+				$i += 2;
+			}
+		}
+		return $decoded;
 	}
 
 	private static function applyToUnicode($data, &$table){
@@ -407,7 +665,11 @@ class we_helpers_pdf2text{
 				$values['value'] = $matches2[1];
 			}
 			if(isset($matches2[3])){
-				$values['stream'] = (self::DEFLATE_ALL && isset($values['Filter']) && $values['Filter'] == self::FILTER_GZ ? @gzuncompress($matches2[3]) : $matches2[3]);
+				$values['stream'] = $matches2[3];
+				if(self::DEFLATE_ALL){
+					$values['stream'] = self::getStream($values);
+					$values['Filter'].='-done';
+				}
 			}
 			/* if(isset($values['Filter'])&&!isset($values['stream'])){
 			  print_r($matches2);
