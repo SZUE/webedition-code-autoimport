6987144aa7f0e4d33858373f7db2ed99bfa63519
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2013-02-15 11:30:03 +0000

    Bug 7393

--- trunk/webEdition/index.php
+++ trunk/webEdition/index.php
@@ -42,7 +42,7 @@ if(!defined('CONF_SAVED_VERSION') || (defined('CONF_SAVED_VERSION') && (intval(W
 	//resave config file(s)
 	we_base_preferences::check_global_config(true);
 }
-we_util_File::checkAndMakeFolder(WE_THUMBNAIL_DIRECTORY);
+we_util_File::checkAndMakeFolder($_SERVER['DOCUMENT_ROOT'].WE_THUMBNAIL_DIRECTORY);
 
 define('LOGIN_DENIED', 4);
 define('LOGIN_OK', 2);
