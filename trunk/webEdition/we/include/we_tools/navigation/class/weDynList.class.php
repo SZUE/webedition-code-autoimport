46ef60821db89ec071f4449667c63b3cb166aeea
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2013-02-18 16:42:18 +0000

    #6729

--- trunk/webEdition/we/include/we_tools/navigation/class/weDynList.class.php
+++ trunk/webEdition/we/include/we_tools/navigation/class/weDynList.class.php
@@ -172,7 +172,7 @@ abstract class weDynList{
 		$categories = is_array($categories) ? $categories : array();
 		$_cats = array();
 		foreach($categories as $cat){
-			$_cats[] = '(OF_Category LIKE "%,' . $_db->escape(path_to_id($cat, CATEGORY_TABLE)) . ',%")';
+			$_cats[] = '(OF_Category LIKE "%,' . is_numeric($cat) ? $cat : $_db->escape(path_to_id($cat, CATEGORY_TABLE)) . ',%")';//bug #6729
 		}
 
 		$_where = array();
