588a26150719d86485ee3b83c61e8c9c4834279b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-04-26 09:17:14 +0000

    Fix #7628

--- webEdition/we/include/we_modules/shop/edit_shop_editorHeader.php
+++ webEdition/we/include/we_modules/shop/edit_shop_editorHeader.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we.inc.php');
-if(isset($_REQUEST["home"]) && $_REQUEST["home"]){
+if (isset($_REQUEST["home"]) && $_REQUEST["home"]) {
 	print '<body bgcolor="#F0EFF0"></body></html>';
 	exit;
 }
@@ -35,59 +35,46 @@ print STYLESHEET;
 
 $bid = isset($_REQUEST["bid"]) ? intval($_REQUEST["bid"]) : 0;
 
-$cid = f("SELECT IntCustomerID FROM " . SHOP_TABLE . " WHERE IntOrderID = " . intval($bid), "IntCustomerID", $DB_WE);
-$DB_WE->query("SELECT IntOrderID,DATE_FORMAT(DateOrder,'" . g_l('date', '[format][mysqlDate]') . "') as orddate FROM " . SHOP_TABLE . " GROUP BY IntOrderID ORDER BY IntID DESC");
-if($DB_WE->next_record()){
-	$headline = sprintf(g_l('modules_shop', '[lastOrder]'), $DB_WE->f("IntOrderID"), $DB_WE->f("orddate"));
-	$textPost = sprintf(g_l('modules_shop', '[orderNo]'), $_REQUEST['bid'], $DB_WE->f("orddate"));
-} else{
-	$headline = '';
-	$textPost = '';
-}
+list($cid, $cdat) = getHash('SELECT IntCustomerID,DATE_FORMAT(DateOrder,"' . g_l('date', '[format][mysqlDate]') . '") FROM ' . SHOP_TABLE . ' WHERE IntOrderID=' . intval($bid), $DB_WE);
+$order = getHash('SELECT IntOrderID,DATE_FORMAT(DateOrder,"' . g_l('date', '[format][mysqlDate]') . '") as orddate FROM ' . SHOP_TABLE . ' GROUP BY IntOrderID ORDER BY IntID DESC LIMIT 1', $DB_WE);
+$headline = (empty($order) ? '' : sprintf(g_l('modules_shop', '[lastOrder]'), $order["IntOrderID"], $order["orddate"]));
+$textPost = ($bid ? sprintf(g_l('modules_shop', '[orderNo]'), $_REQUEST['bid'], $cdat) : '');
 
 
 $we_tabs = new we_tabs();
 
-if(isset($_REQUEST["mid"]) && $_REQUEST["mid"] && $_REQUEST["mid"] != '00'){
+if (isset($_REQUEST["mid"]) && $_REQUEST["mid"] && $_REQUEST["mid"] != '00') {
 
-	$we_tabs->addTab(new we_tab("#", g_l('tabs', "[module][overview]"), "TAB_ACTIVE", "0"));
-} else{
-
-	$we_tabs->addTab(new we_tab("#", g_l('tabs', "[module][orderdata]"), "TAB_ACTIVE", "setTab(0);"));
+	$we_tabs->addTab(new we_tab('#', g_l('tabs', "[module][overview]"), 'TAB_ACTIVE', 0));
+} else {
+	$we_tabs->addTab(new we_tab('#', g_l('tabs', "[module][orderdata]"), "TAB_ACTIVE", "setTab(0);"));
 	$we_tabs->addTab(new we_tab("#", g_l('tabs', "[module][orderlist]"), "TAB_NORMAL", "setTab(1);"));
-	/*
-	  $we_tabs->addTab(new we_tab("#", g_l('tabs',"[module][addtab1]"), "TAB_NORMAL","setTab(2);"));
-	  $we_tabs->addTab(new we_tab("#", g_l('tabs',"[module][addtab2]"), "TAB_NORMAL","setTab(3);"));
-	  $we_tabs->addTab(new we_tab("#", g_l('tabs',"[module][addtab3]"), "TAB_NORMAL","setTab(4);"));
-	  $we_tabs->addTab(new we_tab("#", g_l('tabs',"[module][addtab4]"), "TAB_NORMAL","setTab(5);"));
-	 */
 }
 
 $textPre = isset($_REQUEST['bid']) && $_REQUEST['bid'] > 0 ? g_l('modules_shop', '[orderList][order]') : g_l('modules_shop', '[order_view]');
 $textPost = isset($_REQUEST['mid']) && $_REQUEST['mid'] > 0 ? (strlen($_REQUEST['mid']) > 5 ? g_l('modules_shop', '[month][' . substr($_REQUEST['mid'], 0, -5) . ']') . " " . substr($_REQUEST['mid'], -5, 4) : substr($_REQUEST['mid'], 1)) : $textPost;
-//$textPost = sprintf(g_l('modules_shop','[orderNo]'),$_REQUEST['bid'],"post");
 $we_tabs->onResize();
 $tab_head = $we_tabs->getHeader();
 
 $tab_body = '<div id="main" >' . we_html_tools::getPixel(100, 3) . '<div style="margin:0px;padding-left:10px;" id="headrow"><nobr><b>' . str_replace(" ", "&nbsp;", $textPre) . ':&nbsp;</b><span id="h_path" class="header_small"><b id="titlePath">' . str_replace(" ", "&nbsp;", $textPost) . '</b></span></nobr></div>' . we_html_tools::getPixel(100, 3) .
-	$we_tabs->getHTML() .
-	'</div>';
+				$we_tabs->getHTML() .
+				'</div>';
 ?>
 <script type="text/javascript">
 	<!--
-	function setTab(tab){
-		switch(tab){
+	function setTab(tab) {
+		switch (tab) {
 			case 0:
 				parent.edbody.document.location = 'edit_shop_properties.php?bid=<?php print $bid; ?>';
 				break;
 			case 1:
 				parent.edbody.document.location = 'edit_shop_orderlist.php?cid=<?php print $cid; ?>';
 				break;
-			}
 		}
+	}
 
-		top.content.hloaded=1;
-		//-->
+	top.content.hloaded = 1;
+	//-->
 </script>
 <?php
 print $tab_head;
