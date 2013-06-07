1904ee26fd0b8f1b2458f762dea1a229387ba3d4
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-06-07 23:32:45 +0000

    Fix #123

--- branches/main-develop/webEdition/we/include/weTagWizard/we_tags/we_tag_ifDoctype.inc.php
+++ branches/main-develop/webEdition/we/include/weTagWizard/we_tags/we_tag_ifDoctype.inc.php
@@ -7,5 +7,5 @@ $this->Groups[] = 'if_tags';
 //$this->Module = '';
 $this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
 
-$this->Attributes[] = new weTagData_textAttribute('doctypes', true, '');
+$this->Attributes[] = new weTagData_sqlRowAttribute('doctypes',DOC_TYPES_TABLE, false, 'DocType', 'DocType', 'DocType', '');
 $this->Attributes[] = new weTagData_selectAttribute('doc', array(new weTagDataOption('top', false, ''), new weTagDataOption('self', false, ''), new weTagDataOption('listview', false, '')), false, '');
