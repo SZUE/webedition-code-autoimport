b6b7543386423a9972702e0cf2ffc6284185bb01
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-06-24 10:09:31 +0000

    Fix #4663

--- webEdition/we/include/weTagWizard/we_tags/we_tag_ifbannerexists.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_ifbannerexists.inc.php
@@ -4,4 +4,5 @@
 $this->NeedsEndTag = true;
 $this->Groups[] = 'if_tags';
 $this->Module = 'banner';
+$this->Deprecated = true;
 $this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
