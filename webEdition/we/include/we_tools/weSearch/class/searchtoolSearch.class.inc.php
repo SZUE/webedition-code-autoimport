bb661db8c66a03bb6212a5592651863d20ed5b44
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-07-21 21:28:35 +0000

    Fix zu #7650

--- webEdition/we/include/we_tools/weSearch/class/searchtoolSearch.class.inc.php
+++ webEdition/we/include/we_tools/weSearch/class/searchtoolSearch.class.inc.php
@@ -78,7 +78,7 @@ class searchtoolsearch extends we_search{
 			if(isset($_REQUEST['setView'])){
 				$this->db->query('UPDATE ' . FILE_TABLE . ' SET listview=' . intval($_REQUEST['setView']) . ' WHERE ID=' . intval($obj->ID));
 				$obj->searchclassFolder->setView = ($_REQUEST['setView']);
-			} else{
+			} else {
 				$obj->searchclassFolder->setView = f('SELECT listview FROM ' . FILE_TABLE . ' WHERE ID=' . intval($obj->ID), 'listview', $GLOBALS['DB_WE']);
 			}
 			if(isset($_REQUEST['mode'])){
@@ -97,7 +97,7 @@ class searchtoolsearch extends we_search{
 				$obj->searchclassFolder->height = 0;
 			} elseif(!isset($_REQUEST['searchFields']) && !isset($_REQUEST['searchstart'])){
 				$obj->searchclassFolder->height = 1;
-			} else{
+			} else {
 				$obj->searchclassFolder->height = 1;
 			}
 			if(isset($_REQUEST['location'])){
@@ -106,7 +106,7 @@ class searchtoolsearch extends we_search{
 			if(isset($_REQUEST['search'])){
 				$obj->searchclassFolder->search = ($_REQUEST['search']);
 			}
-		} else{
+		} else {
 			if(isset($_REQUEST['we_cmd']['setView']) && isset($_REQUEST['id'])){
 				$this->db->query('UPDATE ' . FILE_TABLE . ' SET listview=' . intval($_REQUEST['we_cmd']['setView']) . ' WHERE ID=' . intval($_REQUEST['id']));
 			}
@@ -131,7 +131,7 @@ class searchtoolsearch extends we_search{
 		$vals = array();
 
 		$_db->query('SELECT ID, Text FROM ' . USER_TABLE);
-		while($_db->next_record()) {
+		while($_db->next_record()){
 			$v = $_db->f('ID');
 			$t = $_db->f('Text');
 			$vals[$v] = $t;
@@ -253,7 +253,7 @@ class searchtoolsearch extends we_search{
 		$vals = array();
 
 		$_db->query('SELECT ID,DocType FROM ' . DOC_TYPES_TABLE . ' ' . $q);
-		while($_db->next_record()) {
+		while($_db->next_record()){
 			$vals[$_db->f('ID')] = $_db->f('DocType');
 		}
 
@@ -266,22 +266,22 @@ class searchtoolsearch extends we_search{
 		$_db2 = new DB_WE();
 		//first check published documents
 		$_db2->query('SELECT a.DID FROM ' . LINK_TABLE . ' a LEFT JOIN ' . CONTENT_TABLE . " b on (a.CID = b.ID) WHERE a.Name='Title' AND b.Dat LIKE '%" . $_db2->escape(trim($keyword)) . "%' AND NOT a.DocumentTable!='" . TEMPLATES_TABLE . "'");
-		while($_db2->next_record()) {
+		while($_db2->next_record()){
 			$titles[] = $_db2->f('DID');
 		}
 		//check unpublished documents
 		$_db2->query('SELECT DocumentID, DocumentObject  FROM ' . TEMPORARY_DOC_TABLE . " WHERE DocTable = 'tblFile' AND Active = 1 AND DocumentObject LIKE '%" . $_db2->escape(trim($keyword)) . "%'");
-		while($_db2->next_record()) {
+		while($_db2->next_record()){
 			$tempDoc = unserialize($_db2->f('DocumentObject'));
-			if(isset($tempDoc[0]['elements']['Title']) && $tempDoc[0]['elements']['Title']['dat'] != ""){
-				$keyword = str_replace(array("\_", "\%"), array("_", "%"), $keyword);
+			if(isset($tempDoc[0]['elements']['Title']) && $tempDoc[0]['elements']['Title']['dat'] != ''){
+				$keyword = str_replace(array('\_', '\%'), array('_', '%'), $keyword);
 				if(stristr($tempDoc[0]['elements']['Title']['dat'], $keyword)){
 					$titles[] = $_db2->f('DocumentID');
 				}
 			}
 		}
 
-		return (!empty($titles) ? " " . $table . ".ID IN (" . makeCSVFromArray($titles) . ")" : '');
+		return (!empty($titles) ? ' ' . $table . '.ID IN (' . makeCSVFromArray($titles) . ')' : '');
 	}
 
 	function searchCategory($keyword, $table){
@@ -291,21 +291,21 @@ class searchtoolsearch extends we_search{
 		$_db = new DB_WE();
 		switch($table){
 			case FILE_TABLE:
-				$field = "temp_category";
-				$field2 = "Category";
-				$query = "SELECT ID, " . $field . ", " . $field2 . "  FROM " . $table . " WHERE ((" . $field2 . " != NULL OR " . $field2 . " != '') AND Published >= ModDate AND Published !=0) OR (Published < ModDate AND (" . $field . " != NULL OR " . $field . " != '')) ";
+				$field = 'temp_category';
+				$field2 = 'Category';
+				$query = 'SELECT ID, ' . $field . ', ' . $field2 . '  FROM ' . $table . ' WHERE ((' . $field2 . ' != NULL OR ' . $field2 . " != '') AND Published >= ModDate AND Published !=0) OR (Published < ModDate AND (" . $field . " != NULL OR " . $field . " != '')) ";
 				break;
 			case VERSIONS_TABLE:
-				$field = "Category";
-				$query = "SELECT ID," . $field . "  FROM " . $table . " WHERE " . $field . " != NULL OR " . $field . " != '' ";
+				$field = 'Category';
+				$query = 'SELECT ID,' . $field . '  FROM ' . $table . ' WHERE ' . $field . ' != NULL OR ' . $field . " != '' ";
 				break;
-			case (defined("OBJECT_TABLE") ? OBJECT_TABLE : 'OBJECT_TABLE'):
-				$field = "DefaultCategory";
-				$query = "SELECT ID," . $field . "  FROM " . $table . " WHERE " . $field . " != NULL OR " . $field . " != '' ";
+			case (defined('OBJECT_TABLE') ? OBJECT_TABLE : 'OBJECT_TABLE'):
+				$field = 'DefaultCategory';
+				$query = 'SELECT ID,' . $field . '  FROM ' . $table . ' WHERE ' . $field . ' != NULL OR ' . $field . " != '' ";
 				break;
-			case (defined("OBJECT_FILES_TABLE") ? OBJECT_FILES_TABLE : 'OBJECT_FILES_TABLE'):
-				$field = "Category";
-				$query = "SELECT ID," . $field . "  FROM " . $table . " WHERE " . $field . " != NULL OR " . $field . " != '' AND Published >= ModDate AND Published !=0";
+			case (defined('OBJECT_FILES_TABLE') ? OBJECT_FILES_TABLE : 'OBJECT_FILES_TABLE'):
+				$field = 'Category';
+				$query = 'SELECT ID,' . $field . '  FROM ' . $table . ' WHERE ' . $field . ' != NULL OR ' . $field . " != '' AND Published >= ModDate AND Published !=0";
 				break;
 		}
 		$res = array();
@@ -315,28 +315,28 @@ class searchtoolsearch extends we_search{
 
 		switch($table){
 			default:
-				while($_db->next_record()) {
+				while($_db->next_record()){
 					$res[$_db->f('ID')] = $_db->f($field);
 				}
 				break;
 			case FILE_TABLE:
-				while($_db->next_record()) {
-					$res[$_db->f('ID')] = ($_db->f($field) == "" ? $_db->f($field2) : $_db->f($field));
+				while($_db->next_record()){
+					$res[$_db->f('ID')] = ($_db->f($field) == '' ? $_db->f($field2) : $_db->f($field));
 				}
 				break;
-			case (defined("OBJECT_FILES_TABLE") ? OBJECT_FILES_TABLE : 'OBJECT_FILES_TABLE'):
+			case (defined('OBJECT_FILES_TABLE') ? OBJECT_FILES_TABLE : 'OBJECT_FILES_TABLE'):
 				//search in public objects first and write them in the array
-				while($_db->next_record()) {
+				while($_db->next_record()){
 					$res[$_db->f('ID')] = $_db->f($field);
 				}
 				//search in unpublic objects and write them in the array
-				$query2 = "SELECT DocumentObject  FROM " . TEMPORARY_DOC_TABLE . " WHERE DocTable = 'tblObjectFiles' AND Active = 1";
+				$query2 = 'SELECT DocumentObject  FROM ' . TEMPORARY_DOC_TABLE . " WHERE DocTable = 'tblObjectFiles' AND Active=1";
 				$_db->query($query2);
-				while($_db->next_record()) {
+				while($_db->next_record()){
 					$tempObj = unserialize($_db->f('DocumentObject'));
-					if(isset($tempObj[0]["Category"]) && $tempObj[0]["Category"] != ""){
-						if(!array_key_exists($tempObj[0]["ID"], $res)){
-							$res[$tempObj[0]["ID"]] = $tempObj[0]["Category"];
+					if(isset($tempObj[0]['Category']) && $tempObj[0]['Category'] != ''){
+						if(!array_key_exists($tempObj[0]['ID'], $res)){
+							$res[$tempObj[0]['ID']] = $tempObj[0]['Category'];
 						}
 					}
 				}
@@ -355,16 +355,16 @@ class searchtoolsearch extends we_search{
 		foreach($res2 as $k => $v){
 			foreach($v as $v2){
 				//look if the value is numeric
-				if(preg_match("=^[0-9]+$=i", $v2)){
+				if(preg_match('=^[0-9]+$=i', $v2)){
 					if($v2 == $keyword){
-						$where .= ($i > 0 ? " OR " : " AND (") . ' ' . $_db->escape($table) . ".ID = " . intval($k);
+						$where .= ($i > 0 ? ' OR ' : ' AND (') . ' ' . $_db->escape($table) . '.ID=' . intval($k);
 						$i++;
 					}
 				}
 			}
 		}
 
-		$where .= ($where != "" ? " )" : ' 0 ');
+		$where .= ($where != '' ? ' )' : ' 0 ');
 		return $where;
 	}
 
@@ -372,34 +372,34 @@ class searchtoolsearch extends we_search{
 		$userIDs = array();
 		$_db = new DB_WE();
 		switch($searchFields){
-			case "CreatorName":
+			case 'CreatorName':
 				$_table = USER_TABLE;
-				$field = "Text";
-				$fieldFileTable = "CreatorID";
+				$field = 'Text';
+				$fieldFileTable = 'CreatorID';
 				break;
-			case "WebUserName":
+			case 'WebUserName':
 				$_table = CUSTOMER_TABLE;
-				$field = "Username";
-				$fieldFileTable = "WebUserID";
+				$field = 'Username';
+				$fieldFileTable = 'WebUserID';
 				break;
 		}
 
 		if(isset($searchlocation)){
 			switch($searchlocation){
-				case "END" :
+				case 'END' :
 					$searching = " LIKE '%" . $_db->escape($keyword) . "' ";
 					break;
-				case "START" :
+				case 'START' :
 					$searching = " LIKE '" . $_db->escape($keyword) . "%' ";
 					break;
-				case "IS" :
+				case 'IS' :
 					$searching = " = '" . $_db->escape($keyword) . "' ";
 					break;
-				case "<" :
-				case "<=" :
-				case ">" :
-				case ">=" :
-					$searching = " " . $searchlocation . " '" . $_db->escape($keyword) . "' ";
+				case '<' :
+				case '<=' :
+				case '>' :
+				case '>=' :
+					$searching = ' ' . $searchlocation . " '" . $_db->escape($keyword) . "' ";
 					break;
 				default :
 					$searching = " LIKE '%" . $_db->escape($keyword) . "%' ";
@@ -408,7 +408,7 @@ class searchtoolsearch extends we_search{
 		}
 
 		$_db->query('SELECT ID FROM ' . $_db->escape($_table) . ' WHERE ' . $field . ' ' . $searching);
-		while($_db->next_record()) {
+		while($_db->next_record()){
 			$userIDs[] = ($_db->f('ID'));
 		}
 
@@ -482,7 +482,7 @@ class searchtoolsearch extends we_search{
 			$_ids = array();
 			$db->query('SELECT ID, modifications FROM ' . VERSIONS_TABLE . " WHERE modifications != '' ");
 
-			while($db->next_record()) {
+			while($db->next_record()){
 				$modifications[$db->f('ID')] = makeArrayFromCSV($db->f('modifications'));
 			}
 			$m = 0;
@@ -509,7 +509,7 @@ class searchtoolsearch extends we_search{
 							foreach($v as $key => $val){
 								if(!in_array($val, $_ids[0])){
 									unset($_ids[0][$val]);
-								} else{
+								} else {
 									$arr[] = $val;
 								}
 							}
@@ -519,7 +519,7 @@ class searchtoolsearch extends we_search{
 						$where .= ' AND ' . $table . '.ID IN (' . makeCSVFromArray($arr) . ') ';
 					} elseif(!empty($_ids[0])){
 						$where .= ' AND ' . $table . '.ID IN (' . makeCSVFromArray($_ids[0]) . ') ';
-					} else{
+					} else {
 						$where .= ' AND 0';
 					}
 				}
@@ -537,13 +537,13 @@ class searchtoolsearch extends we_search{
 			case TEMPLATES_TABLE:
 				$_db->query('SELECT a.Name, b.Dat, a.DID FROM ' . LINK_TABLE . " a LEFT JOIN " . CONTENT_TABLE . " b on (a.CID = b.ID) WHERE b.Dat LIKE '%" . escape_sql_query(
 						trim($keyword)) . "%' AND a.Name!='completeData' AND a.DocumentTable='" . escape_sql_query(stripTblPrefix($table)) . "'");
-				while($_db->next_record()) {
+				while($_db->next_record()){
 					$contents[] = $_db->f('DID');
 				}
 
 				if($table == FILE_TABLE){
 					$_db->query('SELECT DocumentID, DocumentObject  FROM ' . TEMPORARY_DOC_TABLE . " WHERE DocumentObject LIKE '%" . escape_sql_query(trim($keyword)) . "%' AND DocTable = '" . escape_sql_query(stripTblPrefix($table)) . "' AND Active = 1");
-					while($_db->next_record()) {
+					while($_db->next_record()){
 						$contents[] = $_db->f('DocumentID');
 					}
 				}
@@ -551,7 +551,7 @@ class searchtoolsearch extends we_search{
 				return (!empty($contents) ? "  " . $table . ".ID IN (" . makeCSVFromArray($contents) . ")" : '');
 			case VERSIONS_TABLE:
 				$_db->query('SELECT ID, documentElements  FROM ' . VERSIONS_TABLE);
-				while($_db->next_record()) {
+				while($_db->next_record()){
 					$tempDoc[0]['elements'] = unserialize(html_entity_decode(urldecode($_db->f('documentElements')), ENT_QUOTES));
 
 					foreach($tempDoc[0]['elements'] as $k => $v){
@@ -573,18 +573,18 @@ class searchtoolsearch extends we_search{
 				$_classes = $_db->getAll(true);
 
 				//published objects
-				for($i = 1; $i <= count($_classes); $i++){
+				foreach($_classes as $i){
 					$_obj_table = OBJECT_X_TABLE . $i;
 					//$_obj_table = strtolower($_obj_table);
 					$tableInfo = $_db->metadata($_obj_table);
 					$fields = array();
 					for($c = 0; $c < count($tableInfo); $c++){
-						if(preg_match('/(.+?)_(.*)/', $tableInfo[$c]["name"], $regs)){
-							if($regs[1] != "OF" && $regs[1] != "variant"){
+						if(preg_match('/(.+?)_(.*)/', $tableInfo[$c]['name'], $regs)){
+							if($regs[1] != 'OF' && $regs[1] != 'variant'){
 								$fields[] = array(
-									"name" => $tableInfo[$c]["name"],
-									"type" => $regs[1],
-									"length" => $tableInfo[$c]["len"]
+									'name' => $tableInfo[$c]['name'],
+									'type' => $regs[1],
+									'length' => $tableInfo[$c]['len']
 								);
 							}
 						}
@@ -605,13 +605,13 @@ class searchtoolsearch extends we_search{
 					}
 
 					$_db->query('SELECT ' . escape_sql_query($_obj_table) . '.OF_ID FROM ' . escape_sql_query($_obj_table) . ' WHERE ' . $where);
-					while($_db->next_record()) {
+					while($_db->next_record()){
 						$Ids[] = $_db->f('OF_ID');
 					}
 				}
 				//only saved objects
 				$_db->query('SELECT DocumentID, DocumentObject  FROM ' . TEMPORARY_DOC_TABLE . " WHERE DocumentObject LIKE '%" . escape_sql_query(trim($keyword)) . "%' AND DocTable = 'tblObjectFiles' AND Active = 1");
-				while($_db->next_record()) {
+				while($_db->next_record()){
 					$Ids[] = $_db->f('DocumentID');
 				}
 
@@ -622,12 +622,12 @@ class searchtoolsearch extends we_search{
 	}
 
 	function selectFromTempTable($searchstart, $anzahl, $order){
-		$sortIsNr = "DESC";
-		$sortNr = "";
+		$sortIsNr = 'DESC';
+		$sortNr = '';
 		$sortierung = explode(' ', $order);
 		if(isset($sortierung[1])){
-			$sortIsNr = "";
-			$sortNr = "DESC";
+			$sortIsNr = '';
+			$sortNr = 'DESC';
 		}
 
 		$this->db->query('SELECT SEARCH_TEMP_TABLE.*,LOWER(' . $sortierung[0] . ') AS lowtext, ABS(' . $sortierung[0] . ') as Nr, (' . $sortierung[0] . " REGEXP '^[0-9]') as isNr  FROM SEARCH_TEMP_TABLE  ORDER BY IsFolder DESC, isNr " . $sortIsNr . ',Nr ' . $sortNr . ',lowtext ' . $sortNr . ', ' . $order . '  LIMIT ' . $searchstart . "," . $anzahl);
@@ -655,12 +655,12 @@ class searchtoolsearch extends we_search{
 				$titles = array();
 				//first check published documents
 				$this->db->query('SELECT a.Name, b.Dat, a.DID FROM `' . LINK_TABLE . '` a LEFT JOIN `' . CONTENT_TABLE . '` b on (a.CID = b.ID) WHERE a.Name="Title" AND NOT a.DocumentTable="' . TEMPLATES_TABLE . '"');
-				while($this->db->next_record()) {
+				while($this->db->next_record()){
 					$titles[$this->db->f('DID')] = $this->db->f('Dat');
 				}
 				//check unpublished documents
 				$this->db->query('SELECT DocumentID, DocumentObject  FROM `' . TEMPORARY_DOC_TABLE . '` WHERE DocTable = "tblFile" AND Active = 1 ' . $tmpTableWhere);
-				while($this->db->next_record()) {
+				while($this->db->next_record()){
 					$tempDoc = unserialize($this->db->f('DocumentObject'));
 					if(isset($tempDoc[0]['elements']['Title'])){
 						$titles[$this->db->f('DocumentID')] = $tempDoc[0]['elements']['Title']['dat'];
@@ -813,7 +813,7 @@ class searchtoolsearch extends we_search{
 							} elseif($this->table == VERSIONS_TABLE){
 								$sql .= $this->sqlwhere($tablename . '.TemplateID', $searching, $operator . ' ');
 							}
-						} else{
+						} else {
 							$sql .= $this->sqlwhere($searchfield, $searching, $operator);
 						}
 					} elseif($searchfield == 'temp_doc_type' && $this->table == FILE_TABLE){
@@ -859,7 +859,7 @@ class searchtoolsearch extends we_search{
 								}
 							}
 						}
-					} else{
+					} else {
 						if(isset($searchlocation)){
 							switch($searchlocation){
 								case "END":
@@ -906,7 +906,7 @@ class searchtoolsearch extends we_search{
 				}
 				return ' AND ' . $table . '.ParentID IN (' . $ids . ')';
 			}
-		} else{
+		} else {
 			$childsOfFolderId = $this->getChildsOfParentId($folderID, $table);
 			$ids = makeCSVFromArray($childsOfFolderId);
 
@@ -918,7 +918,7 @@ class searchtoolsearch extends we_search{
 		$DB_WE = new DB_WE();
 
 		$DB_WE->query('SELECT ID FROM ' . $DB_WE->escape($table) . ' WHERE ParentID=' . intval($folderID) . ' AND IsFolder=1');
-		while($DB_WE->next_record()) {
+		while($DB_WE->next_record()){
 			$_SESSION['weS']['weSearch']['countChilds'][] = $DB_WE->f('ID');
 			$this->getChildsOfParentId($DB_WE->f('ID'), $table);
 		}
