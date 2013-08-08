1beb73623214f2bb65129a1adc27fd4ea55865e4
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2013-08-08 13:19:12 +0000

    inlineedit=false back in old wysiwyg

--- webEdition/we/include/we_classes/we_wysiwyg.class.inc.php
+++ webEdition/we/include/we_classes/we_wysiwyg.class.inc.php
@@ -1108,8 +1108,8 @@ function weWysiwygSetHiddenText(arg) {
 			$fns .= str_replace(",", ";", $fn) . ",";
 		}
 		$js_function = $this->isFrontendEdit ? 'open_wysiwyg_win' : 'we_cmd';
-		$param4 = !$this->isFrontendEdit ? 'value' : we_cmd_enc('frontend');
-		
+		$param4 = !$this->isFrontendEdit ? (self::$editorType !== 'tinyMCE' ? we_cmd_enc($value) : '') : we_cmd_enc('frontend');
+
 		return we_button::create_button("image:btn_edit_edit", "javascript:" . $js_function . "('open_wysiwyg_window', '" . $this->name . "','" . max(220, $this->width) . "', '" . $this->height . "','" . $param4 . "','" . $this->propstring . "','" . $this->className . "','" . rtrim($fns, ',') . "',
 			'" . $this->outsideWE . "','" . $tbwidth . "','" . $tbheight . "','" . $this->xml . "','" . $this->removeFirstParagraph . "','" . $this->bgcol . "','" . $this->baseHref . "','" . $this->charset . "','" . $this->cssClassesCSV . "','" . $this->Language . "','" . we_cmd_enc($this->contentCss) . "',
 			'" . $this->origName . "','" . we_cmd_enc($this->tinyParams) . "','" . we_cmd_enc($this->restrictContextmenu) . "', 'true', '" . $this->isFrontendEdit . "');", true, 25);
