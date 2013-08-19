2703a72730a1cdb75a49759c3105429aa5279e1a
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-08-19 13:20:23 +0000

    remove old main-develop

--- branches/main-develop/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotFound.inc.php
+++
@@ -1,2 +0,0 @@
-<?php
-include(__DIR__ . '/we_tag_' . str_replace('Not', '', $tagName) . '.inc.php');
--- branches/main-develop/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotNew.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-//NOTE you are inside the constructor of weTagData.class.php
-include(__DIR__ . '/we_tag_' . str_replace('Not', '', $tagName) . '.inc.php');
