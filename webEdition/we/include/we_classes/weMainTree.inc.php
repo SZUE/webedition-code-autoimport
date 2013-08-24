a10a7b40990b346b0cddafd38a7b5a4e95e0f17a
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-08-24 20:47:18 +0000

    Fix #7928

--- webEdition/we/include/we_classes/weMainTree.inc.php
+++ webEdition/we/include/we_classes/weMainTree.inc.php
@@ -123,8 +123,7 @@ function doClick(id){
 	}
 
 	function getJSUpdateTreeScript($doc, $select = true){
-		$published = ((($doc->Published != 0) && ($doc->Published < $doc->ModDate) && ($doc->ContentType == "text/html" || $doc->ContentType == "text/webedition" || $doc->ContentType == "objectFile")) ? -1 : $doc->Published);
-
+		$published = ((isset($doc->Published)&&($doc->Published != 0) && ($doc->Published >= $doc->ModDate) && ($doc->ContentType == 'text/html' || $doc->ContentType == 'text/webedition' || $doc->ContentType == 'objectFile')) ? $doc->Published:-1);
 		//	This is needed in SeeMode
 		$s = '
 isEditInclude = false;
