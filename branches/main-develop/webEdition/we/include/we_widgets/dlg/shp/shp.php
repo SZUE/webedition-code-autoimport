60d164d2f390541c832446f96b2fdf024bc03dd9
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2013-09-02 23:02:56 +0000

    FR #7963 Shop Cockpit Widget

---
+++ branches/main-develop/webEdition/we/include/we_widgets/dlg/shp/shp.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * webEdition CMS
+ *
+ * $Rev: 6489 $
+ * $Author: mokraemer $
+ * $Date: 2013-08-19 15:19:40 +0200 (Mon, 19 Aug 2013) $
+ *
+ * This source is part of webEdition CMS. webEdition CMS is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the textfile
+ * webEdition/licenses/webEditionCMS/License.txt
+ *
+ * @category   webEdition
+ * @package    webEdition_base
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL
+ */
+// widget LAST MODIFIED
+
+$aCols = $_REQUEST['we_cmd'];
+require_once('../../shp/shp.php');
+
+$sJsCode = "
+var _sObjId='" . $_REQUEST['we_cmd'][0] . "';
+var _sType='shp';
+var _sTb='"Shop"';
+
+function init(){
+	parent.rpcHandleResponse(_sType,_sObjId,document.getElementById(_sType),_sTb);
+}";
+
+print we_html_element::htmlDocType() . we_html_element::htmlHtml(
+		we_html_element::htmlHead(
+			we_html_tools::getHtmlInnerHead(g_l('cockpit', '[shop_dashboard][headline]').'&nbsp;'. $interval . STYLESHEET . we_html_element::jsElement(
+				$sJsCode)) . we_html_element::htmlBody(
+			array(
+			"marginwidth" => 15,
+			"marginheight" => 10,
+			"leftmargin" => 15,
+			"topmargin" => 10,
+			"onload" => "if(parent!=self)init();"
+			), we_html_element::htmlDiv(array(
+				"id" => "shp"
+				), we_html_element::htmlDiv(array('id' => 'shp_data'), $shopDashboard)
+)));
+
