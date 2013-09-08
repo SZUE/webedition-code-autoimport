bca7751ef3e8452451b02e835d310be709fa8bcc
arminschulz <arminschulz@5781a423-4729-0410-aacd-f88070b95415>
2013-09-08 05:22:47 +0000

    Bug 7989 Nachtrag

--- webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotTop.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotTop.inc.php
@@ -1,2 +1,2 @@
 <?php
-include(WE_INCLUDES_DIR . '/weTagWizard/we_tags/we_tag_' . str_replace('Not', '', $tagName) . '.inc.php');
+include(WE_INCLUDES_PATH . 'weTagWizard/we_tags/we_tag_' . str_replace('Not', '', $tagName) . '.inc.php');
