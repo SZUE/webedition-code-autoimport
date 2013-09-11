2bd76d5e2a423d74e903e2dd72ecafa41784977b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-09-11 10:41:42 +0000

    Fix eol-/mime-style

--- externals/additional/scripts/eol-style/eol.sh
+++ externals/additional/scripts/eol-style/eol.sh
@@ -2,13 +2,18 @@
 DIR=`pwd`/../../..
 DIR=`readlink -f ${DIR}`
 
-find ${DIR} \( -path ${DIR}/webEdition/lib/Zend -o -path ${DIR}/webEdition/lib/phpMailer \) -prune -o -name \*.php -exec svn propset svn:keywords "Date Author Revision" {} \;|tee log.txt
-find ${DIR} -name \*.js -exec svn propset svn:keywords "Date Author Revision" {} \;|tee -a log.txt
-find ${DIR} -name \*.php -exec svn propset svn:eol-style native {} \;|tee -a log.txt
-find ${DIR} -name \*.html -exec svn propset svn:eol-style native {} \;|tee -a log.txt
-find ${DIR} -name \*.js -exec svn propset svn:eol-style native {} \;|tee -a log.txt
-find ${DIR} -name \*.css -exec svn propset svn:eol-style native {} \;|tee -a log.txt
-find ${DIR} -name \*.java -exec svn propset svn:eol-style native {} \;|tee -a log.txt
-find ${DIR} -name \*.xml -exec svn propset svn:eol-style native {} \;|tee -a log.txt
-find ${DIR} -name .htaccess -exec svn propset svn:eol-style native {} \;|tee -a log.txt
-find ${DIR} -name \*.txt -exec svn propset svn:eol-style native {} \;|tee -a log.txt
+find ${DIR} \( -path ${DIR}/webEdition/lib/Zend -o -path ${DIR}/webEdition/lib/phpMailer \) -prune -o -name \*.php -exec svn propset svn:keywords "Date Author Revision" {} \;> log.txt
+find ${DIR} -name \*.js -exec svn propset svn:keywords "Date Author Revision" {} \;>> log.txt
+find ${DIR} -name \*.php -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name \*.html -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name \*.js -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name \*.css -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name \*.java -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name \*.xml -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name \*.svg -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name .htaccess -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name .sh -exec svn propset svn:eol-style LF svn:executable {} \;>> log.txt
+find ${DIR} -name \*.txt -exec svn propset svn:eol-style native {} \;>> log.txt
+find ${DIR} -name \*.png -exec svn propset svn:mime-type image/png {} \;>> log.txt
+find ${DIR} -name \*.gif -exec svn propset svn:mime-type image/gif {} \;>> log.txt
+find ${DIR} -name \*.jpg -exec svn propset svn:mime-type image/jpeg {} \;>> log.txt
