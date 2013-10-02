6bebd25915c890cd2bca8419ef0cab3a469cf36f
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2013-10-02 14:45:11 +0000

    fix #8014

--- webEdition/we/include/we_classes/we_fileselector.inc.php
+++ webEdition/we/include/we_classes/we_fileselector.inc.php
@@ -141,8 +141,9 @@ class we_fileselector{
 	}
 
 	function query(){
+		$wsQuery = $this->table == NAVIGATION_TABLE && get_ws($this->table) ? ' ' . getWsQueryForSelector($this->table) : '';
 		$this->db->query('SELECT ' . $this->fields . ' FROM ' . $this->db->escape($this->table) . ' WHERE ParentID=' . intval($this->dir) . ' ' .
-			( ($this->filter != '' ? ($this->table == CATEGORY_TABLE ? 'AND IsFolder = "' . $this->db->escape($this->filter) . '" ' : 'AND ContentType = "' . $this->db->escape($this->filter) . '" ') : '' ) ) .
+			( ($this->filter != '' ? ($this->table == CATEGORY_TABLE ? 'AND IsFolder = "' . $this->db->escape($this->filter) . '" ' : 'AND ContentType = "' . $this->db->escape($this->filter) . '" ') : '' ) . $wsQuery ) .
 			($this->order ? (' ORDER BY ' . $this->order) : ''));
 		$_SESSION['weS']['we_fs_lastDir'][$this->table] = $this->dir;
 	}
