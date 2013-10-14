8a77fc908045cbb620dc48b01733ac1542a036da
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-10-14 20:48:34 +0000

    zu #164

--- trunk/webEdition/we/include/we_widgets/inc/sct.inc.php
+++ trunk/webEdition/we/include/we_widgets/inc/sct.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * webEdition CMS
  *
@@ -21,21 +22,15 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
 $oTblCont = new we_html_table(array(
 	"border" => 0, "cellpadding" => 0, "cellspacing" => 0
-), 1, 1);
+	), 1, 1);
 $oTblCont->setCol(
-		0,
-		0,
-		null,
-		we_html_element::htmlDiv(
-				array(
-
-						"id" => "m_" . $iCurrId . "_inline",
-						"style" => "width:" . $iWidth . "px;height:" . ($aPrefs[$aProps[0]]["height"] - 25) . "px;overflow:auto;"
-				),
-				$sc->getHtml()));
+	0, 0, null, we_html_element::htmlDiv(
+		array(
+		"id" => "m_" . $iCurrId . "_inline",
+		"style" => "width:" . $iWidth . "px;height:" . ($aPrefs[$aProps[0]]["height"] - 25) . "px;overflow:auto;"
+		), $sc->getHtml()));
 $aLang = array(
-	g_l('cockpit','[shortcuts]'), ''
+	g_l('cockpit', '[shortcuts]'), ''
 );
