e0d535f4227a8e3e6e71644f8ba0f979bb5504b0
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2013-11-13 12:12:08 +0000

    #164

--- trunk/webEdition/we/include/we_widgets/mod/fdl.php
+++ trunk/webEdition/we/include/we_widgets/mod/fdl.php
@@ -22,6 +22,7 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we.inc.php');
 
 if(!(defined('CUSTOMER_TABLE') && we_hasPerm('CAN_SEE_CUSTOMER'))){
 	return;
