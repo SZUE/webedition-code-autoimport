dd1396fdc3cda48c3cc4c03080623cbbea19fc3f
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-11-20 21:45:30 +0000

    Fehlerhafte version.php

--- trunk/webEdition/we/include/we_version.php
+++ trunk/webEdition/we/include/we_version.php
@@ -1,14 +1,4 @@
 <?php
-/*
-define("WE_VERSION","6.3.8.0");
-define("WE_VERSION_SUPP","release");
-define('WE_ZFVERSION','1.12.1'); // recommended version of the Zend Framework (bundled with webEdition)
-define("WE_SVNREV","6985");
-define("WE_VERSION_SUPP_VERSION","0");
-define("WE_VERSION_BRANCH","trunk");
-define("WE_VERSION_NAME","6.3.8");
-*/
-
 define('WE_VERSION', '6.3.8.1');
 define('WE_VERSION_SUPP','');
 define('WE_ZFVERSION','1.12.1'); // recommended version of the Zend Framework (bundled with webEdition)
