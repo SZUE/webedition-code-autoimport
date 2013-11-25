4e227db7dfe743673b885607167e2cc01751fe30
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-11-25 20:53:11 +0000

    Cockpit optimierung

--- trunk/webEdition/we/include/we_widgets/inc/upb.inc.php
+++ trunk/webEdition/we/include/we_widgets/inc/upb.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * webEdition CMS
  *
@@ -21,24 +22,18 @@
  * @package    webEdition_base
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
 $oTblCont = new we_html_table(array(
 	"border" => 0, "cellpadding" => 0, "cellspacing" => 0
-), 1, 1);
+		), 1, 1);
 $oTblCont->setCol(
-		0,
-		0,
-		null,
-		we_html_element::htmlDiv(
+		0, 0, null, we_html_element::htmlDiv(
 				array(
-
-						"id" => "m_" . $iCurrId . "_inline",
-						"style" => "width:" . $iWidth . "px;height:" . ($aPrefs[$aProps[0]]["height"] - 25) . "px;overflow:auto;"
-				),
-				$ct));
-$bTypeDoc = (bool)$aProps[3]{0};
-$bTypeObj = (bool)$aProps[3]{1};
-$sTb = ($bTypeDoc && $bTypeObj) ? g_l('cockpit',"[upb_docs_and_objs]") : (($bTypeDoc) ? g_l('cockpit',"[upb_docs]") : (($bTypeObj) ? g_l('cockpit',"[upb_objs]") : g_l('cockpit',"[upb_docs_and_objs]")));
+			"id" => "m_" . $iCurrId . "_inline",
+			"style" => "width:" . $iWidth . "px;height:" . ($aPrefs[$aProps[0]]["height"] - 25) . "px;overflow:auto;"
+				), $ct));
+$bTypeDoc = (bool) $aProps[3]{0};
+$bTypeObj = (bool) $aProps[3]{1};
+$sTb = ($bTypeDoc && $bTypeObj) ? g_l('cockpit', "[upb_docs_and_objs]") : (($bTypeDoc) ? g_l('cockpit', "[upb_docs]") : (($bTypeObj) ? g_l('cockpit', "[upb_objs]") : g_l('cockpit', "[upb_docs_and_objs]")));
 $aLang = array(
 	$sTb, ""
 );
--- trunk/webEdition/we/include/we_widgets/mod/upb.php
+++ trunk/webEdition/we/include/we_widgets/mod/upb.php
@@ -42,18 +42,17 @@ if($bTypeDoc && $bTypeObj){
 	if($bTypeDoc && defined("FILE_TABLE")){
 		$tbls[] = FILE_TABLE;
 	}
-	if($bTypeObj && defined("OBJECT_FILES_TABLE")&&we_hasPerm("CAN_SEE_OBJECTFILES")){
+	if($bTypeObj && defined("OBJECT_FILES_TABLE") && we_hasPerm("CAN_SEE_OBJECTFILES")){
 		$tbls[] = OBJECT_FILES_TABLE;
 	}
 }
 
 $_cont = array();
+$db = new DB_WE();
 foreach($tbls as $table){
-	$wfDocsCSV = "";
-	$myWfDocsCSV = "";
-	if(defined("WORKFLOW_TABLE")){
-		$myWfDocsArray = we_workflow_utility::getWorkflowDocsForUser(
-				$_SESSION["user"]["ID"], $table, we_hasPerm("ADMINISTRATOR"), we_hasPerm("PUBLISH"), ($table == $_objectFilesTable) ? "" : get_ws($table));
+	$wfDocsCSV = $myWfDocsCSV = '';
+	if(defined('WORKFLOW_TABLE')){
+		$myWfDocsArray = we_workflow_utility::getWorkflowDocsForUser($_SESSION["user"]["ID"], $table, we_hasPerm("ADMINISTRATOR"), we_hasPerm("PUBLISH"), ($table == $_objectFilesTable) ? '' : get_ws($table));
 		$myWfDocsCSV = makeCSVFromArray($myWfDocsArray);
 		$wfDocsArray = we_workflow_utility::getAllWorkflowDocs($table);
 		$wfDocsCSV = makeCSVFromArray($wfDocsArray);
@@ -65,92 +64,73 @@ foreach($tbls as $table){
 	#### get workspace query ###
 
 
-	$parents = array();
-	$childs = array();
-	$parentlist = "";
-	$childlist = "";
-	$wsQuery = "";
+	$parents = $childs = array();
+	$parentlist = $childlist = '';
 
-	if($table == FILE_TABLE)
+	if($table == FILE_TABLE){
 		if(($ws = get_ws($table))){
 			$wsArr = makeArrayFromCSV($ws);
 			foreach($wsArr as $i){
 				$parents[] = $i;
 				$childs[] = $i;
-				we_readParents($i, $parents, $table);
-				we_readChilds($i, $childs, $table);
+				we_readParents($i, $parents, $table, 'ContentType', 'folder', $db);
+				we_readChilds($i, $childs, $table, true, '', 'ContentType', 'folder', $db);
 			}
 			$childlist = makeCSVFromArray($childs);
 			$parentlist = makeCSVFromArray($parents);
-			if($parentlist)
-				$wsQuery = " ID IN(" . $parentlist . ") ";
-			if($parentlist && $childlist)
-				$wsQuery .= " OR ";
-			if($childlist)
-				$wsQuery .= " ParentID IN(" . $childlist . ") ";
-			if($wsQuery)
-				$wsQuery = " AND (" . $wsQuery . ") ";
+			$wsQuery = ($parentlist ? ' t.ID IN(' . $parentlist . ') ' . ($childlist ? ' OR ' : '') : '') .
+					($childlist ? ' t.ParentID IN(' . $childlist . ') ' : '');
 		}
+	}
 
 	#####
+	$sqld = g_l('weEditorInfo', '[mysql_date_only_format]');
 
+	$s = 'SELECT ' . ($wfDocsCSV ? "(t.ID IN($wfDocsCSV)) AS wforder," : '') . ' ' . ($myWfDocsCSV ? "(t.ID IN($myWfDocsCSV)) AS mywforder," : '') . ' '
+			. 't.ContentType,t.ID,t.Text,t.ParentID,t.Path,t.Icon,t.ModDate,'
+			. 'IF(t.Published>0,FROM_UNIXTIME(t.Published,"' . $sqld . '"),"-") AS Published,'
+			. 'IF(t.ModDate>0,FROM_UNIXTIME(t.ModDate,"' . $sqld . '"),"-") AS Modified,'
+			. 'IF(t.CreationDate>0,FROM_UNIXTIME(t.CreationDate,"' . $sqld . '"),"-") AS CreationDate,'
+			. 'u2.username AS Modifier,'
+			. 'u1.username AS Creator ';
+	$q = 'FROM ' . $db->escape($table) . ' t LEFT JOIN ' . USER_TABLE . ' u1 ON u1.ID=t.CreatorID LEFT JOIN ' . USER_TABLE . ' u2 ON u2.ID=t.ModifierID ' .
+			" WHERE (((t.Published=0 OR t.Published<t.ModDate) AND t.ContentType IN ('text/webedition','text/html','objectFile'))" .
+			($myWfDocsCSV ? ' OR (t.ID IN(' . $myWfDocsCSV . ')) ' : '') . ')' .
+			(isset($wsQuery) ? ' AND (' . $wsQuery . ') ' : '');
+	$order = ' ORDER BY ' . ($myWfDocsCSV ? 'mywforder DESC,' : '') . $order;
 
-	$q = "
-			SELECT " . ($wfDocsCSV ? "(ID IN($wfDocsCSV)) AS wforder," : "") . " " . ($myWfDocsCSV ? "(ID IN($myWfDocsCSV)) AS mywforder," : "") . " ContentType,ID,Text,ParentID,Path,Published,ModDate,CreationDate,ModifierID,CreatorID,Icon
-			FROM " . $DB_WE->escape($table) . "
-			WHERE (((Published=0 OR Published < ModDate) AND (ContentType='text/webedition' OR ContentType='text/html' OR ContentType='objectFile'))" . ($myWfDocsCSV ? " OR (ID IN($myWfDocsCSV)) " : "") . ") $wsQuery
-			ORDER BY " . ($myWfDocsCSV ? "mywforder DESC," : "") . " $order";
+	$anz = f('SELECT COUNT(1) AS a ' . $q, 'a', $db);
 
-	$DB_WE->query($q);
-	$anz = $DB_WE->num_rows();
-	$DB_WE->query($q . " LIMIT " . intval($offset) . "," . intval($numRows));
-	$db2 = new DB_WE();
+	$db->query($s . $q . $order . ' LIMIT ' . intval($offset) . ',' . intval($numRows));
 	$content = array();
-
-	while($DB_WE->next_record()){
-		$row = array();
-		$_cont[$DB_WE->f("ModDate")] = $path = '<tr><td width="20" height="20" valign="middle" nowrap><img src="' . ICON_DIR . $DB_WE->f(
-				"Icon") . '" width="16" height="18" />' . we_html_tools::getPixel(4, 1) . '</td><td valign="middle" class="middlefont"><nobr><a href="javascript:top.weEditorFrameController.openDocument(\'' . $table . '\',\'' . $DB_WE->f(
-				"ID") . '\',\'' . $DB_WE->f("ContentType") . '\')" title="' . $DB_WE->f("Path") . '" style="color:' . ($DB_WE->f(
-				"Published") ? "#3366CC" : "#FF0000") . ';text-decoration:none;">' . $DB_WE->f("Path") . '</a></nobr></td></tr>';
-		$row[] = array(
-			"dat" => $path
-		);
-		$usern = f("SELECT username FROM " . USER_TABLE . " WHERE ID=" . intval($DB_WE->f("CreatorID")), "username", $db2);
-		$usern = $usern ? $usern : "-";
-		$row[] = array(
-			"dat" => $usern
+	while($db->next_record()){
+		$_cont[$db->f("ModDate")] = $path = '<tr><td width="20" height="20" valign="middle" nowrap><img src="' . ICON_DIR . $db->f("Icon") .
+				'" width="16" height="18" />' . we_html_tools::getPixel(4, 1) . '</td><td valign="middle" class="middlefont"><nobr><a href="javascript:top.weEditorFrameController.openDocument(\'' . $table . '\',\'' .
+				$db->f("ID") . '\',\'' . $db->f("ContentType") . '\')" title="' . $db->f("Path") . '" style="color:' . ($db->f("Published") != '-' ? "#3366CC" : "#FF0000") . ';text-decoration:none;">' . $db->f("Path") . '</a></nobr></td></tr>';
+		$row = array(
+			array("dat" => $path),
+			array("dat" => $db->f("Creator") ? $db->f("Creator") : '-'),
+			array("dat" => $db->f('CreationDate')),
+			array("dat" => $db->f("Modifier") ? $db->f("Modifier") : '-'),
+			array("dat" => $db->f("Modified")),
+			array("dat" => $db->f("Published")),
 		);
 
-		$foo = $DB_WE->f("CreationDate") ? date(g_l('date', '[format][default]'), $DB_WE->f("CreationDate")) : "-";
-		$row[] = array("dat" => $foo);
-		$usern = f("
-				SELECT username
-				FROM " . USER_TABLE . "
-				WHERE ID=" . intval($DB_WE->f("ModifierID")), "username", $db2);
-		$usern = $usern ? $usern : "-";
-		$row[] = array("dat" => $usern);
 
-		$foo = $DB_WE->f("ModDate") ? date(g_l('date', '[format][default]'), $DB_WE->f("ModDate")) : "-";
-		$row[] = array("dat" => $foo);
-		$foo = $DB_WE->f("Published") ? date(g_l('date', '[format][default]'), $DB_WE->f("Published")) : "-";
-		$row[] = array("dat" => $foo);
-		if(defined("WORKFLOW_TABLE"))
-			if($DB_WE->f("wforder")){
-				$step = we_workflow_utility::findLastActiveStep($DB_WE->f("ID"), $table) + 1;
-				$steps = count(we_workflow_utility::getNumberOfSteps($DB_WE->f("ID"), $table));
-				$text = "$step&nbsp;" . g_l('resave', '[of]') . "&nbsp;$steps";
-				$text .= '&nbsp;<img src="' . IMAGE_DIR . 'we_boebbel_' . ($DB_WE->f("mywforder") ? 'blau' : 'grau') . '.gif" align="absmiddle" />';
-				$row[] = array("dat" => $text);
+		if(defined("WORKFLOW_TABLE")){
+			if($db->f("wforder")){
+				$step = we_workflow_utility::findLastActiveStep($db->f("ID"), $table) + 1;
+				$steps = count(we_workflow_utility::getNumberOfSteps($db->f("ID"), $table));
+				$row[] = array("dat" => $step . '&nbsp;' . g_l('resave', '[of]') . '&nbsp;' . $steps . '&nbsp;<img src="' . IMAGE_DIR . 'we_boebbel_' . ($db->f("mywforder") ? 'blau' : 'grau') . '.gif" align="absmiddle" />');
 			} else {
 				$row[] = array("dat" => "-");
 			}
+		}
 		$content[] = $row;
 	}
 }
 
-$ct = "";
-$ct .= "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">\n";
+$ct = "<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">\n";
 asort($_cont);
 reset($_cont);
 foreach($_cont as $k => $v){
