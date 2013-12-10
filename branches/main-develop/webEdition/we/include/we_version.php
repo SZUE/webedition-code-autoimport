d4e603b5fe75468c4510190660564795b82e600d
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-12-10 21:53:56 +0000

    new version

--- branches/main-develop/webEdition/we/include/we_version.php
+++ branches/main-develop/webEdition/we/include/we_version.php
@@ -1,6 +1,6 @@
 <?php
 
-define('WE_VERSION', '6.3.8.2');
+define('WE_VERSION', '6.3.9.2');
 define('WE_VERSION_SUPP', '');
 define('WE_ZFVERSION', '1.12.1'); // recommended version of the Zend Framework (bundled with webEdition)
 define('WE_SVNREV', '');
