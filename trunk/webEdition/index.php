a3f8b77f7fe9c4ad2d50e8b05a2a979b4b7c9eb2
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2013-12-11 21:43:37 +0000

    Fix #6628

--- trunk/webEdition/index.php
+++ trunk/webEdition/index.php
@@ -253,7 +253,7 @@ if(isset($_POST['checkLogin']) && empty($_COOKIE)){
 
 	printHeader($login, 503);
 	print we_html_element::htmlBody(array('style' => 'background-color:#FFFFFF;'), $_layout->getHtml()) . '</html>';
-} else if(isset($_POST['checkLogin']) && $_POST['checkLogin'] != session_id()){
+} else /*if(isset($_POST['checkLogin']) && $_POST['checkLogin'] != session_id()){
 	$_layout = getError(sprintf(g_l('start', '[phpini_problems]'), (ini_get('cfg_file_path') ? ' (' . ini_get('cfg_file_path') . ')' : '')) . we_html_element::htmlBr() . we_html_element::htmlBr() .
 		'Debug-Info:' . we_html_element::htmlBr() .
 		'submitted session id: ' . filterXss($_POST['checkLogin']) . we_html_element::htmlBr() .
@@ -263,7 +263,7 @@ if(isset($_POST['checkLogin']) && empty($_COOKIE)){
 	);
 	printHeader($login, 408);
 	print we_html_element::htmlBody(array('style' => 'background-color:#FFFFFF;'), $_layout->getHtml()) . '</html>';
-} else if(!$ignore_browser && !we_base_browserDetect::isSupported()){
+} else*/ if(!$ignore_browser && !we_base_browserDetect::isSupported()){
 
 	/*	 * *******************************************************************
 	 * CHECK BROWSER
