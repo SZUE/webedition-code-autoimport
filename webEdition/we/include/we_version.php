8ee187b682564f2c52723e5adc9cdf05bb2da8b6
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2013-12-13 15:09:35 +0000

    we_version

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,7 +1,6 @@
 <?php
 
 define("WE_VERSION", "6.3.8.1");
-define("WE_VERSION_SUPP", "nightly-build");
 define("WE_ZFVERSION", "1.12.3"); // recommended version of the Zend Framework (bundled with webEdition)
 define("WE_SVNREV", "");
 define("WE_VERSION_SUPP_VERSION", "0");
