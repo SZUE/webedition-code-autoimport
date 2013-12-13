93a5663ff02e27a7e66c640e1c76188f6b0c3b58
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2013-12-13 14:49:21 +0000

    we_version

--- branches/main-develop/webEdition/we/include/we_version.php
+++ branches/main-develop/webEdition/we/include/we_version.php
@@ -1,8 +1,8 @@
 <?php
 
-define('WE_VERSION', '6.3.9.2');
-define('WE_VERSION_SUPP', 'nightly-build');
-define('WE_ZFVERSION', '1.12.3'); // recommended version of the Zend Framework (bundled with webEdition)
-define('WE_SVNREV', '');
-define('WE_VERSION_SUPP_VERSION', '0');
-define('WE_VERSION_BRANCH', 'main-develop');
+define("WE_VERSION", "6.3.9.2");
+define("WE_VERSION_SUPP", "nightly-build");
+define("WE_ZFVERSION", "1.12.3"); // recommended version of the Zend Framework (bundled with webEdition)
+define("WE_SVNREV", "");
+define("WE_VERSION_SUPP_VERSION", "0");
+define("WE_VERSION_BRANCH", "main-develop");
