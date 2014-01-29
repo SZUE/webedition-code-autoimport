d0b3ed6f327e67192f7a3e57ecd21f7633f4f168
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-01-29 19:02:23 +0000

    Fix #8368

--- webEdition/we/include/we_tags/we_tag_include.inc.php
+++ webEdition/we/include/we_tags/we_tag_include.inc.php
@@ -27,8 +27,8 @@ function we_parse_tag_include($attribs, $c, array $attr){
 	$path = weTag_getParserAttribute('path', $attr);
 	return ($type != 'template' ? '<?php eval(' . we_tag_tagParser::printTag('include', $attribs) . ');?>' :
 			($path ? '<?php $we_inc=getTemplatePath(\'' . str_replace('\'', '', $path) . '\');' :
-				'<?php $we_inc=getTemplatePath(' . intval(weTag_getParserAttribute('id', $attr)) . ');') . 
-				'if($we_inc){$is_inc = isset($is_inc) ? ++$is_inc : 1; include($we_inc); $is_inc--;}?>'
+				'<?php $we_inc=getTemplatePath(' . intval(weTag_getParserAttribute('id', $attr)) . ');') .
+				'if($we_inc){$is_inc = isset($is_inc) ? ++$is_inc : 1; include($we_inc); $is_inc--;}; ?>'
 		);
 }
 
