74a459bf32481d7329438f8b63d669ffd6fcfb4b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-02-03 22:17:26 +0000

    Fix #8099

--- trunk/webEdition/we/include/we_modules/object/we_objectFile.class.php
+++ trunk/webEdition/we/include/we_modules/object/we_objectFile.class.php
@@ -721,15 +721,16 @@ class we_objectFile extends we_document{
 				case we_object::QUERY_PREFIX:
 				case self::TYPE_MULTIOBJECT . '_':
 				case self::TYPE_META . '_':
-				case (defined('WE_SHOP_VARIANTS_ELEMENT_NAME') && !$checkVariants && $arr['name'] == 'variant_' . WE_SHOP_VARIANTS_ELEMENT_NAME): //FIX 8099
-					break;
-				case (defined('WE_SHOP_VARIANTS_ELEMENT_NAME') && $checkVariants ? 'variant_' . WE_SHOP_VARIANTS_ELEMENT_NAME : '-1'):
-					$variantdata = $arr;
+				case (defined('WE_SHOP_VARIANTS_ELEMENT_NAME') ? 'variant_' . WE_SHOP_VARIANTS_ELEMENT_NAME : '-1'):
+					if($checkVariants){
+						$variantdata = $arr;
+					}
 					break;
 				default:
 					$tableInfo2[] = $arr;
 			}
 		}
+
 		if($contentOnly == false){
 			return $tableInfo2;
 		}
