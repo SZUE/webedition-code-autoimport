3f5790b6c90a076a7e491c6ae05bc16725dd950a
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-02-03 22:07:37 +0000

    Fix #8406

--- webEdition/we/include/we_tags/we_tag_css.inc.php
+++ webEdition/we/include/we_tags/we_tag_css.inc.php
@@ -23,39 +23,34 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 function we_tag_css($attribs){
-
 	if(($foo = attributFehltError($attribs, 'id', __FUNCTION__))){
 		return $foo;
 	}
-	$id = weTag_getAttribute('id', $attribs);
-	$rel = weTag_getAttribute('rel', $attribs, 'stylesheet');
 
-	$row = getHash('SELECT Path,IsFolder,IsDynamic FROM ' . FILE_TABLE . ' WHERE ID=' . intval($id), $GLOBALS['DB_WE']);
-	if(!empty($row)){
-		$url = $row['Path'] . ($row['IsFolder'] ? '/' : '');
-
-		//	remove not needed elements
-		$attribs = removeAttribs($attribs, array('id', 'rel'));
-
-		$attribs['rel'] = $rel;
-		$attribs['type'] = 'text/css';
-		$attribs['href'] = (we_isHttps() ? '' : BASE_CSS) . $url;
-
-		$nolink = false;
-		$applyto = weTag_getAttribute("applyto", $attribs, defined("CSSAPPLYTO_DEFAULT") ? CSSAPPLYTO_DEFAULT : "around");
-		switch($applyto){
-			case 'around' :
-				break;
-			case 'wysiwyg' :
-				$nolink = true;
-			case 'all' :
-				$media = weTag_getAttribute('media', $attribs);
-				if($media == "" || $media == "screen" || $media == "all"){
-					$GLOBALS['we_doc']->addDocumentCss($attribs['href'] . "?" . time());
-				}
-				break;
-		}
-		return $nolink ? '' : getHtmlTag('link', $attribs) . "\n";
+	$row = getHash('SELECT Path,IsFolder,IsDynamic FROM ' . FILE_TABLE . ' WHERE ID=' . intval(weTag_getAttribute('id', $attribs)));
+	if(empty($row)){
+		return '';
 	}
-	return '';
+
+	$nolink = false;
+	switch(weTag_getAttribute('applyto', $attribs, defined("CSSAPPLYTO_DEFAULT") ? CSSAPPLYTO_DEFAULT : 'around')){
+		case 'around' :
+			break;
+		case 'wysiwyg' :
+			$nolink = true;
+		case 'all' :
+			$media = weTag_getAttribute('media', $attribs);
+			if($media == "" || $media == "screen" || $media == "all"){
+				$GLOBALS['we_doc']->addDocumentCss($attribs['href'] . '?' . time());
+			}
+			break;
+	}
+	//	remove not needed elements
+	$attribs = removeAttribs($attribs, array('id', 'applyto'));
+
+	$attribs['rel'] = weTag_getAttribute('rel', $attribs, 'stylesheet');
+	$attribs['type'] = 'text/css';
+	$attribs['href'] = (we_isHttps() ? '' : BASE_CSS) . $row['Path'] . ($row['IsFolder'] ? '/' : '');
+
+	return $nolink ? '' : getHtmlTag('link', $attribs) . "\n";
 }
