451a2912b2ee8c69f99aef46cc85cd344e95b0da
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-03-01 19:14:12 +0000

    Fix faulty form-tag

--- trunk/webEdition/we/include/we_templates/we_image_imageedit.inc.php
+++ trunk/webEdition/we/include/we_templates/we_image_imageedit.inc.php
@@ -46,39 +46,21 @@ print STYLESHEET;
 
 		$_gdtype = $we_doc->getGDType();
 
-		$editselect = '<select name="editmenue" size="1" onchange="var cmnd = this.options[this.selectedIndex].value; if(cmnd){if(cmnd==\'doImage_convertPNG\' || cmnd==\'doImage_convertGIF\'){_EditorFrame.setEditorIsHot(true);};we_cmd(cmnd,\'' . $we_transaction . '\');}this.selectedIndex=0"' . (($we_doc->getElement("data") && we_base_imageEdit::is_imagetype_read_supported($_gdtype) && we_base_imageEdit::gd_version() > 0) ? "" : ' disabled="disabled"') . '>
+
+		echo '<table cellpadding="0" cellspacing="0" border="0">
+' . ($we_doc->EditPageNr == 15 ?
+				'<tr><td><select name="editmenue" size="1" onchange="var cmnd = this.options[this.selectedIndex].value; if(cmnd){if(cmnd==\'doImage_convertPNG\' || cmnd==\'doImage_convertGIF\'){_EditorFrame.setEditorIsHot(true);};we_cmd(cmnd,\'' . $we_transaction . '\');}this.selectedIndex=0"' . (($we_doc->getElement("data") && we_base_imageEdit::is_imagetype_read_supported($_gdtype) && we_base_imageEdit::gd_version() > 0) ? "" : ' disabled="disabled"') . '>
 <option value="">' . g_l('weClass', "[edit]") . '</option>
 <option value="image_resize">' . g_l('weClass', "[resize]") . '&hellip;</option>
 <option value="image_rotate">' . g_l('weClass', "[rotate]") . '&hellip;</option>
 <option value="image_crop">' . g_l('weClass', "[crop]") . '&hellip;</option>
-<option value="" disabled="disabled" style="color:grey">' . g_l('weClass', "[convert]") . '</option>
-' . ((in_array("jpg", we_base_imageEdit::supported_image_types())) ? '<option value="image_convertJPEG">&nbsp;&nbsp;' . g_l('weClass', "[convert_jpg]") . '...</option>' : '') . '
-' . (($_gdtype != "gif" && in_array("gif", we_base_imageEdit::supported_image_types())) ? '<option value="doImage_convertGIF">&nbsp;&nbsp;' . g_l('weClass', "[convert_gif]") . '</option>' : '') . '
-' . (($_gdtype != "png" && in_array("png", we_base_imageEdit::supported_image_types())) ? '<option value="doImage_convertPNG">&nbsp;&nbsp;' . g_l('weClass', "[convert_png]") . '</option>' : '') . '
-</select>';
-		$_html = '<table cellpadding="0" cellspacing="0" border="0">
-';
-		if($we_doc->EditPageNr == 15){
-			$_html .= '<tr>
-								<td>' . $editselect . '</td>
-							</tr>
-							<tr>
-									<td>' . we_html_tools::getPixel(2, 10) . '</td>
-							</tr>
-							<tr>
-									<td>' . we_html_tools::getPixel(2, 10) . '</td>
-							</tr>
-							';
-		}
-
-		$_html .= '
-                        <tr>
-							<td>' . $we_doc->getHtml(true) . '</td>
-						</tr>
-
-			';
-
-		$_html .= '</table>';
-
-		print $_html;
-
+<option value="" disabled="disabled" style="color:grey">' . g_l('weClass', "[convert]") . '</option>' .
+				((in_array("jpg", we_base_imageEdit::supported_image_types())) ? '<option value="image_convertJPEG">&nbsp;&nbsp;' . g_l('weClass', "[convert_jpg]") . '...</option>' : '') .
+				(($_gdtype != "gif" && in_array("gif", we_base_imageEdit::supported_image_types())) ? '<option value="doImage_convertGIF">&nbsp;&nbsp;' . g_l('weClass', "[convert_gif]") . '</option>' : '') .
+				(($_gdtype != "png" && in_array("png", we_base_imageEdit::supported_image_types())) ? '<option value="doImage_convertPNG">&nbsp;&nbsp;' . g_l('weClass', "[convert_png]") . '</option>' : '') .
+				'</select></td></tr>
+<tr><td>' . we_html_tools::getPixel(2, 10) . '</td></tr>
+<tr><td>' . we_html_tools::getPixel(2, 10) . '</td></tr>' :
+				''
+		) . '<tr><td>' . $we_doc->getHtml(true) . '</td></tr>'
+		. '</table>';
