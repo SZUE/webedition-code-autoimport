0fa0c2278aa2c9f429683f22e771abf7f18ea8a2
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-03-11 13:21:43 +0000

    sec.-Fixes

--- webEdition/we/include/we_classes/database/we_database_base.class.php
+++ webEdition/we/include/we_classes/database/we_database_base.class.php
@@ -310,7 +310,7 @@ if(!$allowUnion && stristr($Query_String, 'union') || stristr($Query_String, '/*
 				}
 
 				if(!$allowUnion && stristr($Query_String, 'union') || stristr($Query_String, '/*!')){
-					exit('Bad SQL statement! For security reasons, the UNION operator is not allowed within SQL statements per default! You need to set the second parameter of the query function to true if you want to use the UNION operator!');
+					exit();
 
 			}
 		}
