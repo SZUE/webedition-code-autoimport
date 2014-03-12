075a15a7a35c8ee630591fbb113c2eaa32f592bb
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-03-12 17:03:03 +0000

    changes from updateserver

--- tags/6.3.3.0/webEdition/index.php
+++ tags/6.3.3.0/webEdition/index.php
@@ -145,6 +145,10 @@ function showMessage(message, prio, win){
 if(is_dir($_SERVER['DOCUMENT_ROOT'] . WEBEDITION_DIR . 'we/cache')){
 	we_util_File::deleteLocalFolder($_SERVER['DOCUMENT_ROOT'] . WEBEDITION_DIR . 'we/cache', true);
 }
+//s1
+if(is_dir(WEBEDITION_DIR . 'OnlineInstaller')){
+	we_util_File::deleteLocalFolder(WEBEDITION_DIR . 'OnlineInstaller');
+}
 
 cleanTempFiles(true);
 cleanWEZendCache();
