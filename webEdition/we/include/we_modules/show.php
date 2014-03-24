88443980e5889dc3d55edabcb7e90c3eafcedcb3
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-03-24 10:43:44 +0000

    fix notice

--- webEdition/we/include/we_modules/show.php
+++ webEdition/we/include/we_modules/show.php
@@ -25,6 +25,6 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we.inc.php');
 we_html_tools::protect();
 
-if(we_base_moduleInfo::isActive($_REQUEST["mod"])){
+if(isset($_REQUEST["mod"]) && we_base_moduleInfo::isActive($_REQUEST["mod"])){
 	require_once(WE_MODULES_PATH . $_REQUEST["mod"] . "/edit_" . $_REQUEST["mod"] . "_frameset.php");
 }
