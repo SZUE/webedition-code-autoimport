e089d62313e07b38124770a9b6af1db62f844b57
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-05-09 23:25:31 +0000

    Fix #8569

--- trunk/webEdition/apps/toolfactory/pattern/index.php
+++ trunk/webEdition/apps/toolfactory/pattern/index.php
@@ -22,31 +22,5 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 
-// include autoload function
-require_once ($_SERVER['DOCUMENT_ROOT']. '/webEdition/lib/we/core/autoload.inc.php');
-
-// include configuration
-include_once('<?php print $TOOLNAME;?>/conf/meta.conf.php');
-
-// get controller instabce
-$controller = Zend_Controller_Front::getInstance();
-
-// set path fpr controller directory
-$controller->setControllerDirectory('./controllers');
-
-// turn on exceptions
-$controller->throwExceptions(true); // should be turned off in production server
-
-// disables automatic view rendering
-$controller->setParam('noViewRenderer', true);
-
-// set some app specific parameter
-$controller->setParam('appDir',dirname($_SERVER['SCRIPT_NAME']));
-$controller->setParam('appPath',dirname($_SERVER['SCRIPT_FILENAME']));
-$controller->setParam('appName', '<?php print $TOOLNAME;?>');
-
-// alerts a message and exits when a user is not logged in or when the session is expired
-we_core_Permissions::protect();
-
-// run!
-$controller->dispatch();
+$TOOLNAME = '<?php print $TOOLNAME;?>';
+require_once('../index.inc.php');
