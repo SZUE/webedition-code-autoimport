ba7b4f80ec096904bf331928ebf11b991ab89b77
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-05-14 11:56:31 +0000

    Fix show del files

--- externals/additional/patches/6390/update6390.php
+++ externals/additional/patches/6390/update6390.php
@@ -5,16 +5,22 @@ Attempt to update
 include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we.inc.php");
 
 $all = file('./del.files', FILE_IGNORE_NEW_LINES);
+if(!$all){
+	t_e('file not found');
+}
+$delFiles=array();
  foreach($all as $cur){
      if(file_exists(WEBEDITION_PATH . $cur)){
          if(is_file(WEBEDITION_PATH . $cur)){
-             echo $cur . '<br/>';
+					 $delFiles[]=$cur;
              unlink(WEBEDITION_PATH . $cur);
-         } elseif(is_dir(WEBEDITION_PATH . $cur)){
-             echo $cur . '<br/>';
+				 } elseif(is_dir(WEBEDITION_PATH . $cur)){
+					 $delFiles[]='Folder: '. $cur;
              we_util_File::deleteLocalFolder(WEBEDITION_PATH . $cur, false);
          }
      }
  }
+
+t_e('deleted Files',$delFiles);
 
 return true;
