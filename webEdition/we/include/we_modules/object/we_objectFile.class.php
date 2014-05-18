0a7994dd4231a56ad2e6609c8c6bc373dff33489
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-05-18 13:15:18 +0000

    FR #8590

--- webEdition/we/include/we_modules/object/we_objectFile.class.php
+++ webEdition/we/include/we_modules/object/we_objectFile.class.php
@@ -2024,7 +2024,7 @@ class we_objectFile extends we_document{
 			$text = str_replace(array(' ', '//'), array('-', '/'), $text);
 			$text = (URLENCODE_OBJECTSEOURLS) ?
 				str_replace('%2F', '/', urlencode($text)) :
-				preg_replace('~[^0-9a-zA-Z/._-]~', '', correctUml($text));
+				preg_replace(array('~&(.)(uml|grave|acute|circ|tilde|ring|cedil|slash|caron);|&(..)(lig);|&#.*;~', '~[^0-9a-zA-Z/._-]~'), array('\1\3', ''), htmlentities($text));
 			$this->Url = substr($text, 0, 256);
 		} else {
 			$this->Url = '';
