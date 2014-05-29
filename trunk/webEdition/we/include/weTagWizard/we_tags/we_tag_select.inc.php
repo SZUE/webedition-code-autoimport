65b69f8e4bb6a4b87ae8bbd45d546d8305fca610
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-05-29 14:01:58 +0000

    Fix tw: we:select

--- trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_select.inc.php
+++ trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_select.inc.php
@@ -15,7 +15,7 @@ $size = new weTagData_textAttribute('size', false, '');
 $reload = new weTagData_selectAttribute('reload', weTagData_selectAttribute::getTrueFalse(), false, '');
 $values = new weTagData_textAttribute('values', false, '');
 $this->TypeAttribute = new weTagData_typeAttribute('type', array(
-	new weTagDataOption('', false, '', array(), array($name, $size, $reload)),
+	new weTagDataOption('', false, '', array($name, $size, $reload)),
 	new weTagDataOption('csv', false, '', array($name, $size, $reload, $values), array())), false, '');
 
 $this->Attributes = array($name, $size, $reload, $values);
