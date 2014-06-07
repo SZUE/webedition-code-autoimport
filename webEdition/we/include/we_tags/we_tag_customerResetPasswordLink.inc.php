dea967412a92789fa1412238551f7871e0eedb65
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-06-07 00:31:40 +0000

    changes resetpwd

--- webEdition/we/include/we_tags/we_tag_customerResetPasswordLink.inc.php
+++ webEdition/we/include/we_tags/we_tag_customerResetPasswordLink.inc.php
@@ -30,7 +30,7 @@ function we_tag_customerResetPasswordLink(array $attribs, $content){
 	$id = weTag_getAttribute('id', $attribs);
 	$host = weTag_getAttribute('host', $attribs, getServerUrl());
 
-	$attribs["href"] = $host . we_tag('a', array('hrefonly' => true, 'id' => $id)) . '?user=' . $_SESSION['webuser']['ID'] . '&token=' . $_SESSION['webuser']['token'];
+	$attribs["href"] = $host . we_tag('a', array('hrefonly' => true, 'id' => $id)) . '?user=' . $_SESSION['webuser']['ID'] . '&token=' . $_SESSION['webuser']['WE_token'];
 
 	return (weTag_getAttribute("plain", $attribs, false, true) ?
 			$attribs["href"] :
