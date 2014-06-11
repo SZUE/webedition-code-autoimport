f6ba45b84fd6bb87cf675db6555f750be497546f
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-06-11 13:30:21 +0000

    Fix #8649

--- trunk/webEdition/we/include/we_modules/shop/we_shop_Basket.class.php
+++ trunk/webEdition/we/include/we_modules/shop/we_shop_Basket.class.php
@@ -230,7 +230,7 @@ class we_shop_Basket{
 					return array();
 				}
 
-				$olv = new we_object_listview(0, 1, 0, '', false, $classArray['TableID'], '', '', ' ' . OBJECT_X_TABLE . $classArray['TableID'] . '.ID=' . $classArray['ObjectID'], 0, 0, true, false);
+				$olv = new we_object_listview(0, 1, 0, '', false, $classArray['TableID'], '', '', ' ' . OBJECT_X_TABLE . $classArray['TableID'] . '.ID=' . $classArray['ObjectID'], 0, 0, true, false, '', '', '', '', '', '', '', 0, '', '', '', '', TAGLINKS_DIRECTORYINDEX_HIDE, TAGLINKS_OBJECTSEOURLS);
 				$olv->next_record();
 
 				$Record = $olv->getDBRecord();
