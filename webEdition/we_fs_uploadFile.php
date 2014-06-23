3021d3684fad7323f7665b1fc15677ea34a7993a
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-06-23 13:20:11 +0000

    Fix upload

--- webEdition/we_fs_uploadFile.php
+++ webEdition/we_fs_uploadFile.php
@@ -39,7 +39,7 @@ switch($we_ContentType){
 	default:
 		$allowedContentTypes = $we_ContentType;
 }
-$pid = we_base_request::_(we_base_request::INT, 'pid', we_base_request::_(we_base_request::INT, 'dir', 0));
+$pid = we_base_request::_(we_base_request::INT, 'pid', 0);
 $parts = array();
 $we_alerttext = (!in_workspace($pid, get_ws(FILE_TABLE), FILE_TABLE, $GLOBALS['DB_WE']) || isset($_FILES['we_uploadedFile']) && !permissionhandler::hasPerm(we_base_ContentTypes::inst()->getPermission(getContentTypeFromFile($_FILES['we_uploadedFile']['name']))) ?
 		g_l('alert', '[upload_targetDir_notallowed]') :
