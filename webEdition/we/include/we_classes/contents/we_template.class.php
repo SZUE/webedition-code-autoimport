6a42b8f9386840bdbe621b0a646a207149269b99
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-06-26 12:53:32 +0000

    Fix match

--- webEdition/we/include/we_classes/contents/we_template.class.php
+++ webEdition/we/include/we_classes/contents/we_template.class.php
@@ -456,7 +456,7 @@ we_templateInit();?>';
 							$spacer = "[\040|\n|\t|\r]*";
 							$selregs = array();
 							//FIXME: this regex is not correct [^name] will not match any of those chars
-							if(preg_match('-(<we:select [^name]*name' . $spacer . '[\=\"|\=\'|\=\\\\|\=]*' . $spacer . $att['name'] . '[\'\"]*[^>]*>)(.*)<' . $spacer . '/' . $spacer . 'we:select' . $spacer . '>-i', $templateCode, $selregs)){
+							if(preg_match('-(<we:select [^name]*name' . $spacer . '[\=\"|\=\'|\=\\\\|\=]*' . $spacer . preg_quote($att['name'],'-') . '[\'\"]*[^>]*>)(.*)<' . $spacer . '/' . $spacer . 'we:select' . $spacer . '>-i', $templateCode, $selregs)){
 								$out[$name]['content'] = $selregs[2];
 							}
 						}
