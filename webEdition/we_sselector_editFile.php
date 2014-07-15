6db716f5bac0290b322d94709a04b68fb43ce3d7
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-07-15 19:47:18 +0000

    Fix #8763

--- webEdition/we_sselector_editFile.php
+++ webEdition/we_sselector_editFile.php
@@ -35,7 +35,7 @@ $we_fileData = "";
 
 $id = we_base_request::_(we_base_request::FILE, 'id');
 if(we_base_request::_(we_base_request::STRING, "cmd") == "save"){
-	if(($data = we_base_request::_(we_base_request::RAW, "editFile")) !== false){
+	if(($data = we_base_request::_(we_base_request::RAW_CHECKED, "editFile")) !== false){
 		we_base_file::save($id, $data);
 	}
 	$we_fileData = stripslashes($_REQUEST["editFile"]);
