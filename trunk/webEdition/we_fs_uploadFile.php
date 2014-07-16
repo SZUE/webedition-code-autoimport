095b4eecbf58034a297003629a784581cc7d3689
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-07-16 14:18:33 +0000

    Fix #8760

--- trunk/webEdition/we_fs_uploadFile.php
+++ trunk/webEdition/we_fs_uploadFile.php
@@ -213,8 +213,8 @@ if($inputTypeFile->processFileRequest()){
 	</head>
 	<body class="weDialogBody" onload="self.focus();" ><center>
 			<form method="post" enctype="multipart/form-data">
-				<input type="hidden" name="table" value="<?php echo $_REQUEST["tab"]; ?>" />
-				<input type="hidden" name="pid" value="<?php echo $_REQUEST["pid"]; ?>" />
+				<input type="hidden" name="table" value="<?php echo we_base_request::_(we_base_request::TABLE, 'tab'); ?>" />
+				<input type="hidden" name="pid" value="<?php echo $pid; ?>" />
 				<input type="hidden" name="ct" value="<?php echo $we_ContentType; ?>" />
 				<?php echo we_html_multiIconBox::getHTML("", "100%", $parts, 30, $buttons, -1, "", "", false, g_l('newFile', "[import_File_from_hd_title]"), "", 620); ?>
 			</form></center>
