1f33ed47ba272106d03970ee2acccb1623fa9f44
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-08-24 20:21:19 +0000

    fix for security

--- webEdition/liveUpdate/sqldumps/tblSessions.sql
+++ webEdition/liveUpdate/sqldumps/tblSessions.sql
@@ -1,9 +1,9 @@
 CREATE TABLE ###TBLPREFIX###tblSessions (
-  `session_id` binary(20) NOT NULL,
-  `lockTime` timestamp NULL DEFAULT NULL,
-  `lockid` char(23) NOT NULL,
-  `touch` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `sessionName` char(15) NOT NULL,
-  `session_data` longblob NOT NULL,
-  PRIMARY KEY (`session_id`)
+  sessionName char(15) NOT NULL,
+  session_id binary(20) NOT NULL,
+  lockTime timestamp NULL DEFAULT NULL,
+  lockid char(23) NOT NULL,
+  touch timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  session_data longblob NOT NULL,
+  PRIMARY KEY (sessionName,session_id)
 ) ENGINE=MyISAM;
