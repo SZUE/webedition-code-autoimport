926b90640919ee36b3085f905bc1625c139b118e
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-08-27 01:10:55 +0000

    less duplicate types

--- webEdition/liveUpdate/sqldumps/tblLink.sql
+++ webEdition/liveUpdate/sqldumps/tblLink.sql
@@ -1,6 +1,10 @@
 ###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="href" WHERE l.Name LIKE '%we_jkhdsf_%' AND Type="txt"
 /* query separator */
+###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="txt" WHERE l.Type='text'
+/* query separator */
 ###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="attrib" WHERE l.Name IN('Charset','useMetaTitle','origwidth','origheight','width','height') AND Type="txt"
+/* query separator */
+###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="block" WHERE l.Type='list'
 /* query separator */
 
 CREATE TABLE ###TBLPREFIX###tblLink (
@@ -12,7 +16,7 @@ CREATE TABLE ###TBLPREFIX###tblLink (
   PRIMARY KEY (CID),
   KEY DID (DID,DocumentTable),
   KEY Name (Name(4)),
-  KEY `Type` (`Type`)
+  KEY `Type` (`Type`,DocumentTable)
 ) ENGINE=MyISAM;
 /*
 type field - current known
@@ -26,9 +30,7 @@ href
 img
 input
 linklist
-list
 object
-text
 txt
 variant
 variants
