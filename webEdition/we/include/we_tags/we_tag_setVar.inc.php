5246d99f3cc0cdcc9c5ec8d1d19737a92710cb8f
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-08-27 20:56:06 +0000

    Fix #8869

--- webEdition/we/include/we_tags/we_tag_setVar.inc.php
+++ webEdition/we/include/we_tags/we_tag_setVar.inc.php
@@ -60,8 +60,8 @@ function we_tag_setVar($attribs){
 				if($propertyFrom){
 					$valueFrom = isset($GLOBALS['WE_MAIN_DOC']->$nameFrom) ? $GLOBALS['WE_MAIN_DOC']->$nameFrom : '';
 				} else {
-					$valueFrom = isset($GLOBALS['WE_MAIN_DOC']->elements[$nameFrom . ($typeFrom == 'href' ? we_base_link::MAGIC_INT_LINK : '')]) ?
-						$GLOBALS['WE_MAIN_DOC']->getField(array('name' => $nameFrom), $typeFrom, true) :
+					$valueFrom = $GLOBALS['WE_MAIN_DOC']->issetElement($nameFrom . ($typeFrom == 'href' ? we_base_link::MAGIC_INT_LINK : '')) ?
+						$GLOBALS['WE_MAIN_DOC']->getField(array('name' => $nameFrom), (strpos($nameFrom, we_base_link::MAGIC_INT_LINK_ID) ? 'href' : $typeFrom), true) :
 						'';
 				}
 				break;
@@ -69,8 +69,8 @@ function we_tag_setVar($attribs){
 				if($propertyFrom){
 					$valueFrom = isset($GLOBALS['we_doc']->$nameFrom) ? $GLOBALS['we_doc']->$nameFrom : '';
 				} else {
-					$valueFrom = isset($GLOBALS['we_doc']->elements[$nameFrom . ($typeFrom == 'href' ? we_base_link::MAGIC_INT_LINK : '')]) ?
-						$GLOBALS['we_doc']->getField(array('name' => $nameFrom), $typeFrom, true) :
+					$valueFrom = $GLOBALS['we_doc']->issetElement($nameFrom . ($typeFrom == 'href' ? we_base_link::MAGIC_INT_LINK : '')) ?
+						$GLOBALS['we_doc']->getField(array('name' => $nameFrom), (strpos($nameFrom, we_base_link::MAGIC_INT_LINK_ID) ? 'href' : $typeFrom), true) :
 						'';
 				}
 				break;
@@ -78,7 +78,7 @@ function we_tag_setVar($attribs){
 			case 'document' :
 				$valueFrom = ($propertyFrom ?
 						(isset($GLOBALS['we_' . $from][$formnameFrom]->$nameFrom) ? $GLOBALS['we_' . $from][$formnameFrom]->$nameFrom : '') :
-						(isset($GLOBALS['we_' . $from][$formnameFrom]->elements[$nameFrom]) ? $GLOBALS['we_' . $from][$formnameFrom]->getElement($nameFrom) : ''));
+						($GLOBALS['we_' . $from][$formnameFrom]->issetElement($nameFrom) ? $GLOBALS['we_' . $from][$formnameFrom]->getElement($nameFrom) : ''));
 
 				break;
 			case 'sessionfield' :
@@ -97,14 +97,14 @@ function we_tag_setVar($attribs){
 				$nameFrom = we_tag_getPostName($nameFrom);
 				if($typeFrom == 'href'){
 
-					if($GLOBALS['we_doc']->elements[$nameFrom . we_base_link::MAGIC_INT_LINK]['dat']){
+					if($GLOBALS['we_doc']->issetElement($nameFrom . we_base_link::MAGIC_INT_LINK)){
 						$nameFrom .= we_base_link::MAGIC_INT_LINK_PATH;
 					}
 				}
-				$valueFrom = isset($GLOBALS['WE_MAIN_DOC']->elements[$nameFrom]) ? $GLOBALS['WE_MAIN_DOC']->getField(
+				$valueFrom = $GLOBALS['WE_MAIN_DOC']->issetElement($nameFrom) ? $GLOBALS['WE_MAIN_DOC']->getField(
 						array(
 						'name' => $nameFrom
-						), $typeFrom, true) : '';
+						), (strpos($nameFrom, we_base_link::MAGIC_INT_LINK_ID) ? 'href' : $typeFrom), true) : '';
 				break;
 			case 'listdir' :
 				$valueFrom = isset($GLOBALS['we_position']['listdir'][$nameFrom]) ? $GLOBALS['we_position']['listdir'][$nameFrom] : '';
