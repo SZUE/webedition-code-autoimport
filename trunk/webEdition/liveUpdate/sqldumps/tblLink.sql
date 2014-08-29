e32a0282339e70cdd16ae885310fbe3c9588b317
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-08-29 08:26:50 +0000

    type in link is enum now

--- trunk/webEdition/liveUpdate/sqldumps/tblLink.sql
+++ trunk/webEdition/liveUpdate/sqldumps/tblLink.sql
@@ -1,6 +1,6 @@
 ###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="href" WHERE l.Name LIKE '%we_jkhdsf_%' AND Type="txt"
 /* query separator */
-###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="txt" WHERE l.Type='text'
+###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="txt" WHERE l.Type IN ('text','application')
 /* query separator */
 ###UPDATEONLY### UPDATE ###TBLPREFIX###tblLink l SET l.Type="attrib" WHERE l.Name IN('Charset','useMetaTitle','origwidth','origheight','width','height') AND Type="txt"
 /* query separator */
@@ -10,7 +10,7 @@
 CREATE TABLE ###TBLPREFIX###tblLink (
   DID int(11) unsigned NOT NULL default '0',
   CID int(11) unsigned NOT NULL default '0',
-  `Type` varchar(16) NOT NULL default '',
+  `Type` enum('attrib','block','customer','date','formfield','href','img','input','LanguageDocName','linklist','object','txt','variant','variants') NOT NULL default 'txt',
   Name varchar(255) NOT NULL default '',
   DocumentTable enum('tblFile','tblTemplates','tblWebUser') NOT NULL,
   PRIMARY KEY (CID),
@@ -18,21 +18,3 @@ CREATE TABLE ###TBLPREFIX###tblLink (
   KEY Name (Name(4)),
   KEY `Type` (`Type`,DocumentTable)
 ) ENGINE=MyISAM;
-/*
-type field - current known
-application
-attrib
-block
-customer
-date
-formfield
-href
-img
-input
-LanguageDocName
-linklist
-object
-txt
-variant
-variants
-*/
