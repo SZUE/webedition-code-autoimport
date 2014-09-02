3b34070d614ebad405295c42455dfa2d2d3f54a9
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-09-02 08:14:48 +0000

    JS fehler

--- webEdition/we/include/we_widgets/dlg/mfd.php
+++ webEdition/we/include/we_widgets/dlg/mfd.php
@@ -191,7 +191,7 @@ $sUsrContent = '<table border="0" cellpadding="0" cellspacing="0" width="300"><t
 	)) . '</td></tr>' . (permissionhandler::hasPerm('EDIT_MFD_USER') ? '<tr><td align="right">' . we_html_tools::getPixel(2, 8) . we_html_element::htmlBr() . we_html_button::create_button_table(
 			array(
 				we_html_button::create_button('delete_all', "javascript:delUser(-1)", true, -1, -1, "", "", (count($users)) ? false : true),
-				we_html_button::create_button('add', "javascript:opener.getUser('browse_users','top.weEditorFrameController.getActiveDocumentReference()._propsDlg['" . $cmd0 . "'].document.forms[0].elements['UserIDTmp'].value','" . $wecmdenc2 . "','','','" . $wecmdenc5 . "','','',1);")
+				we_html_button::create_button('add', "javascript:opener.getUser('browse_users','top.weEditorFrameController.getActiveDocumentReference()._propsDlg[\"" . $cmd0 . "\"].document.forms[0].elements[\"UserIDTmp\"].value','" . $wecmdenc2 . "','','','" . $wecmdenc5 . "','','',1);")
 		)) . '</td></tr>' : '') . '</table>';
 
 $oShowUser = we_html_tools::htmlFormElementTable($sUsrContent, g_l('cockpit', '[following_users]'), "left", "defaultfont");
