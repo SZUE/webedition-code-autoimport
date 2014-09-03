ba359ca8bffc144ee1abe002fc37345cff262772
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-09-03 23:24:05 +0000

    Fix #8888

--- trunk/webEdition/we/include/we_fs.inc.php
+++ trunk/webEdition/we/include/we_fs.inc.php
@@ -25,9 +25,15 @@ $cmd0 = we_base_request::_(we_base_request::STRING, 'we_cmd', '', 0);
 switch($cmd0){//FIMXE most of the stuff can be handled via session! transfer is a bit complicated due to elements inside documents
 	case 'openCatselector' :
 		$_REQUEST['noChoose'] = we_base_request::_(we_base_request::BOOL, 'we_cmd', false, 8);
+		$_REQUEST['id'] = we_base_request::_(we_base_request::INT, 'we_cmd', 0, 1);
+		$table = $_REQUEST['table'] = we_base_request::_(we_base_request::TABLE, 'we_cmd', FILE_TABLE, 2);
+		$_REQUEST['JSIDName'] = we_base_request::_(we_base_request::CMD, 'we_cmd', '', 3);
+		$_REQUEST['JSTextName'] = we_base_request::_(we_base_request::CMD, 'we_cmd', '', 4);
+		$_REQUEST['JSCommand'] = we_base_request::_(we_base_request::CMD, 'we_cmd', '', 5);
+		$_REQUEST['rootDirID'] = we_base_request::_(we_base_request::INT, 'we_cmd', 0, 7);
+		break;
 	case 'openDirselector':
 	case 'openSelector' :
-	case 'openCatselector' :
 	case 'openDelSelector' :
 		$_REQUEST['id'] = we_base_request::_(we_base_request::INT, 'we_cmd', 0, 1);
 		$table = $_REQUEST['table'] = we_base_request::_(we_base_request::TABLE, 'we_cmd', FILE_TABLE, 2);
