2e0e45c41d93a1009aa1b960256c422e684236db
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-09-04 09:33:22 +0000

    missing we.inc

--- trunk/webEdition/we/include/we_widgets/dlg/fdl/fdl.php
+++ trunk/webEdition/we/include/we_widgets/dlg/fdl/fdl.php
@@ -38,7 +38,7 @@ function init(){
 	parent.rpcHandleResponse(_sType,_sObjId,document.getElementById(_sType),_sTb);
 }";
 
-print we_html_element::htmlDocType() . we_html_element::htmlHtml(
+echo we_html_element::htmlDocType() . we_html_element::htmlHtml(
 		we_html_element::htmlHead(
 			we_html_tools::getHtmlInnerHead(g_l('cockpit', '[kv_failedLogins][headline]') . ' (' . $maxRows . ')') . STYLESHEET . we_html_element::jsElement(
 				$sJsCode)) . we_html_element::htmlBody(
