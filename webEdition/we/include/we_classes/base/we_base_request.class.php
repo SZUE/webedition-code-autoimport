08d97ed449daf3e7a59cd38ed271b9eba729caac
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-09-11 15:24:26 +0000

    Fix JS error

--- webEdition/we/include/we_classes/base/we_base_request.class.php
+++ webEdition/we/include/we_classes/base/we_base_request.class.php
@@ -88,7 +88,7 @@ class we_base_request{
 				return;
 			case self::UNIT:
 				$regs = array(); //FIMXE: check for %d[em,ex,pt,%...]?
-				$var = (preg_match('/(\d+) ?(em|ex|pt|px|%)/', $var, $regs) ? $regs[1] . $regs[2] : 0 );
+				$var = (preg_match('/(\d+) ?(em|ex|pt|px|%)/', $var, $regs) ? $regs[1] . $regs[2] : '' );
 				return;
 			case self::INT:
 				$var = intval($var);
--- webEdition/we/include/we_tags/we_tag_href.inc.php
+++ webEdition/we/include/we_tags/we_tag_href.inc.php
@@ -124,7 +124,7 @@ function we_tag_href($attribs){
 	$int_elem_Name = 'we_' . $GLOBALS['we_doc']->Name . '_href[' . $nint . ']';
 	$intPath_elem_Name = 'we_' . $GLOBALS['we_doc']->Name . '_href[' . $nintPath . ']';
 	$intID_elem_Name = 'we_' . $GLOBALS['we_doc']->Name . '_href[' . $nintID . '#bdid]';
-	$ext_elem_Name = 'we_' . $GLOBALS['we_doc']->Name . '_href[' . $name . ']';
+	$ext_elem_Name = 'we_' . $GLOBALS['we_doc']->Name . '_txt[' . $name . ']';
 
 	$trashbut = we_html_button::create_button('image:btn_function_trash', "javascript:document.we_form.elements['" . $intID_elem_Name . "'].value = ''; document.we_form.elements['" . $intPath_elem_Name . "'].value = ''; _EditorFrame.setEditorIsHot(true);" . (($include || $reload) ? "setScrollTo(); top.we_cmd('reload_editpage');" : ''), true);
 	$span = '<span style="color: black;font-size:' . ((we_base_browserDetect::isMAC()) ? "11px" : ((we_base_browserDetect::isUNIX()) ? "13px" : "12px")) . ';font-family:' . g_l('css', '[font_family]') . ';">';
