5bb7a3adf803a2c81de960433391a909bab64316
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-09-26 13:02:02 +0000

    tw

--- trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_customerResetPassword.inc.php
+++ trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_customerResetPassword.inc.php
@@ -8,12 +8,12 @@ $this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
 
 $required = new weTagData_textAttribute('required', true, 'customer');
 $loadFields = new weTagData_textAttribute('loadFields', false, 'customer');
-$customerEmailField = new weTagData_textAttribute('customerEmailField',false, 'customer');
-$expireToken= new weTagData_textAttribute('expireToken',false, 'customer');
+$customerEmailField = new weTagData_textAttribute('customerEmailField', false, 'customer');
+$expireToken = new weTagData_textAttribute('expireToken', false, 'customer');
 
 $this->TypeAttribute = new weTagData_typeAttribute('type', array(
 	new weTagDataOption('direct', false, 'customer', array($required, $loadFields), array($required)),
-	new weTagDataOption('email', false, 'customer', array($required,$customerEmailField,$expireToken), array($required,$customerEmailField)),
-	new weTagDataOption('emailPassword', false, 'customer', array($required,$expireToken), array($required)),
+	new weTagDataOption('email', false, 'customer', array($required, $customerEmailField, $expireToken, $loadFields), array($required, $customerEmailField)),
+	new weTagDataOption('emailPassword', false, 'customer', array($required, $expireToken, $loadFields), array($required)),
 	new weTagDataOption('resetFromMail', false, 'customer', array($required), array($required)),
 	), true, '');
