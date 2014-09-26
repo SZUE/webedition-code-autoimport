905566e137ecf110b42ccc2c975f69f226db5c84
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-09-26 10:49:07 +0000

    #8999

--- trunk/webEdition/we/include/we_tags/we_tag_addDelNewsletterEmail.inc.php
+++ trunk/webEdition/we/include/we_tags/we_tag_addDelNewsletterEmail.inc.php
@@ -211,9 +211,8 @@ function we_tag_addDelNewsletterEmail($attribs){
 
 				$cnt = 0;
 				$confirmLink = ($id ? id_to_path($id, FILE_TABLE) : $_SERVER['SCRIPT_NAME']) . '?confirmID=' . $confirmID . '&mail=' . rawurlencode($f['subscribe_mail']);
-				$urlReplace = we_folder::getUrlReplacements($GLOBALS['DB_WE']);
+				$urlReplace = we_folder::getUrlReplacements($GLOBALS['DB_WE'], true);
 				if($urlReplace){
-
 					$confirmLink = preg_replace('-(["\'])//-', '\\1' . $protocol, preg_replace($urlReplace, array_keys($urlReplace), $confirmLink, -1, $cnt));
 				}
 
