0c91ba3dd7ba7317bde289a6a2747f9c8d2e90b7
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2014-09-29 11:25:34 +0000

    we_version for trunk nightly

--- trunk/webEdition/we/include/we_version.php
+++ trunk/webEdition/we/include/we_version.php
@@ -1,6 +1,6 @@
 <?php
 
-define("WE_VERSION", "6.3.8.4");
+define("WE_VERSION", "6.3.8.5");
 define("WE_ZFVERSION", "1.12.3"); // recommended version of the Zend Framework (bundled with webEdition)
 define("WE_SVNREV", "");
 define("WE_VERSION_SUPP_VERSION", "0");
