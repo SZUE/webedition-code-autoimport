76233a33b864bc2f033d06f9fbc4044c91a35f1f
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2014-10-01 13:26:27 +0000

    small fix sdk pattern

--- trunk/webEdition/apps/toolfactory/controllers/RpcController.php
+++ trunk/webEdition/apps/toolfactory/controllers/RpcController.php
@@ -30,7 +30,7 @@ class RpcController extends Zend_Controller_Action{
 	public function indexAction(){
 		$jsonOutput = we_net_rpc_JsonRpc::getReply('toolfactory');
 		//response for callback
-		$this->getResponse()->setHeader('Content-Type', 'application/json')->appendBody($jsonOutput);
+		$this->getResponse()->setHeader('Content-Type', 'application/json', true)->appendBody($jsonOutput);
 	}
 
 }
