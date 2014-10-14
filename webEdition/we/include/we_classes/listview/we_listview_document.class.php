5857feda575d0c9935f9bd8b13a48b210d0a3dd3
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-10-14 23:34:57 +0000

    Fix #9078

--- webEdition/we/include/we_classes/listview/we_listview_document.class.php
+++ webEdition/we/include/we_classes/listview/we_listview_document.class.php
@@ -172,7 +172,7 @@ class we_listview_document extends we_listview_base{
 			$this->contentTypes = str_replace(array('img', 'wepage', 'binary'), array(we_base_ContentTypes::IMAGE, we_base_ContentTypes::WEDOCUMENT, we_base_ContentTypes::APPLICATION), $this->contentTypes);
 			$CtArr = makeArrayFromCSV($this->contentTypes);
 			foreach($CtArr as $ct){
-				$sql_tail .= ' AND ' . FILE_TABLE . '.ContentType = "' . $this->DB_WE->escape($ct) . '"';
+				$sql_tail .= ' AND ' . FILE_TABLE . '.ContentType="' . $this->DB_WE->escape($ct) . '"';
 			}
 		}
 		if(defined('CUSTOMER_FILTER_TABLE')){
