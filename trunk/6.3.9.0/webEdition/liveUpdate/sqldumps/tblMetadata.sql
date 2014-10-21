b20684eed012399e1e0a25cb677628781b45e05e
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-10-21 09:43:11 +0000

    reverted commit

--- trunk/6.3.9.0/webEdition/liveUpdate/sqldumps/tblMetadata.sql
+++
@@ -1,8 +0,0 @@
-###UPDATEDROPCOL(ID,###TBLPREFIX###tblMetadata)###
-/* query separator */
-CREATE TABLE ###TBLPREFIX###tblMetadata (
-  `tag` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `importFrom` varchar(255) NOT NULL,
-  PRIMARY KEY  (`tag`)
-)  ENGINE=MyISAM;
