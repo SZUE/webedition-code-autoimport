012304f695bd44d8957074d3f41658b8c5e80a7c
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-10-21 09:43:11 +0000

    reverted commit

--- 6.3.9.0/webEdition/liveUpdate/sqldumps/tblformmaillog.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE ###TBLPREFIX###tblformmaillog (
-  id bigint(20) unsigned NOT NULL auto_increment,
-  ip varchar(40) NOT NULL,
-  unixTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY  (id),
-  KEY ipwhen (ip,unixTime)
-) ENGINE=MyISAM;
