3f1290ee2ea2bd6d4c5a0646846c06d55969316e
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-10-21 09:38:14 +0000

    removed duplicate content

--- 6.3.9.0/webEdition/apps/toolfactory/pattern/tags/we_tag_TOOLNAME.inc.php
+++
@@ -1,9 +0,0 @@
-
-function we_tag_<?php print $TOOLNAME;?>($attribs,$content){
-
-	include_once (WE_APPS_PATH. . '<?php print $TOOLNAME;?>/conf/define.conf.php');
-    if(<?php print $ACTIVECONSTANT;?>){ //check if application is disabled
-		return "Hello <?php print $TOOLNAME;?>!";
-    }
-
-}
--- 6.3.9.0/webEdition/we/include/we_version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("WE_VERSION", "6.3.9.0");
-define("WE_ZFVERSION", "1.12.3"); // recommended version of the Zend Framework (bundled with webEdition)
-define("WE_SVNREV", "8443");
-define("WE_VERSION_SUPP_VERSION", "0");
-define("WE_VERSION_SUPP","release");
-define("WE_VERSION_BRANCH", "trunk");
