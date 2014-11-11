53489dcf387faea213c9a9c98df54dee4812e8f5
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-11-11 18:21:57 +0000

    Fix #9121

--- webEdition/we/include/we_widgets/mod/mfd.php
+++ webEdition/we/include/we_widgets/mod/mfd.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * webEdition CMS
  *
@@ -85,8 +86,8 @@ $bDateLastMfd = $sDisplayOpt{1};
 $db = $GLOBALS['DB_WE'];
 
 $aUsers = array_filter(array_map('intval', (permissionhandler::hasPerm('EDIT_MFD_USER') ?
-			makeArrayFromCSV($aCols[4]) :
-			array($_SESSION['user']['ID']))));
+						makeArrayFromCSV($aCols[4]) :
+						array($_SESSION['user']['ID']))));
 
 if($aUsers){
 	$aUsers = implode(',', $aUsers);
@@ -94,6 +95,7 @@ if($aUsers){
 	$folders = $db->getAll(true);
 	if($folders){
 		$db->query('SELECT ID FROM ' . USER_TABLE . ' WHERE IsFolder=0 AND (Path REGEXP "^(' . implode('/|', $folders) . '/)" OR ID IN (' . $aUsers . '))');
+		$aUsers = implode(',', $db->getAll(true));
 	}
 	$where[] = 'UID IN (' . $aUsers . ')';
 }
@@ -140,7 +142,7 @@ $queries = array();
 foreach($tables as $ctable => $ids){
 	$table = addTblPrefix($ctable);
 	$paths = ((!permissionhandler::hasPerm('ADMINISTRATOR') || ($table != TEMPLATES_TABLE && (defined('OBJECT_TABLE') ? ($table != OBJECT_TABLE) : true))) && isset($workspace[$table]) ?
-			$workspace[$table] : '');
+					$workspace[$table] : '');
 
 	$queries[] = '(SELECT ID,Path,Icon,Text,ContentType,ModDate,CreatorID,Owners,RestrictOwners,"' . $ctable . '" AS ctable FROM ' . $db->escape($table) . ' WHERE ID IN(' . implode(',', $ids) . ')' . ($paths ? (' AND (' . $paths . ')') : '') . ')';
 }
@@ -159,19 +161,19 @@ if($queries){
 		$table = addTblPrefix($db->f('ctable'));
 
 		$show = ($table == FILE_TABLE || (defined('OBJECT_FILES_TABLE') && ($table == OBJECT_FILES_TABLE)) ?
-				$admin || we_history::userHasPerms($file['CreatorID'], $file['Owners'], $file['RestrictOwners']) :
-				true);
+						$admin || we_history::userHasPerms($file['CreatorID'], $file['Owners'], $file['RestrictOwners']) :
+						true);
 
 		if($show){
 			$isOpen = $hist['isOpen'];
 			$lastModified .= '<tr><td style="width:20px;height:20px;padding-right:4px;" nowrap><img src="' . TREE_ICON_DIR . $file['Icon'] . '" />' . '</td>' .
-				'<td style="vertical-align: middle;" class="middlefont" ' . ($isOpen ? 'style="color:red;"' : '') . '>' .
-				($isOpen ? '' : '<a style="color:#000000;text-decoration:none;" href="javascript:top.weEditorFrameController.openDocument(\'' . $table . '\',' . $file['ID'] . ',\'' . $file['ContentType'] . '\');" title="' . $file['Path'] . '" >') .
-				$file['Path'] . ($isOpen ? '' : '</a>') .
-				'</td>' .
-				($bMfdBy ? '<td style="padding-left:.5em;" class="middlefont" nowrap>' . $hist['UserName'] . (($bDateLastMfd) ? ',' : '') . '</td>' : '') .
-				($bDateLastMfd ? '<td style="padding-left:.5em;" class="middlefont" nowrap>' . $hist['MDate'] . '</td>' : '') .
-				'</tr>';
+					'<td style="vertical-align: middle;" class="middlefont" ' . ($isOpen ? 'style="color:red;"' : '') . '>' .
+					($isOpen ? '' : '<a style="color:#000000;text-decoration:none;" href="javascript:top.weEditorFrameController.openDocument(\'' . $table . '\',' . $file['ID'] . ',\'' . $file['ContentType'] . '\');" title="' . $file['Path'] . '" >') .
+					$file['Path'] . ($isOpen ? '' : '</a>') .
+					'</td>' .
+					($bMfdBy ? '<td style="padding-left:.5em;" class="middlefont" nowrap>' . $hist['UserName'] . (($bDateLastMfd) ? ',' : '') . '</td>' : '') .
+					($bDateLastMfd ? '<td style="padding-left:.5em;" class="middlefont" nowrap>' . $hist['MDate'] . '</td>' : '') .
+					'</tr>';
 
 			$j++;
 		}
