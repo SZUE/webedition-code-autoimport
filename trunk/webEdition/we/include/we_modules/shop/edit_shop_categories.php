a0f4e29cfd0e419057d956ce57d0a5291d2b41f6
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2014-11-17 13:14:39 +0000

    edit_shop_categories pt3

--- trunk/webEdition/we/include/we_modules/shop/edit_shop_categories.php
+++ trunk/webEdition/we/include/we_modules/shop/edit_shop_categories.php
@@ -22,12 +22,106 @@
  * @package none
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-
-//FIXME: Base editor on edit_shop_frameset and we_shop_view (maybe in 6.4.1)
-
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/we.inc.php');
 $protect = we_base_moduleInfo::isActive('shop') && we_users_util::canEditModule('shop') ? null : array(false);
 we_html_tools::protect($protect);
+
+//FIXME: mak sowme view class for this editor and use processVariables() and processCommands()?
+//process request
+$shopCategoriesDir = ($val = we_base_request::_(we_base_request::STRING, 'weShopCatDir', false)) !== false ? $val : (f('SELECT pref_value FROM ' . SETTINGS_TABLE . ' WHERE tool="shop" AND pref_name="shop_categories_dir"', '', $DB_WE, -1));
+$rels = array();//TODO: take from db!
+
+
+
+
+//process cmds
+$debug_output = '';
+if($shopCategoriesDir !== -1){
+	switch(we_base_request::_(we_base_request::STRING, 'we_cmd', '', 0)){
+		case 'saveShopCatRels':
+			$DB_WE->query('REPLACE INTO ' . SETTINGS_TABLE . ' SET tool="shop",pref_name="shop_categories_dir",pref_value=' . intval($shopCategoriesDir));
+
+			//how to get data for saving relations in db
+			if(we_base_request::_(we_base_request::CMD, 'we_cmd', '', 0) === 'saveShopCatRels'){
+				$debug_output .= '<br/><strong>Save...</strong><br><br>';
+				$rels = we_base_request::_(we_base_request::STRING, 'weShopCatRels');
+				foreach($rels as $k => $v){
+					$debug_output .= 'cat <strong>' . $k . '</strong> is to be related with vats: ';
+					foreach($v as $territory => $id){
+						$debug_output .= $id . ' (' . $territory . '), ';
+					}
+					$debug_output .= '<br/>';
+				}
+			}
+			break;
+		case 'load': 
+			//nothing yet
+	}
+}
+
+//make category dirs select
+$DB_WE->query('SELECT ID,Path FROM ' . CATEGORY_TABLE . ' WHERE IsFolder = 1 ORDER BY Path');
+$allCategoryDirs = array('-1' => 'bitte wählen');//GL
+while($DB_WE->next_record()){
+	$allCategoryDirs[$DB_WE->f('ID')] = $DB_WE->f('Path');
+}
+$selCategoryDirs = we_html_tools::htmlSelect('weShopCatDir', $allCategoryDirs, 1, $shopCategoriesDir, false, array('id' => 'weShopCatDir', 'onchange' => 'we_submitForm(\'' . $_SERVER['SCRIPT_NAME'] . '\');'));
+
+//get all shop categories (from inside $shopCategoriesDir)
+if(intval($shopCategoriesDir) !== -1){
+	$DB_WE->query('SELECT ID, Text FROM ' . CATEGORY_TABLE . ' WHERE Path LIKE "' . $allCategoryDirs[$shopCategoriesDir] . '/%" AND IsFolder = 0');
+	$shopCategories = array();
+	while($DB_WE->next_record()){
+		$shopCategories[] = array("id" => $DB_WE->f('ID'), "text" => $DB_WE->f('Text'));
+	}
+
+
+	//Categories/VATs-Matrix
+	$DB_WE->query('SELECT id,text,vat,territory,textProvince FROM ' . WE_SHOP_VAT_TABLE);
+	$allVats = array();
+
+	while($DB_WE->next_record()){
+		if(!isset($allVats[$DB_WE->f('territory')])){
+			$allVats[$DB_WE->f('territory')] = array();
+		}
+
+		$vat = new we_shop_vat($DB_WE->f('id'), $DB_WE->f('text'), $DB_WE->f('vat'), 0, $DB_WE->f('territory'), $DB_WE->f('textProvince'));
+		$allVats[$DB_WE->f('territory')]['textTerritory'] = $vat->textTerritory;
+		$allVats[$DB_WE->f('territory')]['vatObjects'][] = $vat;
+		$allVats[$DB_WE->f('territory')]['selOptions'][$vat->id] = $vat->getNaturalizedText() . ': ' . $vat->vat . '%';
+	}
+
+	$matrix = new we_html_table(array("border" => 0, "cellpadding" => 2, "cellspacing" => 4), $rows_num = (count($shopCategories) + 1), count($allVats) + 2);
+
+	//generate column titles
+	$i = 0;
+	$j = 0;
+	$matrix->setCol($i, $j++, array("class" => "defaultfont", "style" => "font-weight:bold", "nowrap" => "nowrap", "width" => 110), '');
+	foreach($allVats as $v){
+		$matrix->setCol($i, $j++, array("class" => "defaultfont", "style" => "font-weight:bold", "nowrap" => "nowrap", "width" => 110), $v['textTerritory']);
+	}
+
+	//generate columns
+	if(count($shopCategories)){
+		foreach($shopCategories as $cat){
+			$j = 0;
+			$matrix->setCol(++$i, $j++, array("class" => "defaultfont", "style" => "font-weight:bold", "nowrap" => "nowrap", "width" => 110), $cat['text']);
+			if(!count($allVats)){
+				$matrix->setCol($i, $j, array("class" => "defaultfont", "style" => "font-weight:normal", "nowrap" => "nowrap", "width" => 110), 'no vats defined yet');
+			} else {
+				foreach($allVats as $k => $v){
+					$sel = we_html_tools::htmlSelect('weShopCatRels[' . $cat['id'] . '][' . $k . ']', $v['selOptions'], 1, $rels[$cat['id']][$k]);
+					$matrix->setCol($i, $j++, array("class" => "defaultfont", "style" => "font-weight:normal", "nowrap" => "nowrap", "width" => 110), $sel);
+				}
+			}
+		}
+		$matrixHtml = $matrix->getHtml();
+	} else {
+		$matrixHtml = "Das ausgewählte Kategoeir-Verzeichnis enthält keine Einträge.";//GL
+	}
+} else {
+	$matrixHtml = "Sie habe noch kein Kategorie-Verzeichnis gewählt.";//GL
+}
 
 echo we_html_tools::getHtmlTop() . STYLESHEET;
 
@@ -54,76 +148,24 @@ $jsFunction = '
 			break;
 
 			case "save":
+				document.forms["we_form"]["we_cmd[0]"].value = "saveShopCatRels";
 				we_submitForm("' . $_SERVER['SCRIPT_NAME'] . '");
 			break;
 		}
 	}';
 
-// initialise the shopStatusMails Object
-
-$shopCatSelector = new we_html_table(array("border" => 0, "cellpadding" => 2, "cellspacing" => 4), $rows_num = 1, $cols_num = 2);
-
 $parts = array(
 	array(
-		'headline' => 'Select Shop Categories\' Parent',
-		'space' => 100,
-		'html' => '',
-		'noline' => 1
-	),
-	array(
-		'headline' => '',
+		'headline' => 'Verzeichnis der Shop-Kategorien',
 		'space' => 0,
-		'html' => 'nothing much to see \'till now...'
+		'html' => $selCategoryDirs,
+
 	),
 );
-
-//Categories/VATs-Matrix
-$DB_WE->query('SELECT id,text,vat,territory,textProvince FROM ' . WE_SHOP_VAT_TABLE);
-$allVats = array();
-$allCats = array(
-	array('id' => 254, 'text' => 'dummyCat_254'),
-	array('id' => 62, 'text' => 'dummyCat_62'),
-	array('id' => 75, 'text' => 'dummyCat_75'),
-	array('id' => 27, 'text' => 'dummyCat_27'),
-	array('id' => 163, 'text' => 'dummyCat_163')
-);
-
-while($DB_WE->next_record()){
-	if(!isset($allVats[$DB_WE->f('territory')])){
-		$allVats[$DB_WE->f('territory')] = array();
-	}
-
-	$vat = new we_shop_vat($DB_WE->f('id'), $DB_WE->f('text'), $DB_WE->f('vat'), 0, $DB_WE->f('territory'), $DB_WE->f('textProvince'));
-
-	$allVats[$DB_WE->f('territory')]['textTerritory'] = $vat->textTerritory;
-	$allVats[$DB_WE->f('territory')]['vatObjects'][] = $vat;
-	$allVats[$DB_WE->f('territory')]['selOptions'][$vat->id] = $vat->getNaturalizedText() . ': ' . $vat->vat . '%';
-}
-
-$shopCategories = new we_html_table(array("border" => 0, "cellpadding" => 2, "cellspacing" => 4), $rows_num = (count($allCats) + 1), $cols_num = 17);
-
-$i = 0;
-
-//generate column titles
-$j = 0;
-$shopCategories->setCol($i, $j++, array("class" => "defaultfont", "style" => "font-weight:bold", "nowrap" => "nowrap", "width" => 110), '');
-foreach($allVats as $v){
-	$shopCategories->setCol($i, $j++, array("class" => "defaultfont", "style" => "font-weight:bold", "nowrap" => "nowrap", "width" => 110), $v['textTerritory']);
-}
-
-//generate columns
-foreach($allCats as $dummyCat){
-	$j = 0;
-	$shopCategories->setCol(++$i, $j++, array("class" => "defaultfont", "style" => "font-weight:bold", "nowrap" => "nowrap", "width" => 110), $dummyCat['text']);
-	foreach($allVats as $k => $v){
-		$sel = we_html_tools::htmlSelect('weShopCatRelations[' . $dummyCat['id'] . '][' . $k . ']', $v['selOptions']);
-		$shopCategories->setCol($i, $j++, array("class" => "defaultfont", "style" => "font-weight:normal", "nowrap" => "nowrap", "width" => 110), $sel);
-	}
-}
 
 $parts[] = array(
 	'headline' => 'Categories List',
-	'space' => 100,
+	'space' => 200,
 	'html' => '',
 	'noline' => 1
 );
@@ -131,41 +173,21 @@ $parts[] = array(
 $parts[] = array(
 	'headline' => '',
 	'space' => 0,
-	'html' => 'list based on new shop_vats with territory field and some dummy cats allready exists :-)',
+	'html' => $matrixHtml,
 	'noline' => 1
 );
 
 $parts[] = array(
 	'headline' => '',
 	'space' => 0,
-	'html' => $shopCategories->getHtml()
+	'html' => $debug_output
 );
-
-//how to get data for saving relations in db
-$out = '';
-if(we_base_request::_(we_base_request::CMD, 'we_cmd', '', 0) === 'saveShopCatRelations'){
-	$out .= '<strong>Save...</strong><br><br>';
-	$rels = we_base_request::_(we_base_request::STRING, 'weShopCatRelations');
-	foreach($rels as $k => $v){
-		$out .= 'cat <strong>' . $k . '</strong> is to be related with vats: ';
-		foreach($v as $territory => $id){
-			$out .= $id . ' (' . $territory . '), ';
-		}
-		$out .= '<br/>';
-	}
-}
-$parts[] = array(
-	'headline' => '',
-	'space' => 0,
-	'html' => $out
-);
-
 
 echo we_html_element::jsElement($jsFunction) .
  '</head>
 <body class="weDialogBody" onload="window.focus();">
 	<form name="we_form" method="post" >
-	<input type="hidden" name="we_cmd[0]" value="saveShopCatRelations" />' .
+	<input type="hidden" name="we_cmd[0]" value="load" />' .
  we_html_multiIconBox::getHTML(
 	'weShopCategories', 700, $parts, 30, we_html_button::position_yes_no_cancel(
 		we_html_button::create_button('save', 'javascript:we_cmd(\'save\');'), '', we_html_button::create_button('cancel', 'javascript:we_cmd(\'close\');')
