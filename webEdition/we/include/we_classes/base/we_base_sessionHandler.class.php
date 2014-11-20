72adf5b230bc49fad0c97851dd3bbb9268274a2b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-11-20 16:59:29 +0000

    notice

--- webEdition/we/include/we_classes/base/we_base_sessionHandler.class.php
+++ webEdition/we/include/we_classes/base/we_base_sessionHandler.class.php
@@ -2,7 +2,6 @@
 
 class we_base_sessionHandler{//implements SessionHandlerInterface => 5.4
 	//prevent crashed or killed sessions to stay
-
 	private $execTime;
 	private $sessionName;
 	private $DB;
@@ -22,7 +21,7 @@ class we_base_sessionHandler{//implements SessionHandlerInterface => 5.4
 			$this->execTime = ($this->execTime > 60 ? 60 : $this->execTime); //time might be wrong (1&1)
 			$this->id = uniqid('', true);
 			if(!(extension_loaded('suhosin') && ini_get('suhosin.session.encrypt')) && defined('SYSTEM_WE_SESSION_CRYPT') && SYSTEM_WE_SESSION_CRYPT){
-				$key = $_SERVER['DOCUMENT_ROOT'] . ($_SERVER['HTTP_USER_AGENT'] ? : 'HTTP_USER_AGENT');
+				$key = $_SERVER['DOCUMENT_ROOT'] . (isset($_SERVER['HTTP_USER_AGENT']) && $_SERVER['HTTP_USER_AGENT'] ? : 'HTTP_USER_AGENT');
 				if(SYSTEM_WE_SESSION_CRYPT === 2){
 					if(!isset($_COOKIE['secure'])){
 						$_COOKIE['secure'] = we_users_user::getHashIV(30);
@@ -86,9 +85,9 @@ class we_base_sessionHandler{//implements SessionHandlerInterface => 5.4
 		}
 		if(md5($sessID . $sessData) == $this->hash){//if nothing changed,we don't have to bother the db
 			$this->DB->query('UPDATE ' . SESSION_TABLE . ' SET ' . we_database_base::arraySetter(array(
-						'lockid' => $lock ? $this->id : '',
-						'lockTime' => sql_function($lock ? 'NOW()' : 'NULL'),
-					)) . ' WHERE session_id=x\'' . $sessID . '\' AND sessionName="' . $this->sessionName . '"');
+					'lockid' => $lock ? $this->id : '',
+					'lockTime' => sql_function($lock ? 'NOW()' : 'NULL'),
+				)) . ' WHERE session_id=x\'' . $sessID . '\' AND sessionName="' . $this->sessionName . '"');
 
 			if($this->DB->affected_rows()){//make sure we had an successfull update
 				return true;
@@ -100,13 +99,13 @@ class we_base_sessionHandler{//implements SessionHandlerInterface => 5.4
 		$sessID = self::getSessionID($sessID);
 
 		$this->DB->query('REPLACE INTO ' . SESSION_TABLE . ' SET ' . we_database_base::arraySetter(array(
-					'sessionName' => $this->sessionName,
-					'session_id' => sql_function('x\'' . $sessID . '\''),
-					'session_data' => sql_function('x\'' . bin2hex($sessData) . '\''),
-					'lockid' => $lock ? $this->id : '',
-					'lockTime' => sql_function($lock ? 'NOW()' : 'NULL'),
-						/* 'uid' => isset($_SESSION['webuser']['ID']) ? $_SESSION['webuser']['ID'] : (isset($_SESSION['user']['ID']) ? $_SESSION['user']['ID'] : 0),
-						  -				'tmp' => serialize($_SESSION), */
+				'sessionName' => $this->sessionName,
+				'session_id' => sql_function('x\'' . $sessID . '\''),
+				'session_data' => sql_function('x\'' . bin2hex($sessData) . '\''),
+				'lockid' => $lock ? $this->id : '',
+				'lockTime' => sql_function($lock ? 'NOW()' : 'NULL'),
+				/* 'uid' => isset($_SESSION['webuser']['ID']) ? $_SESSION['webuser']['ID'] : (isset($_SESSION['user']['ID']) ? $_SESSION['user']['ID'] : 0),
+				  -				'tmp' => serialize($_SESSION), */
 		)));
 		return true;
 	}
