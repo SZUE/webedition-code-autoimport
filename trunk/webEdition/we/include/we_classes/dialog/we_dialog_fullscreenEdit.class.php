899240cd507476694ec23e6d5b7193f08a955ded
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2014-11-25 15:02:48 +0000

    Fix #8253 pt.2

--- trunk/webEdition/we/include/we_classes/dialog/we_dialog_fullscreenEdit.class.php
+++ trunk/webEdition/we/include/we_classes/dialog/we_dialog_fullscreenEdit.class.php
@@ -78,10 +78,6 @@ function IsDigit(e) {
 	return (((key >= 48) && (key <= 57)) || (key == 0) || (key == 13));
 }
 
-function openColorChooser(name,value) {
-	var win = new jsWindow("colorDialog.php?we_dialog_args[type]=dialog&we_dialog_args[name]="+encodeURI(name)+"&we_dialog_args[color]="+encodeURI(value),"colordialog",-1,-1,400,380,true,false,true,false);
-}
-
 function IsDigitPercent(e) {
 	var ' . (we_base_browserDetect::isGecko() || we_base_browserDetect::isOpera() ? 'key = e.charCode;' : 'key = event.keyCode;') . '
 
