b349ad71684d24a9389dee04b7b166448fd30bac
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2014-12-03 11:25:18 +0000

    err. correction

--- trunk/webEdition/we/include/we_classes/base/we_base_request.class.php
+++ trunk/webEdition/we/include/we_classes/base/we_base_request.class.php
@@ -198,10 +198,11 @@ class we_base_request{
 	  } */
 
 	public static function filterVar($var, $varType, $default = ''){
+		//FIXME: remove checker at release
 		$preVar = $var;
 		self::_weRequest($var, '', array($varType, $default));
 		if($preVar != $var && $var != $default){
-			t_e('changed var', $preVar, $var);
+			t_e('changed var/tag attribute', $preVar, $var);
 		}
 		return $var;
 	}
