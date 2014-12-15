3bf5d5d5205e8d8659665236573fec9207199a8f
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2014-12-15 16:07:35 +0000

    Fix #9271

--- webEdition/we/include/we_modules/shop/edit_shop_categories.php
+++ webEdition/we/include/we_modules/shop/edit_shop_categories.php
@@ -83,7 +83,7 @@ if($shopCategoriesDir !== -1 && we_base_request::_(we_base_request::STRING, 'we_
 
 //make category dirs select
 $DB_WE->query('SELECT ID,Path FROM ' . CATEGORY_TABLE . ' WHERE IsFolder = 1 ORDER BY Path');
-$allCategoryDirs = array('-1' => 'bitte wählen');//GL
+$allCategoryDirs = array('-1' => g_l('modules_shop', '[shopcats][select_shopCatDir]'));
 while($DB_WE->next_record()){
 	$data = $DB_WE->getRecord();
 	$allCategoryDirs[$data['ID']] = $data['Path'];
@@ -103,7 +103,7 @@ if(intval($shopCategoriesDir) !== -1){
 
 		if(!isset($allVats[$data['territory']])){
 			$allVats[$data['territory']] = array();
-			$allVats[$data['territory']]['selOptions'][0] = ' ';//GL
+			$allVats[$data['territory']]['selOptions'][0] = ' ';
 		}
 
 		$vat = new we_shop_vat($data['id'], $data['text'], $data['vat'], 0, $data['territory'], $data['textProvince']);
@@ -137,16 +137,16 @@ if(intval($shopCategoriesDir) !== -1){
 			$j = 0;
 			$matrix->setCol($i, 1, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 20), $cat['ID'] . ': ');
 			$matrix->setCol($i, 2, array("class" => "defaultfont", "style" => "font-weight:bold", "nowrap" => "nowrap", "width" => 140), $cat['Category']);
-			$matrix->setCol($i++, 3, array("class" => "defaultfont", "style" => "font-weight:bold", "colspan" => "2","nowrap" => "nowrap", "width" => 140), $cat['Path']);//GL
+			$matrix->setCol($i++, 3, array("class" => "defaultfont", "style" => "font-weight:bold", "colspan" => "2","nowrap" => "nowrap", "width" => 174), $cat['Path']);
 			if($cat['ID'] != $shopCategoriesDir){
-				$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 140), 'Aktive Shop-Kategorie');//GL
+				$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 174), 'Is active as Shop category');
 				$matrix->setCol($i++, 4, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 240), we_html_forms::checkboxWithHidden(($cat['IsInactive'] == 0), 'weShopCatIsActive[' . $cat['ID'] . ']', '', false, '', 'we_switch_active_by_id(' . $cat['ID'] . ')'));
 			}
-			$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 140, "style" => "padding-bottom: 10px"), 'Destination Principle');//GL
+			$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 174, "style" => "padding-bottom: 10px"), g_l('modules_shop', '[shopcats][text_destPrinciple]'));
 			$matrix->setCol($i++, 4, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 240, "style" => "padding-bottom: 10px"), we_html_forms::checkboxWithHidden(($cat['DestPrinciple'] == 1), 'weShopCatDestPrinciple[' . $cat['ID'] . ']', ''));
 
 			if(!count($allVats)){
-				$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 140), 'no vats defined yet');//GL
+				$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 140), g_l('modules_shop', '[shopcats][warning_noVatsDefined]'));
 			} else {
 				foreach($allVats as $k => $v){
 					$isDefCountry = we_shop_category::getDefaultCountry() == $k;
@@ -156,7 +156,7 @@ if(intval($shopCategoriesDir) !== -1){
 						$selAttribs = array_merge($selAttribs, array('disabled' => 'disabled'));
 					}
 					$sel = we_html_tools::htmlSelect('weShopCatRels[' . $cat['ID'] . '][' . $k . ']', $v['selOptions'], 1, $value, false, $selAttribs, 'value', 240);
-					$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 140, "style" => ($isDefCountry ? "font-weight: normal;" : "")), $v['textTerritory']);
+					$matrix->setCol($i, 3, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 174, "style" => ($isDefCountry ? "font-weight: normal;" : "")), $v['textTerritory']);
 					$matrix->setCol($i++, 4, array("class" => "defaultfont", "nowrap" => "nowrap", "width" => 240), $sel);
 				}
 			}
@@ -166,13 +166,13 @@ if(intval($shopCategoriesDir) !== -1){
 			$i = $cat['ID'] == $shopCategoriesDir ? $iTmp : $i;
 		}
 	} else {
-		$catsMatrix = "Das ausgewählte Kategoeir-Verzeichnis enthält keine Einträge.";//GL
-		$catsDirMatrix = "Das ausgewählte Kategoeir-Verzeichnis enthält keine Einträge.";//GL
+		$catsMatrix = g_l('modules_shop', '[shopcats][warning_shopCatDirEmpty]');
+		$catsDirMatrix = g_l('modules_shop', '[shopcats][warning_shopCatDirEmpty]');
 	}
 	$catsMatrixHtml = $catsMatrix->getHtml();
 	$catsDirMatrixHtml = $catsDirMatrix->getHtml();
 } else {
-	$catsMatrixHtml = $catsDirMatrixHtml = "Sie habe noch kein Kategorie-Verzeichnis gewählt.";//GL
+	$catsMatrixHtml = $catsDirMatrixHtml = g_l('modules_shop', '[shopcats][warning_noShopCatDir]');
 }
 
 echo we_html_tools::getHtmlTop() . STYLESHEET;
@@ -229,7 +229,7 @@ $jsFunction = '
 
 $parts = array(
 	array(
-		'headline' => 'Shop-Kategorien-Verzeichnis',//GL
+		'headline' => g_l('modules_shop', '[shopcats][text_shopCatDir]'),
 		'space' => 200,
 		'html' => $selCategoryDirs,
 
@@ -237,17 +237,16 @@ $parts = array(
 );
 
 $parts[] = array(
-	'headline' => 'Shop-Kategorie-Verzeichnis bearbeiten',
+	'headline' => g_l('modules_shop', '[shopcats][text_editShopCatDir]'),
 	'space' => 400,
 	'html' => '',
 	'noline' => 1
 );
 
-$info_1 = 'Wählen Sie hier für das Shop-Kategorie-Verzeichnis die passenden Mehrwertsteuersätze aus: Zur Auswahl stehen alle im Editor "Mehrwertsteuersätze" für das jeweilige Land definierten Steuersätze. Wählen Sie für ein Land keinen Steuersatz aus, so gilt für die betreffende Kombination von Land und Kategorie der Standard-Steuersatz. Mit der Checkbox "Destination Principle" legen Sie fest, ob für die Besteuerung dieser Kategorie das Bestimmungsland-Prinzip gilt.'; //GL
-	$parts[] = array(
+$parts[] = array(
 	'headline' => '',
 	'space' => 0,
-	'html' => we_html_tools::htmlAlertAttentionBox($info_1, we_html_tools::TYPE_INFO, "580", false, 100),
+	'html' => we_html_tools::htmlAlertAttentionBox(g_l('modules_shop', '[shopcats][info_edit_shopCatDir]'), we_html_tools::TYPE_INFO, "614", false, 100),
 	'noline' => 1
 );
 
@@ -258,17 +257,16 @@ $parts[] = array(
 );
 
 $parts[] = array(
-	'headline' => 'Kategorien bearbeiten',
+	'headline' => g_l('modules_shop', '[shopcats][text_editShopCats]'),
 	'space' => 200,
 	'html' => '',
 	'noline' => 1
 );
 
-$info_2 = 'Hier können Sie für die weiteren Shop-Kategorien pro Land einen Mehrwertsteuersatz auswählen. Auch hier wird als Fallback der Standard-Steuersatz verwendet. Wird das Häckchen bei "Aktive Shop-Kategorie" entfernt, greift webEdition so lange auf die jeweilige Parent-Kategorie zurück, bis eine aktive Shop-Kategorie gefunden wird. Das Shop-Kategorie-Verzeichnis ist immer aktiv.'; //GL
 $parts[] = array(
 	'headline' => '',
 	'space' => 0,
-	'html' => we_html_tools::htmlAlertAttentionBox($info_2, we_html_tools::TYPE_INFO, "580", false, 101),
+	'html' => we_html_tools::htmlAlertAttentionBox(g_l('modules_shop', '[shopcats][info_editShopCats]'), we_html_tools::TYPE_INFO, "614", false, 101),
 	'noline' => 1
 );
 
@@ -293,7 +291,7 @@ echo we_html_element::jsElement($jsFunction) .
  we_html_multiIconBox::getHTML(
 	'weShopCategories', 700, $parts, 30, we_html_button::position_yes_no_cancel(
 		we_html_button::create_button('save', 'javascript:we_cmd(\'save\');'), '', we_html_button::create_button('cancel', 'javascript:we_cmd(\'close\');')
-	), -1, '', '', false, 'Shop-Kategorien', '', '', 'scroll'
+	), -1, '', '', false, g_l('modules_shop', '[shopcats][title_editorShopCats]'), '', '', 'scroll'
 ) . '</form>
 
 </body>
