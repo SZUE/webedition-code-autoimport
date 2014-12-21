e7d0eb5077cf59bfc966e31dfec9e7f5e66f91f0
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2014-12-21 12:30:00 +0000

    optimized Fix #9335

--- trunk/webEdition/we/include/we_tags/we_tag_ifHtmlMail.inc.php
+++ trunk/webEdition/we/include/we_tags/we_tag_ifHtmlMail.inc.php
@@ -30,7 +30,7 @@ function we_tag_ifHtmlMail(){
 	if(isset($GLOBALS['we_editmode'])){
 		return ($GLOBALS['we_editmode'] ? //editmode always HTML Mode
 						true :
-						!(bool) $GLOBALS['we_doc']->getEditorPersistent('newsletterFormat') //html=false, text=true Fix #9335 : do not use $GLOBALS['we_doc']->InWebEdition in this case
+						($GLOBALS['we_doc']->InWebEdition ? !(bool) $GLOBALS['we_doc']->getEditorPersistent('newsletterFormat') : true) //html=false, text=true
 				);
 	}
 	return true;
