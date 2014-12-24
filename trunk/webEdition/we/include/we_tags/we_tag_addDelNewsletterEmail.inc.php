1d75fd7696185d81976ea37bbb56c2579b1ae90b
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2014-12-24 10:17:48 +0000

    Fix #9336

--- trunk/webEdition/we/include/we_tags/we_tag_addDelNewsletterEmail.inc.php
+++ trunk/webEdition/we/include/we_tags/we_tag_addDelNewsletterEmail.inc.php
@@ -433,7 +433,7 @@ function we_tag_addDelNewsletterEmail($attribs){
 					if($_customerFieldPrefs['customer_html_field'] != 'ID'){
 						$set .= $_customerFieldPrefs['customer_html_field'] . '= "' . $__db->escape($f["subscribe_html"]) . '"';
 					}else{
-						$set = rtrim($set, ","); //Fix #9336
+						$set = rtrim($set, ","); //Fix #9336 
 						t_e('warning','missing newsletter customer settings', 'no customer html field found in settings: field "ID" is not allowed');
 					}
 					
