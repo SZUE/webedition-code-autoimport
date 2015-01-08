40673fe4cb019224ba4d323d2cbd8b33983d803e
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-01-08 15:52:12 +0000

    shopcats: some improvements

--- webEdition/we/include/weTagWizard/we_tags/we_tag_field.inc.php
+++ webEdition/we/include/weTagWizard/we_tags/we_tag_field.inc.php
@@ -110,14 +110,14 @@ $vatfield = new weTagData_selectAttribute('field', array(
 	new weTagDataOption('vat'),
 	new weTagDataOption('name'),
 	new weTagDataOption('country'),
-	new weTagDataOption('country_iso'),
+	new weTagDataOption('countrycode'),
 	new weTagDataOption('is_standard'),
 	new weTagDataOption('is_fallback_to_standard'),
 	new weTagDataOption('is_fallback_to_prefs'),
 	new weTagDataOption('is_country_fallback_to_prefs')
 	), false, '');
 $customerid = new weTagData_textAttribute('customerid', false, '');
-$country = new weTagData_textAttribute('country', false, '');
+$countrycode = new weTagData_textAttribute('countrycode', false, '');
 $this->TypeAttribute = new weTagData_typeAttribute('type', array(
 	new weTagDataOption('-', false, '', array(), array()),
 	new weTagDataOption('text', false, '', array($name, $hyperlink, $href, $target, $num_format, $alt, $max, $striphtml, $htmlspecialchars, $triggerid), array($name)),
@@ -136,7 +136,7 @@ $this->TypeAttribute = new weTagData_typeAttribute('type', array(
 	new weTagDataOption('binary', false, 'object', array($name, $hyperlink, $href, $target, $only), array($name)),
 	new weTagDataOption('float', false, '', array($name, $hyperlink, $href, $target, $num_format, $triggerid), array($name)),
 	new weTagDataOption('int', false, 'object', array($name, $hyperlink, $href, $target, $triggerid), array($name)),
-	new weTagDataOption('shopVat', false, '', array($vatfield, $customerid, $country), array()),
+	new weTagDataOption('shopVat', false, '', array($vatfield, $customerid, $countrycode), array()),
 	new weTagDataOption('shopCategory', false, '', array($catfield, $showpath, $rootdir), array()),
 	new weTagDataOption('checkbox', false, '', array($name), array($name)),
 	new weTagDataOption('country', false, '', array($outputlanguage, $doc), array()),
