ad19192f9db368b229f816e4a1411383b334f912
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-01-15 00:35:43 +0000

    Fix patches

---
+++ externals/additional/patches/6390/update6390.php
@@ -0,0 +1,27 @@
+﻿<?php
+/*
+Attempt to update
+*/
+// only execute on liveUpdate:
+//aber meiner Meinung nach nicht notwendig, patches werden nicht ausgeführt
+if(!is_readable("../../we/include/conf/we_conf.inc.php")) {
+	//return true;
+}
+include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we.inc.php");
+//include_once(WE_INCLUDES_PATH."we_classes/base/weFile.class.php");
+
+
+function up6390_updateConf(){
+	$filename= $_SERVER["DOCUMENT_ROOT"].'/webEdition/we/include/conf/we_conf.inc.php';
+        
+        if(($conf = file_get_contents($filename))){
+            $conf = str_replace(array('include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/"."db_mysql.inc.php");', '?>'), '', $conf);
+
+            return file_put_contents($filename, $conf);
+        }
+}
+
+up6390_updateConf();
+//file_put_contents($_SERVER["DOCUMENT_ROOT"]."/say_6390.txt", 'patch gelaufen 6390');
+we_updater::doUpdate();
+return true;
