efa63baf3f25922e955586a4c945ff9360e0a26e
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-01-15 14:42:08 +0000

    we_version 6.4.0.1 nightly

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,8 +1,7 @@
-<?php
-define("WE_VERSION","6.4.0.0");
-define("WE_VERSION_SUPP","release");
-define("WE_ZFVERSION","1.12.3"); // recommended version of the Zend Framework (bundled with webEdition)
-define("WE_SVNREV","8996");
-define("WE_VERSION_SUPP_VERSION","0");
-define("WE_VERSION_BRANCH","trunk");
-define("WE_VERSION_NAME","6.4.0");
+	<?php
+	define('WE_VERSION', '6.3.9.4');
+	define('WE_ZFVERSION', '1.12.3'); // recommended version of the Zend Framework (bundled with webEdition)
+	define('WE_SVNREV', '$Rev$');
+	define('WE_VERSION_SUPP_VERSION', '0');
+	define('WE_VERSION_SUPP','');
+	define('WE_VERSION_BRANCH', 'trunk');
