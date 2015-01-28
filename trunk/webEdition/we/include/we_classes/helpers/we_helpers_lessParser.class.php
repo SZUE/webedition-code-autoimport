fc94d0c8373550e62d94518f428026438f1f3aef
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-01-28 17:05:25 +0000

    Fix #9407

--- trunk/webEdition/we/include/we_classes/helpers/we_helpers_lessParser.class.php
+++ trunk/webEdition/we/include/we_classes/helpers/we_helpers_lessParser.class.php
@@ -23,6 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 class we_helpers_lessParser extends Less_Parser{
+	public static $includedFiles = array();
 
 	public function __construct($env = null){
 		if(!is_object($env)){
@@ -36,6 +37,9 @@ class we_helpers_lessParser extends Less_Parser{
 		if($env->path->value && preg_match('|#WE:(\d+)#|', $env->path->value, $matches)){
 			$hash = getHash('SELECT Path,ParentID FROM ' . FILE_TABLE . ' WHERE ID=' . intval($matches[1]), null, MYSQL_NUM);
 			list($path, $parentid) = ($hash? : array(0, 0));
+			if($hash){
+				self::$includedFiles[] = intval($matches[1]);
+			}
 			return array($path ? $env->path->value : $path, $parentid ? f('SELECT Path FROM ' . FILE_TABLE . ' WHERE ID=' . $parentid) : '/');
 		}
 		return null;
@@ -43,6 +47,7 @@ class we_helpers_lessParser extends Less_Parser{
 
 	public function SetInput($file_path){
 		if(is_numeric($file_path)){
+			self::$includedFiles[] = $file_path;
 			$this->input = f('SELECT c.Dat FROM ' . LINK_TABLE . ' l JOIN ' . CONTENT_TABLE . ' c ON l.CID=c.ID WHERE l.Type="txt" AND l.Name="data" AND l.DocumentTable="tblFile" AND l.DID=' . intval($file_path));
 			$file_path = '';
 		}
