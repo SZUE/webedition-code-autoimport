b25dd9dc6e4c8145ec132387530f9b5c29a226b2
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-01-28 11:42:36 +0000

    Fix #9490

--- webEdition/we/include/we_classes/contents/we_imageDocument.class.php
+++ webEdition/we/include/we_classes/contents/we_imageDocument.class.php
@@ -240,13 +240,6 @@ class we_imageDocument extends we_binaryDocument{
 		if(!$this->getElement('RollOverFlag')){
 			return '';
 		}
-		if(!$src){
-			$src = (we_isHttps() ? '' : BASE_IMG) . $this->Path;//FIXME:remove BASE_IMG
-		}
-
-		if(!$src_over){
-			$src_over = (we_isHttps() ? '' : BASE_IMG) . f('SELECT Path FROM ' . FILE_TABLE . ' WHERE ID=' . intval($this->getElement('RollOverID')), 'Path', $this->DB_WE);
-		}
 
 		if(!$this->getElement('name')){
 			$this->setElement('name', 'ro_' . $this->Name, 'attrib');
@@ -255,8 +248,8 @@ class we_imageDocument extends we_binaryDocument{
 		$js = '
 img' . self::$imgCnt . 'Over = new Image();
 img' . self::$imgCnt . 'Out = new Image();
-img' . self::$imgCnt . 'Over.src = "' . $src_over . '";
-img' . self::$imgCnt . 'Out.src = "' . $src . '";';
+img' . self::$imgCnt . 'Over.src = "' . ($src_over? : f('SELECT Path FROM ' . FILE_TABLE . ' WHERE ID=' . intval($this->getElement('RollOverID')), '', $this->DB_WE)) . '";
+img' . self::$imgCnt . 'Out.src = "' . ($src? : $this->Path) . '";';
 		return ($useScript ? we_html_element::jsElement($js) : $js);
 	}
 
@@ -413,10 +406,7 @@ img' . self::$imgCnt . 'Out.src = "' . $src . '";';
 			srand((double) microtime() * 1000000);
 			$randval = rand();
 			$src = $dyn ?
-				WEBEDITION_DIR . 'we_cmd.php?we_cmd[0]=show_binaryDoc&we_cmd[1]=' .
-				$this->ContentType . '&we_cmd[2]=' .
-				$GLOBALS['we_transaction'] . '&rand=' . $randval :
-				($this->getElement('LinkType') == we_base_link::TYPE_INT ? (we_isHttps() ? '' : BASE_IMG) : '') .
+				WEBEDITION_DIR . 'we_cmd.php?we_cmd[0]=show_binaryDoc&we_cmd[1]=' . $this->ContentType . '&we_cmd[2]=' . $GLOBALS['we_transaction'] . '&rand=' . $randval :
 				$img_path;
 
 			if($this->issetElement('sizingrel')){
@@ -459,7 +449,7 @@ img' . self::$imgCnt . 'Out.src = "' . $src . '";';
 				'src' => $src
 			);
 
-			$filter = array('filesize', 'type', 'id', 'showcontrol', 'showthumbcontrol', 'thumbnail', 'href', 'longdescid', 'showimage', 'showinputs', 'listviewname', 'parentid', 'startid', 'origwidth', 'origheight','useMetaTitle'); //  dont use these array-entries
+			$filter = array('filesize', 'type', 'id', 'showcontrol', 'showthumbcontrol', 'thumbnail', 'href', 'longdescid', 'showimage', 'showinputs', 'listviewname', 'parentid', 'startid', 'origwidth', 'origheight', 'useMetaTitle'); //  dont use these array-entries
 
 			if(defined('HIDENAMEATTRIBINWEIMG_DEFAULT') && HIDENAMEATTRIBINWEIMG_DEFAULT){
 				$filter[] = 'name';
