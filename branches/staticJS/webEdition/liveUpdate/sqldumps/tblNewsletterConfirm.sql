4401ab59d798323713e8ed9250635f77705a2598
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-02-02 10:16:33 +0000

    better tables

--- branches/staticJS/webEdition/liveUpdate/sqldumps/tblNewsletterConfirm.sql
+++ branches/staticJS/webEdition/liveUpdate/sqldumps/tblNewsletterConfirm.sql
@@ -7,7 +7,7 @@ CREATE TABLE ###TBLPREFIX###tblNewsletterConfirm (
   subscribe_firstname varchar(255) NOT NULL default '',
   subscribe_lastname varchar(255) NOT NULL default '',
   lists text NOT NULL,
-  expires bigint(20) unsigned NOT NULL default '0',
+  expires int(11) unsigned NOT NULL default '0',
   PRIMARY KEY (confirmID(30)),
   KEY expires (expires),
   KEY subscribe (subscribe_mail(50),confirmID(30))
--- branches/staticJS/webEdition/liveUpdate/sqldumps/tblWebAdmin.sql
+++ branches/staticJS/webEdition/liveUpdate/sqldumps/tblWebAdmin.sql
@@ -1,14 +1,10 @@
-###UPDATEONLY###CREATE TEMPORARY TABLE IF NOT EXISTS _tblWebAdmin(
-  Name varchar(255) NOT NULL default '',
-  `Value` text NOT NULL,
-  PRIMARY KEY (Name)
-)ENGINE = MYISAM;
+###ONKEYFAILED(PRIMARY,###TBLPREFIX###tblWebAdmin) CREATE TEMPORARY TABLE IF NOT EXISTS _tblWebAdmin( Name varchar(255) NOT NULL default '', `Value` text NOT NULL,  PRIMARY KEY (Name))ENGINE = MYISAM;
 /* query separator */
-###UPDATEONLY###INSERT IGNORE INTO _tblWebAdmin SELECT DISTINCT * FROM ###TBLPREFIX###tblWebAdmin GROUP BY Name;
+###ONKEYFAILED(PRIMARY,###TBLPREFIX###tblWebAdmin) INSERT IGNORE INTO _tblWebAdmin SELECT DISTINCT * FROM ###TBLPREFIX###tblWebAdmin GROUP BY Name;###
 /* query separator */
-###UPDATEONLY###TRUNCATE ###TBLPREFIX###tblWebAdmin;
+###ONKEYFAILED(PRIMARY,###TBLPREFIX###tblWebAdmin) TRUNCATE ###TBLPREFIX###tblWebAdmin;###
 /* query separator */
-###UPDATEONLY###INSERT INTO ###TBLPREFIX###tblWebAdmin SELECT * FROM _tblWebAdmin;
+###ONKEYFAILED(PRIMARY,###TBLPREFIX###tblWebAdmin) INSERT INTO ###TBLPREFIX###tblWebAdmin SELECT * FROM _tblWebAdmin;###
 /* query separator */
 
 ###UPDATEONLY###DROP TEMPORARY TABLE IF EXISTS _tblWebAdmin;
@@ -19,6 +15,7 @@ CREATE TABLE ###TBLPREFIX###tblWebAdmin (
   `Value` text NOT NULL,
   PRIMARY KEY (Name)
 ) ENGINE=MyISAM;
+
 /* query separator */
 INSERT IGNORE INTO ###TBLPREFIX###tblWebAdmin SET Name='FieldAdds',Value='a:13:{s:8:"Username";a:1:{s:4:"type";s:5:"input";}s:8:"Password";a:1:{s:4:"type";s:5:"input";}s:8:"Forename";a:1:{s:4:"type";s:5:"input";}s:7:"Surname";a:1:{s:4:"type";s:5:"input";}s:11:"LoginDenied";a:1:{s:4:"type";s:5:"input";}s:11:"MemberSince";a:1:{s:4:"type";s:5:"input";}s:9:"LastLogin";a:1:{s:4:"type";s:5:"input";}s:10:"LastAccess";a:1:{s:4:"type";s:5:"input";}s:15:"AutoLoginDenied";a:1:{s:4:"type";s:5:"input";}s:9:"AutoLogin";a:1:{s:4:"type";s:5:"input";}s:13:"Anrede_Anrede";a:2:{s:7:"default";s:10:",Herr,Frau";s:4:"type";s:6:"select";}s:13:"Newsletter_Ok";a:2:{s:7:"default";s:3:",ja";s:4:"type";s:6:"select";}s:25:"Newsletter_HTMLNewsletter";a:2:{s:7:"default";s:3:",ja";s:4:"type";s:6:"select";}}';
 /* query separator */
