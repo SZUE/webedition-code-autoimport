3062843247a808407da29003409bd0e06cf49e33
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-02-09 15:37:14 +0000

    changed type

--- branches/mgallery/webEdition/liveUpdate/sqldumps/tblFileLink.sql
+++ branches/mgallery/webEdition/liveUpdate/sqldumps/tblFileLink.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ###TBLPREFIX###tblFileLink (
   ID int unsigned NOT NULL,
 	DocumentTable enum('tblFile','tblObjectFiles','tblVFile','tblCategorys') NOT NULL default 'tblFile',
-	type enum('image','master','archive') NOT NULL default 'image',
+	type enum('image','document','object','master','archive') NOT NULL default 'image',
 	remObj int unsigned NOT NULL default '0',
 	remTable enum('tblFile','tblObjectFiles','tblVFile') NOT NULL default 'tblFile',
 	`position` int unsigned NOT NULL default '0',
