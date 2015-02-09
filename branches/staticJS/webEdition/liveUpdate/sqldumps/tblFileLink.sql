80e80b3bb9dd44e8aba18cc3aa3541bc2b622f26
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-02-09 14:53:05 +0000

    error tblcreate

--- branches/staticJS/webEdition/liveUpdate/sqldumps/tblFileLink.sql
+++ branches/staticJS/webEdition/liveUpdate/sqldumps/tblFileLink.sql
@@ -1,9 +1,10 @@
 CREATE TABLE ###TBLPREFIX###tblFileLink (
-  ID int(11) unsigned NOT NULL auto_increment,
+  ID int unsigned NOT NULL,
 	DocumentTable enum('tblFile','tblObjectFiles','tblVFile') NOT NULL default 'tblFile',
 	type enum('image','master','archive') NOT NULL default 'image',
 	remObj int unsigned NOT NULL default '0',
+	remTable enum('tblFile','tblObjectFiles','tblVFile') NOT NULL default 'tblFile',
 	`position` int unsigned NOT NULL default '0',
-	PRIMARY KEY  (ID,DocumentTable,type),
-	KEY remObj (DocumentTable,remObj)
+	PRIMARY KEY  (ID,DocumentTable,remObj),
+	KEY remObj (remTable,remObj)
 ) ENGINE=MyISAM;
