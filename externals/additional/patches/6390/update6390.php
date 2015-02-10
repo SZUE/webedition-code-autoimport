8775e7ca1b1ac0453af58fcc6464eb44faf97004
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-02-10 10:05:20 +0000

    changed pacth

--- externals/additional/patches/6390/update6390.php
+++ externals/additional/patches/6390/update6390.php
@@ -1,17 +1,17 @@
-﻿<?php
+<?php
 /*
 Attempt to update
 */
 // only execute on liveUpdate:
 //aber meiner Meinung nach nicht notwendig, patches werden nicht ausgeführt
 if(!is_readable("../../we/include/conf/we_conf.inc.php")) {
-	//return true;
+        //return true;
 }
 include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/we.inc.php");
 
 
 function up6390_updateConf(){
-	$filename= $_SERVER["DOCUMENT_ROOT"].'/webEdition/we/include/conf/we_conf.inc.php';
+        $filename= $_SERVER["DOCUMENT_ROOT"].'/webEdition/we/include/conf/we_conf.inc.php';
         
         if(($conf = file_get_contents($filename))){
             $conf = str_replace(array('include_once($_SERVER["DOCUMENT_ROOT"]."/webEdition/we/include/"."db_mysql.inc.php");', '?>'), '', $conf);
@@ -22,5 +22,4 @@ function up6390_updateConf(){
 
 up6390_updateConf();
 //file_put_contents($_SERVER["DOCUMENT_ROOT"]."/say_6390.txt", 'patch gelaufen 6390');
-we_updater::doUpdate();
 return true;
