349e78a2a4b6281612eb218d79f8335032c36eee
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-02-17 12:19:02 +0000

    new version

--- trunk/webEdition/we/include/we_version.php
+++ trunk/webEdition/we/include/we_version.php
@@ -1,7 +1,7 @@
 <?php
-	define('WE_VERSION', '6.4.0.2');
+	define('WE_VERSION', '6.4.0.3');
 	define('WE_ZFVERSION', '1.12.3'); // recommended version of the Zend Framework (bundled with webEdition)
-	define('WE_SVNREV', '9328');
+	define('WE_SVNREV', '$Rev$');
 	define('WE_VERSION_SUPP_VERSION', '0');
-	define('WE_VERSION_SUPP','');
+	define('WE_VERSION_SUPP','nightly');
 	define('WE_VERSION_BRANCH', 'trunk');
