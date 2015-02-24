6c0c7944d81a43fdf4717dd35f4eee57e33b8dd8
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-02-24 10:30:08 +0000

    Fix autologins

--- trunk/webEdition/we/include/we_tags/we_tag_saveRegisteredUser.inc.php
+++ trunk/webEdition/we/include/we_tags/we_tag_saveRegisteredUser.inc.php
@@ -106,7 +106,7 @@ function we_tag_saveRegisteredUser($attribs){
 			we_saveCustomerImages();
 			$set_a = we_tag_saveRegisteredUser_processRequest($protected, $allowed);
 			$password = we_base_request::_(we_base_request::RAW, 's', false, 'Password');
-			if($password != we_customer_customer::NOPWD_CHANGE && $password != $_SESSION['webuser']['Password']){//bei Password�nderungen m�ssen die Autologins des Users gel�scht werden
+			if($password != we_customer_customer::NOPWD_CHANGE && !we_customer_customer::comparePassword(f('SELECT Password FROM ' . CUSTOMER_TABLE . ' WHERE ID=' . $_SESSION['webuser']['ID']), $password)){//bei Passwordaenderungen muessen die Autologins des Users geloescht werden
 				$GLOBALS['DB_WE']->query('DELETE FROM ' . CUSTOMER_AUTOLOGIN_TABLE . ' WHERE WebUserID=' . intval($_SESSION['webuser']['ID']));
 			}
 			if($set_a){
