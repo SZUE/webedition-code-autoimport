8ffa2219bbf7836d72395629c30642e1b17868bd
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-02-26 14:43:48 +0000

    some tags more explicit

---
+++ webEdition/we/include/we_tags/we_tag_ifNotObjectLanguage.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * webEdition CMS
+ *
+ * This source is part of webEdition CMS. webEdition CMS is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the textfile
+ * webEdition/licenses/webEditionCMS/License.txt
+ *
+ * @category   webEdition
+ * @package none
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL
+ */
+function we_parse_tag_ifNotObjectLanguage($attribs, $content){
+	return '<?php if(!' . we_tag_tagParser::printTag('ifObjectLanguage', $attribs) . '){ ?>' . $content . '<?php } ?>';
+}
+
+function we_tag_ifNotObjectLanguage($attribs){
+	return !we_tag('ifObjectLanguage', $attribs);
+}
