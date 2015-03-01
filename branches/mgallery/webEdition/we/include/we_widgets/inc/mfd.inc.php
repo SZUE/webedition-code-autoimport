711e2e01906a92490e2936b642c0550fb56f3e11
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-03-01 13:57:56 +0000

    Fix lastmod layout

--- branches/mgallery/webEdition/we/include/we_widgets/inc/mfd.inc.php
+++ branches/mgallery/webEdition/we/include/we_widgets/inc/mfd.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * webEdition CMS
  *
@@ -23,13 +22,13 @@
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
 $oTblCont = new we_html_table(array(
-	"border" => 0, "cellpadding" => 0, "cellspacing" => 0
+	"border" => 0, "cellpadding" => 0, "cellspacing" => 0, 'width' => '100%'
 	), 1, 1);
 $oTblCont->setCol(
 	0, 0, null, we_html_element::htmlDiv(
 		array(
 		'id' => 'm_' . $iCurrId . '_inline',
-		"style" => "width:100%;height:" . ($aPrefs[$aProps[0]]["height"]) . "px;overflow:auto;"
+		"style" => 'width:100%;height:' . ($aPrefs[$aProps[0]]["height"]) . 'px;overflow:auto;'
 		), we_html_element::htmlDiv(array('id' => 'mfd_data'), $lastModified)
 ));
 $aLang = array(
