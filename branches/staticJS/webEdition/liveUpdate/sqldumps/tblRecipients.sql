f5afe536168a7d230933edd7cc69708fa4ef5c5c
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-03-09 15:11:08 +0000

    remove unused branch

--- branches/staticJS/webEdition/liveUpdate/sqldumps/tblRecipients.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE ###TBLPREFIX###tblRecipients (
-  ID smallint(6) unsigned NOT NULL auto_increment,
-  Email varchar(255) NOT NULL default '',
-  PRIMARY KEY  (ID),
-  UNIQUE KEY Email (Email)
-) ENGINE=MyISAM;
