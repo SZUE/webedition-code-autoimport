b9d292793725caf4451bfb39754d558f419e5b64
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-03-19 15:42:52 +0000

    fix file if we is contained

--- webEdition/we/include/we_classes/base/we_base_request.class.php
+++ webEdition/we/include/we_classes/base/we_base_request.class.php
@@ -171,7 +171,7 @@ class we_base_request{
 						)), ','));
 				foreach($var as &$cur){
 					$cur = ($type == self::WEFILELIST || $type == self::WEFILELISTA ? $cur : filter_var($cur, FILTER_SANITIZE_URL));
-					if(strpos($cur, rtrim(WEBEDITION_DIR, '/')) === 0){//file-selector has propably access
+					if($cur === rtrim(WEBEDITION_DIR, '/') || strpos($cur, WEBEDITION_DIR) === 0){//file-selector has propably access
 						if(!(strstr($cur, SITE_DIR) || strstr($cur, TEMP_DIR))){//allow site/tmp dir
 							$cur = isset($GLOBALS['supportDebugging']) ? $cur : '-1';
 						}
