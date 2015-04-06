f284d838fed82b1ac0bab423030915f494a71bdc
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2015-04-06 19:11:08 +0000

    Fix #9697

--- branches/mgallery/webEdition/redirectSEOurls.php
+++ branches/mgallery/webEdition/redirectSEOurls.php
@@ -31,65 +31,42 @@ if((isset($GLOBALS['we_editmode']) && $GLOBALS['we_editmode'])){
 	return;
 }
 
+$urlLookingFor = (isset($_SERVER['REDIRECT_URL']) && $_SERVER['REDIRECT_URL'] && !strpos($_SERVER['REDIRECT_URL'], ltrim(WEBEDITION_DIR, "/"))) ?
+	urldecode($_SERVER['REDIRECT_URL']) :
+	(isset($_SERVER['REQUEST_URI']) && ($_SERVER['REQUEST_URI']) && !strpos($_SERVER['REQUEST_URI'], ltrim(WEBEDITION_DIR, "/")) ?
+		parse_url(urldecode($_SERVER['REQUEST_URI']),PHP_URL_PATH) : 
+		'');
+
+if(!$urlLookingFor){
+	return;
+}
+
 /**
  * url without query string
  * we need this in some we_tag()
  */
-define('WE_REDIRECTED_SEO', (isset($_SERVER['REDIRECT_URL']) ?
-		$_SERVER['REDIRECT_URL'] :
-		(isset($_SERVER['PHP_SELF']) ?
-			$_SERVER['PHP_SELF'] :
-			$_SERVER['SCRIPT_NAME'])
-	)
-);
-
-//do we need this any more?
-$prefix = f('SELECT Path FROM ' . FILE_TABLE . ' WHERE urlMap="' . $DB_WE->escape($_SERVER["HTTP_HOST"]) . '"'); //multidomains
-
-$urlQueryString = array();
-
-if(isset($_SERVER['SCRIPT_URL']) && ($_SERVER['SCRIPT_URL']) && !strpos($_SERVER['SCRIPT_URL'], WEBEDITION_DIR)){
-	$pathParts = pathinfo($prefix . urldecode($_SERVER['SCRIPT_URL']));
-} elseif(isset($_SERVER['REDIRECT_URL']) && ($_SERVER['REDIRECT_URL']) && !strpos($_SERVER['REDIRECT_URL'], WEBEDITION_DIR)){
-	$pathParts = pathinfo($prefix . urldecode($_SERVER['REDIRECT_URL']));
-} elseif(isset($_SERVER['REQUEST_URI']) && ($_SERVER['REQUEST_URI']) && !strpos($_SERVER['REQUEST_URI'], WEBEDITION_DIR)){
-	$splittUrl = (strpos($_SERVER['REQUEST_URI'], '?') !== false) ? explode('?', urldecode($_SERVER['REQUEST_URI'])) : urldecode($_SERVER['REQUEST_URI']);
-	if(is_array($splittUrl)){
-		$pathParts = pathinfo($prefix . $splittUrl[0]);
-		parse_str($splittUrl[1], $urlQueryString); //get query string
-	} else {
-		$pathParts = pathinfo($prefix . $splittUrl);
-	}
-} else {
-	$pathParts = array();
-}
-
-//get query string if there
-if(isset($_SERVER['REDIRECT_QUERY_STRING']) && ($_SERVER['REDIRECT_QUERY_STRING'])){
-	parse_str($_SERVER['REDIRECT_QUERY_STRING'], $urlQueryString);
-} elseif(isset($_SERVER['QUERY_STRING']) && ($_SERVER['QUERY_STRING'])){
-	parse_str($_SERVER['QUERY_STRING'], $urlQueryString);
-}
+define('WE_REDIRECTED_SEO', $urlLookingFor);
 
 /**
- * now, we looking for an object ID an starting with the last part of the URL
- * for that we split the URL via pathinfo()
- * and then we are checking the URL from right to left
- * e.g.
- * /mainfolder/subfolder/part-1-of-seo-url/part-2-of-seo-url
+ * now, we looking for an object ID an starting with full path of the URL
+ * and then we are checking the URL from left to right
+ * e.g. /mainfolder/subfolder/part-1-of-seo-url/part-2-of-seo-url
+ * in /mainfolder/subfolder is a dynamic webEdition document aka Trigger document
+ * this Trigger document is part oh the whole URL but not of the object SEO-URL
  *
- * first check: part-2-of-seo-url --> nothing is found
- * second check: part-1-of-seo-url/part-2-of-seo-url --> we get the object
+ * first check: mainfolder/subfolder/part-1-of-seo-url/part-2-of-seo-url --> nothing is found
+ * second check: subfolder/part-1-of-seo-url/part-2-of-seo-url --> nothing is found
+ * third check: part-1-of-seo-url/part-2-of-seo-url --> we get the object
  * and so one
  */
-$urlLookingFor = '';
-while(($pathParts['filename'])){
-	$urlLookingFor = (URLENCODE_OBJECTSEOURLS ?
-			strtr(urlencode($pathParts['filename']), array('%2F' => '/', '//' => '/')) :
-			strtr($pathParts['filename'], array('//' => '/'))
-		) . (($urlLookingFor) ? '/' . $urlLookingFor : '');
 
-	if(($object = getHash('SELECT ID,TriggerID FROM ' . OBJECT_FILES_TABLE . ' WHERE Published>0 AND Url="' . $GLOBALS['DB_WE']->escape($urlLookingFor) . '" LIMIT 1'))){
+$urlLookingFor = (URLENCODE_OBJECTSEOURLS ?
+	strtr(urlencode($urlLookingFor), array('%2F' => '/', '//' => '/')) :
+	strtr($urlLookingFor, array('//' => '/'))
+	);
+ 
+while($urlLookingFor){
+	if(($object = getHash('SELECT ID,TriggerID FROM ' . OBJECT_FILES_TABLE . ' WHERE Published>0 AND Url LIKE "' . $GLOBALS['DB_WE']->escape($urlLookingFor) . '" LIMIT 1'))){
 
 		//remove all cookies from Request String if set (if not, cookies are exposed on listviews etc & max interfer with given Cookies)
 		if(stristr('C', ini_get('request_order') ? : ini_get('variables_order'))){
@@ -97,6 +74,14 @@ while(($pathParts['filename'])){
 			foreach(array_keys($_COOKIE) as $name){
 				unset($_REQUEST[$name]);
 			}
+		}
+
+		//get query string if there
+		$urlQueryString = array();
+		if(isset($_SERVER['REDIRECT_QUERY_STRING']) && ($_SERVER['REDIRECT_QUERY_STRING'])){
+			parse_str($_SERVER['REDIRECT_QUERY_STRING'], $urlQueryString);
+		} elseif(isset($_SERVER['QUERY_STRING']) && ($_SERVER['QUERY_STRING'])){
+			parse_str($_SERVER['QUERY_STRING'], $urlQueryString);
 		}
 
 		//should we also send $_GET?
@@ -112,7 +97,7 @@ while(($pathParts['filename'])){
 
 		exit;
 	} else {//reduce the rest of the given url and try again
-		$pathParts = pathinfo($pathParts['dirname']);
+		$urlLookingFor = ltrim(stristr($urlLookingFor, "/"), "/");
 	}
 }
 
@@ -121,7 +106,7 @@ while(($pathParts['filename'])){
  * for seo it's better to make a redirect to errorDoc404 instead of include,
  * but for that we need en separate webedition config parameter
  *
- * header("Location: http://" . $_SERVER['HTTP_HOST'] . $errorDoc404, true, (SUPPRESS404CODE ? 200 : 404));
+ * header("Location: //" . $_SERVER['HTTP_HOST'] . $errorDoc404, true, (SUPPRESS404CODE ? 200 : 404));
  */
 we_html_tools::setHttpCode(SUPPRESS404CODE ? 200 : 404);
 $errorDoc404 = ERROR_DOCUMENT_NO_OBJECTFILE ? id_to_path(ERROR_DOCUMENT_NO_OBJECTFILE, FILE_TABLE) : 0;
