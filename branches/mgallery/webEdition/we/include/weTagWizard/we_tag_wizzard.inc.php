a9075b3c9eb96d626c5d562d37f7c35782b60356
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-04-09 22:14:48 +0000

    Fix ;

--- branches/mgallery/webEdition/we/include/weTagWizard/we_tag_wizzard.inc.php
+++ branches/mgallery/webEdition/we/include/weTagWizard/we_tag_wizzard.inc.php
@@ -57,7 +57,7 @@ if(($typeAttribute = $weTag->getTypeAttribute())){
 	if($_typeOptions){
 		$typeAttributeJs .= '
 var typeAttributeAllows = {};
-var typeAttributeRequires = {}';
+var typeAttributeRequires = {};';
 
 		foreach($_typeOptions as $option){
 			$_allowedAttribs = $option->getAllowedAttributes();
