d33c926792fea522c202358353279f27a075bf10
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-04-10 20:08:00 +0000

    small fix medial links

--- branches/mgallery/webEdition/liveUpdate/sqldumps/tblFileLink.sql
+++ branches/mgallery/webEdition/liveUpdate/sqldumps/tblFileLink.sql
@@ -3,7 +3,7 @@ CREATE TABLE ###TBLPREFIX###tblFileLink (
 	DocumentTable enum('tblFile','tblObjectFiles','tblVFile','tblCategorys') NOT NULL default 'tblFile',
 	type enum('media','document','object','master','archive') NOT NULL default 'media',
 	remObj int unsigned NOT NULL default '0',
-	remTable enum('tblFile','tblObjectFiles','tblVFile') NOT NULL default 'tblFile',
+	remTable enum('tblFile','tblObjectFiles','tblVFile','tblCategorys','tblTemplates','tblNavigation') NOT NULL default 'tblFile',
 	`position` int unsigned NOT NULL default '0',
 	`isTemp` tinyint(1) unsigned NOT NULL default '0',
 	PRIMARY KEY (ID,DocumentTable,`type`,remObj,`position`,isTemp),
