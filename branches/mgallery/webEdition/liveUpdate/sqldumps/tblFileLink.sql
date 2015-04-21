bc606bd0df1e70d6a5e377ecd2b2cb18807b4fbf
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-04-21 17:05:47 +0000

    table comment

--- branches/mgallery/webEdition/liveUpdate/sqldumps/tblFileLink.sql
+++ branches/mgallery/webEdition/liveUpdate/sqldumps/tblFileLink.sql
@@ -4,8 +4,8 @@ CREATE TABLE ###TBLPREFIX###tblFileLink (
 	type enum('media','document','object','master','archive','collection') NOT NULL default 'media' COMMENT 'referenced type',
 	remObj int unsigned NOT NULL default '0' COMMENT 'the referenced object',
 	remTable enum('tblFile','tblObjectFiles') NOT NULL default 'tblFile' COMMENT 'the table where to find the referenced object',
-	`position` int unsigned NOT NULL default '0' COMMENT 'optional position in case ordering is important',
-	`isTemp` tinyint(1) unsigned NOT NULL default '0',
+	`position` smallint unsigned NOT NULL default '0' COMMENT 'optional position in case ordering is important',
+	`isTemp` tinyint(1) unsigned NOT NULL default '0' COMMENT 'this is one if the referenced object is in temporary table',
 	PRIMARY KEY (ID,DocumentTable,`type`,remObj,`position`,isTemp),
 	KEY remObj (remTable,remObj)
 ) ENGINE=MyISAM;
