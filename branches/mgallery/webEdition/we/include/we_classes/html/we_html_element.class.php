2b931b22045f8932b45abc4e6e6675d93d5e3969
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-06-14 11:00:48 +0000

    changed default doctype to html5

--- branches/mgallery/webEdition/we/include/we_classes/html/we_html_element.class.php
+++ branches/mgallery/webEdition/we/include/we_classes/html/we_html_element.class.php
@@ -322,11 +322,11 @@ abstract class we_html_element{
 	 */
 	public static function htmlDocType($version = '4Trans'){
 		switch($version){
+			default:
 			case 5:
 			case '5':
 				return '<!DOCTYPE html>';
 			case '4Trans':
-			default:
 				return '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">';
 		}
 	}
