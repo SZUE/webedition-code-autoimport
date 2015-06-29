ec84313d3a3edf32949a2e0cef633d21838c927d
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2015-06-29 21:55:01 +0000

    Fix #9229

--- trunk/webEdition/we/include/we_tags/we_tag_ifFieldEmpty.inc.php
+++ trunk/webEdition/we/include/we_tags/we_tag_ifFieldEmpty.inc.php
@@ -133,7 +133,7 @@ function we_isFieldNotEmpty($attribs){
 			}
 		//no break;
 	}
-	return $GLOBALS['lv']->f($match) != '';
+	return $GLOBALS['lv']->f($match) ? true : false;
 }
 
 function we_tag_ifFieldEmpty($attribs){
