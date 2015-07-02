11be426ad410c1c1494fd8cee0f7f2a5fa30d636
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-07-02 13:02:27 +0000

    small fix

--- branches/mgallery/webEdition/we/include/we_classes/contents/we_collection.class.php
+++ branches/mgallery/webEdition/we/include/we_classes/contents/we_collection.class.php
@@ -524,7 +524,7 @@ $jsStorageItems;
 
 	private function makeGridItem($item, $index){
 		$idname = 'collectionItem_we_id_' . $index;
-		$wecmd1 = "document.we_form.elements['collectionItem_we_id_" . $idname . "'].value";
+		$wecmd1 = "document.we_form.elements['" . $idname . "'].value";
 		$wecmd2 = "";
 		$wecmd3 = "opener._EditorFrame.setEditorIsHot(true);try{opener._EditorFrame.getContentEditor().weCollectionEdit.callForValidItemsAndInsert(" . $index . ", opener._EditorFrame.getContentEditor().document.we_form.elements['collectionItem_we_id_" . $index . "'].value);} catch(e){}";
 
