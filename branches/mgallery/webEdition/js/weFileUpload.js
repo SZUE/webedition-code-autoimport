565a7eb5f6abc84859c1046dccdfe5afc7419680
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-08-12 16:31:23 +0000

    fileUpload: better clientside image scaling

--- branches/mgallery/webEdition/js/weFileUpload.js
+++ branches/mgallery/webEdition/js/weFileUpload.js
@@ -273,6 +273,8 @@ var weFileUpload = (function () {
 				quality: 0.8,
 				degrees: 0
 			};
+			this.EDIT_IMAGES_CLIENTSIDE = true;
+
 			this.resetParams = function () {
 			};
 
@@ -280,14 +282,36 @@ var weFileUpload = (function () {
 				return true;
 			};
 
-
 			//new client side image editing
 			this.transformAndSendFile = function (cur) {
+				top.console.debug(this.transformAll);
 				var reader = new FileReader();
 				reader.onloadend = function () {
-					var tempImg = new Image();
+					var tempImg = new Image(),
+						ratio = 1;
+
 					tempImg.src = reader.result;
 					tempImg.onload = function () {
+						if(_.sender.transformAll.width){
+							ratio = _.sender.transformAll.widthSelect === 'percent' ? _.sender.transformAll.width / 100 : _.sender.transformAll.width / tempImg.width;
+						} else if(_.sender.transformAll.height){
+							ratio = _.sender.transformAll.heightSelect === 'percent' ? _.sender.transformAll.height / 100 : _.sender.transformAll.height / tempImg.height;
+						}
+						ratio = ratio < 1 ? ratio : 1;
+
+						/* var 1: GameAlchemist @ http://stackoverflow.com/questions/18922880/html5-canvas-resize-downscale-image-high-quality */
+						var canv = document.createElement('canvas');
+						canv.width = tempImg.width;
+						canv.height = tempImg.height;
+						canv.getContext('2d').drawImage(tempImg, 0, 0);
+						var resulting_canvas = _.utils.downScaleCanvas(canv, ratio);
+						var canvas = resulting_canvas;
+
+						/* var 2: K3N @ http://stackoverflow.com/questions/17861447/html5-canvas-drawimage-how-to-apply-antialiasing */
+
+
+						/* var 3: simple resize + rotation */
+ 						/*
 						var MAX_WIDTH = 600;
 						var MAX_HEIGHT = 600;
 						var tempW = tempImg.width;
@@ -303,11 +327,11 @@ var weFileUpload = (function () {
 								tempH = MAX_HEIGHT;
 							}
 						}
-
+						
 						var canvas = document.createElement('canvas'),
-										ctx = canvas.getContext("2d"),
-										deg = 0,
-										x = 0, y = 0;
+							ctx = canvas.getContext("2d"),
+							deg = 0,
+							x = 0, y = 0;
 
 						canvas.width = tempW;
 						canvas.height = tempH;
@@ -330,23 +354,25 @@ var weFileUpload = (function () {
 						}
 						ctx.rotate(-Math.PI * deg / 180);
 						ctx.drawImage(tempImg, x, y, tempW, tempH);
+						*/
+
 
 						canvas.toBlob(function (blob) {
-							var arrayBufferNew = null;
-							var fr = new FileReader();
-							fr.onload = function (e) {
-								arrayBufferNew = this.result;
-								cur.dataArray = new Uint8Array(arrayBufferNew);
-								_.sender.sendNextChunk(true);
+								var arrayBufferNew = null;
+								var fr = new FileReader();
+								fr.onload = function (e) {
+									arrayBufferNew = this.result;
+									cur.dataArray = new Uint8Array(arrayBufferNew);
+									_.sender.sendNextChunk(true);
 
-							};
-							cur.size = blob.size;
-							//_.view.setInternalProgress(0);
-							cur.totalParts = Math.ceil(blob.size / _.sender.chunkSize);
-							cur.lastChunkSize = blob.size % _.sender.chunkSize;
-							fr.readAsArrayBuffer(blob);
-						}, "image/jpeg", 0.8
-										);
+								};
+								cur.size = blob.size;
+								//_.view.setInternalProgress(0);
+								cur.totalParts = Math.ceil(blob.size / _.sender.chunkSize);
+								cur.lastChunkSize = blob.size % _.sender.chunkSize;
+								fr.readAsArrayBuffer(blob);
+							}, "image/jpeg", 1.0
+						);
 
 					};
 				};
@@ -372,8 +398,8 @@ var weFileUpload = (function () {
 
 							var transformables = ['image/jpeg', 'image/gif', 'image/png'];//TODO: add all transformable types
 
-							//clientside editing diabled!
-							if (false && this.transformAll.doTrans && transformables.indexOf(cur.type) !== -1) {//TODO: && !cur.doTrans!!)
+							//clientside editing disabled!
+							if (this.EDIT_IMAGES_CLIENTSIDE && this.transformAll.doTrans && transformables.indexOf(cur.type) !== -1) {//TODO: && !cur.doTrans!!)
 								_.sender.transformAndSendFile(cur);
 							} else {
 								fr = new FileReader();
@@ -655,6 +681,134 @@ var weFileUpload = (function () {
 					}
 				}
 				return false;
+			};
+
+			/* GameAlchemist @ http://stackoverflow.com/questions/18922880/html5-canvas-resize-downscale-image-high-quality */
+			//TODO: try to scale width and height by different ratio
+			this.downScaleCanvas = function(cv, scale) {
+				if (!(scale < 1) || !(scale > 0)) throw ('scale must be a positive number <1 ');
+				var sqScale = scale * scale; // square scale = area of source pixel within target
+				var sw = cv.width; // source image width
+				var sh = cv.height; // source image height
+				var tw = Math.floor(sw * scale); // target image width
+				var th = Math.floor(sh * scale); // target image height
+				var sx = 0, sy = 0, sIndex = 0; // source x,y, index within source array
+				var tx = 0, ty = 0, yIndex = 0, tIndex = 0; // target x,y, x,y index within target array
+				var tX = 0, tY = 0; // rounded tx, ty
+				var w = 0, nw = 0, wx = 0, nwx = 0, wy = 0, nwy = 0; // weight / next weight x / y
+				// weight is weight of current source point within target.
+				// next weight is weight of current source point within next target's point.
+				var crossX = false; // does scaled px cross its current px right border ?
+				var crossY = false; // does scaled px cross its current px bottom border ?
+				var sBuffer = cv.getContext('2d').
+				getImageData(0, 0, sw, sh).data; // source buffer 8 bit rgba
+				var tBuffer = new Float32Array(4 * sw * sh); // target buffer Float32 rgb
+				var sR = 0, sG = 0,  sB = 0; // source's current point r,g,b
+				// untested !
+				var sA = 0;  //source alpha    
+
+				for (sy = 0; sy < sh; sy++) {
+					ty = sy * scale; // y src position within target
+					tY = 0 | ty;     // rounded : target pixel's y
+					yIndex = 4 * tY * tw;  // line index within target array
+					crossY = (tY != (0 | ty + scale)); 
+					if (crossY) { // if pixel is crossing botton target pixel
+						wy = (tY + 1 - ty); // weight of point within target pixel
+						nwy = (ty + scale - tY - 1); // ... within y+1 target pixel
+					}
+					for (sx = 0; sx < sw; sx++, sIndex += 4) {
+						tx = sx * scale; // x src position within target
+						tX = 0 |  tx;    // rounded : target pixel's x
+						tIndex = yIndex + tX * 4; // target pixel index within target array
+						crossX = (tX != (0 | tx + scale));
+						if (crossX) { // if pixel is crossing target pixel's right
+							wx = (tX + 1 - tx); // weight of point within target pixel
+							nwx = (tx + scale - tX - 1); // ... within x+1 target pixel
+						}
+						sR = sBuffer[sIndex    ];   // retrieving r,g,b for curr src px.
+						sG = sBuffer[sIndex + 1];
+						sB = sBuffer[sIndex + 2];
+						sA = sBuffer[sIndex + 3];
+
+						if (!crossX && !crossY) { // pixel does not cross
+							// just add components weighted by squared scale.
+							tBuffer[tIndex    ] += sR * sqScale;
+							tBuffer[tIndex + 1] += sG * sqScale;
+							tBuffer[tIndex + 2] += sB * sqScale;
+							tBuffer[tIndex + 3] += sA * sqScale;
+						} else if (crossX && !crossY) { // cross on X only
+							w = wx * scale;
+							// add weighted component for current px
+							tBuffer[tIndex    ] += sR * w;
+							tBuffer[tIndex + 1] += sG * w;
+							tBuffer[tIndex + 2] += sB * w;
+							tBuffer[tIndex + 3] += sA * w;
+							// add weighted component for next (tX+1) px                
+							nw = nwx * scale
+							tBuffer[tIndex + 4] += sR * nw; // not 3
+							tBuffer[tIndex + 5] += sG * nw; // not 4
+							tBuffer[tIndex + 6] += sB * nw; // not 5
+							tBuffer[tIndex + 7] += sA * nw; // not 6
+						} else if (crossY && !crossX) { // cross on Y only
+							w = wy * scale;
+							// add weighted component for current px
+							tBuffer[tIndex    ] += sR * w;
+							tBuffer[tIndex + 1] += sG * w;
+							tBuffer[tIndex + 2] += sB * w;
+							tBuffer[tIndex + 3] += sA * w;
+							// add weighted component for next (tY+1) px                
+							nw = nwy * scale
+							tBuffer[tIndex + 4 * tw    ] += sR * nw; // *4, not 3
+							tBuffer[tIndex + 4 * tw + 1] += sG * nw; // *4, not 3
+							tBuffer[tIndex + 4 * tw + 2] += sB * nw; // *4, not 3
+							tBuffer[tIndex + 4 * tw + 3] += sA * nw; // *4, not 3
+						} else { // crosses both x and y : four target points involved
+							// add weighted component for current px
+							w = wx * wy;
+							tBuffer[tIndex    ] += sR * w;
+							tBuffer[tIndex + 1] += sG * w;
+							tBuffer[tIndex + 2] += sB * w;
+							tBuffer[tIndex + 3] += sA * w;
+							// for tX + 1; tY px
+							nw = nwx * wy;
+							tBuffer[tIndex + 4] += sR * nw; // same for x
+							tBuffer[tIndex + 5] += sG * nw;
+							tBuffer[tIndex + 6] += sB * nw;
+							tBuffer[tIndex + 7] += sA * nw;
+							// for tX ; tY + 1 px
+							nw = wx * nwy;
+							tBuffer[tIndex + 4 * tw    ] += sR * nw; // same for mul
+							tBuffer[tIndex + 4 * tw + 1] += sG * nw;
+							tBuffer[tIndex + 4 * tw + 2] += sB * nw;
+							tBuffer[tIndex + 4 * tw + 3] += sA * nw;
+							// for tX + 1 ; tY +1 px
+							nw = nwx * nwy;
+							tBuffer[tIndex + 4 * tw + 4] += sR * nw; // same for both x and y
+							tBuffer[tIndex + 4 * tw + 5] += sG * nw;
+							tBuffer[tIndex + 4 * tw + 6] += sB * nw;
+							tBuffer[tIndex + 4 * tw + 7] += sA * nw;
+						}
+					} // end for sx 
+				} // end for sy
+
+				// create result canvas
+				var resCV = document.createElement('canvas');
+				resCV.width = tw;
+				resCV.height = th;
+				var resCtx = resCV.getContext('2d');
+				var imgRes = resCtx.getImageData(0, 0, tw, th);
+				var tByteBuffer = imgRes.data;
+				// convert float32 array into a UInt8Clamped Array
+				var pxIndex = 0; //  
+				for (sIndex = 0, tIndex = 0; pxIndex < tw * th; sIndex += 4, tIndex += 4, pxIndex++) {
+					tByteBuffer[tIndex] = Math.ceil(tBuffer[sIndex]);
+					tByteBuffer[tIndex + 1] = Math.ceil(tBuffer[sIndex + 1]);
+					tByteBuffer[tIndex + 2] = Math.ceil(tBuffer[sIndex + 2]);
+					tByteBuffer[tIndex + 3] = Math.ceil(tBuffer[sIndex + 3]);
+				}
+				// writing result to canvas.
+				resCtx.putImageData(imgRes, 0, 0);
+				return resCV;
 			};
 		}
 
@@ -1109,15 +1263,17 @@ var weFileUpload = (function () {
 
 				var transformables = ['image/jpeg', 'image/gif', 'image/png'];//TODO: add all transformable types
 				//clientside editing disabled!
-				if (true || cur.partNum === cur.totalParts && this.isGdOk && transformables.indexOf(cur.type) === -1) {//transformables are transformed by js
+				//if (true || cur.partNum === cur.totalParts && this.isGdOk && transformables.indexOf(cur.type) === -1) {//transformables are transformed by js
+					/*
 					fd.append('width', sf.width.value);
 					fd.append('height', sf.height.value);
 					fd.append('widthSelect', sf.widthSelect.value);
 					fd.append('heightSelect', sf.heightSelect.value);
 					fd.append('keepRatio', sf.keepRatio.value);
 					fd.append('quality', sf.quality.value);
+					*/
 
-					if (this.isGdOk) {
+					if (transformables.indexOf(cur.type) !== -1 && cur.partNum === cur.totalParts && this.isGdOk && !this.EDIT_IMAGES_CLIENTSIDE) {
 						fd.append('thumbs', sf.thumbs.value);
 						fd.append('width', sf.width.value);
 						fd.append('height', sf.height.value);
@@ -1127,7 +1283,7 @@ var weFileUpload = (function () {
 						fd.append('quality', sf.quality.value);
 						fd.append('degrees', sf.degrees.value);
 					}
-				}
+				//}
 
 				return fd;
 			};
@@ -1587,8 +1743,8 @@ var weFileUpload = (function () {
 								_.view.elems.dragInnerRight.appendChild(_.view.preview);
 							} else {
 								var width = image.width,
-												height = image.height,
-												cv = document.createElement('canvas');
+									height = image.height,
+									cv = document.createElement('canvas');
 
 								if (width > height) {
 									if (width > maxSize) {
