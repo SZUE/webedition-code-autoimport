1a98668b1d68559dc6a5571b74fb3e046be31d23
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-09-02 12:10:35 +0000

    fix calls for we_updater::doUpdate()

--- webEdition/liveUpdate/classes/liveUpdateFunctions.class.php
+++ webEdition/liveUpdate/classes/liveUpdateFunctions.class.php
@@ -871,6 +871,14 @@ class liveUpdateFunctions{
 		return true;
 	}
 
+	function updaterDoUpdate(){
+		if(method_exists('we_updater', 'doUpdate')){
+			we_updater::doUpdate();
+		}
+
+		return true;
+	}
+
 	/**
 	 * This file sets another errorhandler - to make specific error-messages
 	 *
