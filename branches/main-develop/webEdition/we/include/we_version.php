a515f878c90dbd4c3df8a9f1bf6c591ec547f179
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-10-26 10:02:36 +0000

    removed unused branch

--- branches/main-develop/webEdition/we/include/we_version.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('WE_VERSION','6.4.9.2');
-define('WE_ZFVERSION','1.12.3');
-define('WE_SVNREV', '$Rev$');
-define('WE_VERSION_SUPP_VERSION','0');
-define('WE_VERSION_SUPP','nightly-build');
-define('WE_VERSION_BRANCH','main-develop');
-define('WE_VERSION_NAME','6.3.9.2 Nightly');
