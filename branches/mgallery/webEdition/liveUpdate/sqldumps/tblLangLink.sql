c15225324f0f5e8745fb57c6e501b1f45ab04557
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-11-20 20:16:48 +0000

    Fix tbllanglink

--- branches/mgallery/webEdition/liveUpdate/sqldumps/tblLangLink.sql
+++ branches/mgallery/webEdition/liveUpdate/sqldumps/tblLangLink.sql
@@ -6,12 +6,15 @@ CREATE TABLE ###TBLPREFIX###tblLangLink (
   IsObject tinyint(1) unsigned NOT NULL default '0',
   LDID int(11) unsigned NOT NULL default '0',
   Locale char(5) NOT NULL default '',
-  DocumentTable enum('tblFile','tblObjectFile','tblDocTypes') NOT NULL,
+  DocumentTable enum('tblFile','tblObjectFile','tblObjectFiles','tblDocTypes') NOT NULL,
   PRIMARY KEY (ID),
   UNIQUE KEY DID (DID,DLocale,IsObject,IsFolder,Locale,DocumentTable),
   UNIQUE KEY DLocale (DLocale,IsFolder,IsObject,LDID,Locale,DocumentTable),
   KEY LDID (LDID,DocumentTable,Locale)
 ) ENGINE=MyISAM;
+
+/* query separator */
+###UPDATEONLY### UPDATE ###TBLPREFIX###tblLangLink SET DocumentTable="tblObjectFiles" WHERE DocumentTable="tblObjectFile";
 
 /* query separator */
 ###ONKEYFAILED(DID,###TBLPREFIX###tblLangLink)ALTER IGNORE TABLE ###TBLPREFIX###tblLangLink ADD UNIQUE KEY DID (DID,DLocale,IsObject,IsFolder,Locale,DocumentTable);###
