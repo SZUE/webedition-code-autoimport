0aeb0d571d849a64558285080173c58def8b337b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-12-01 21:38:46 +0000

    FR #0010162

--- trunk/webEdition/we/include/we_tags/we_tag_ifVarEmpty.inc.php
+++ trunk/webEdition/we/include/we_tags/we_tag_ifVarEmpty.inc.php
@@ -22,59 +22,6 @@
  * @package none
  * @license    http://www.gnu.org/copyleft/gpl.html  GPL
  */
-function we_isVarNotEmpty($attribs){
-	$docAttr = weTag_getAttribute('doc', $attribs, '', we_base_request::STRING);
-	$match_orig = weTag_getAttribute('match', $attribs, '', we_base_request::RAW);
-	$match = we_tag_getPostName($match_orig); //#6367
-	$type = weTag_getAttribute('type', $attribs, '', we_base_request::STRING);
-
-	switch($docAttr){
-		case 'object' :
-		case 'document' :
-			$formname = weTag_getAttribute('formname', $attribs, 'we_global_form', we_base_request::STRING);
-			$doc = isset($GLOBALS['we_' . $docAttr][$formname]) ? $GLOBALS['we_' . $docAttr][$formname] : false;
-			break;
-		case 'top' :
-			$doc = isset($GLOBALS['WE_MAIN_DOC']) ? $GLOBALS['WE_MAIN_DOC'] : false;
-			break;
-		default :
-			$doc = isset($GLOBALS['we_doc']) ? $GLOBALS['we_doc'] : false;
-	}
-	if(!$doc){
-		return false;
-	}
-	if(weTag_getAttribute('property', $attribs, false, we_base_request::BOOL)){
-		return isset($doc->$match_orig) ? !empty($doc->$match_orig) : false;
-	}
-
-	$type = $type ? : $doc->getElement($match_orig, 'type');
-	switch($type){
-		case 'href' :
-			$attribs['name'] = $match;
-			$attribs['_name_orig'] = $match_orig;
-			$foo = $doc->getField($attribs, $type, true);
-			break;
-		case 'multiobject':
-			//FIXME: this makes no sense
-			$attribs['name'] = $match;
-			$attribs['_name_orig'] = $match_orig;
-			$data = unserialize($doc->getField($attribs, $type, true));
-			if(!is_array($data['objects'])){
-				$data['objects'] = array();
-			}
-			$temp = new we_object_listviewMultiobject($match);
-			return (!empty($temp->Record));
-		default :
-			$type = $doc->getElement($match, 'type');
-			$foo = $doc->getElement($match, $type === 'img' ? 'bdid' : 'dat');
-
-			if(!$foo){
-				$type = $doc->getElement($match_orig, 'type');
-				$foo = $doc->getElement($match_orig, $type === 'img' ? 'bdid' : 'dat');
-			}
-	}
-	return (!empty($foo));
-}
 
 function we_tag_ifVarEmpty($attribs){
 	if(($foo = attributFehltError($attribs, 'match', __FUNCTION__))){
