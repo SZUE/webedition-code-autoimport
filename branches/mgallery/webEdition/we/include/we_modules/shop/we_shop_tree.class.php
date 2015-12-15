d9744b822ee03e3618254aa46ba289bdeb60597d
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2015-12-15 18:36:47 +0000

    Fix #0008166

--- branches/mgallery/webEdition/we/include/we_modules/shop/we_shop_tree.class.php
+++ branches/mgallery/webEdition/we/include/we_modules/shop/we_shop_tree.class.php
@@ -42,22 +42,22 @@ function startTree(){
 
 	function getJSTreeCode(){
 		$ret = we_html_element::cssLink(CSS_DIR . 'tree.css') .
-			we_html_element::jsElement('
+				we_html_element::jsElement('
 var table="' . SHOP_TABLE . '";
 WE().consts.g_l.shop.tree={
 	treeYearClick:"' . g_l('modules_shop', '[treeYearClick]') . '",
 	treeYear:"' . g_l('modules_shop', '[treeYear]') . '"
 };
 ') .
-			we_html_element::jsScript(JS_DIR . 'tree.js', 'self.focus();') .
-			we_html_element::jsScript(JS_DIR . 'shop_tree.js');
+				we_html_element::jsScript(JS_DIR . 'tree.js', 'self.focus();') .
+				we_html_element::jsScript(JS_DIR . 'shop_tree.js');
 		$menu = '
 function loadData() {
 	treeData.clear();
 	treeData.add(node.prototype.rootEntry(0, "root", "root"));';
 
 
-		$this->db->query("SELECT IntOrderID,DateShipping,DateConfirmation,DateCustomA,DateCustomB,DateCustomC,DateCustomD,DateCustomE,DatePayment,DateCustomF,DateCustomG,DateCancellation,DateCustomH,DateCustomI,DatecustomJ,DateFinished, DATE_FORMAT(DateOrder,'" . g_l('date', '[format][mysqlDate]') . "') as orddate, DATE_FORMAT(DateOrder,'%c%Y') as mdate FROM " . SHOP_TABLE . ' GROUP BY IntOrderID ORDER BY IntID DESC');
+		$this->db->query("SELECT IntOrderID,DateShipping,DateConfirmation,DateCustomA,DateCustomB,DateCustomC,DateCustomD,DateCustomE,DatePayment,DateCustomF,DateCustomG,DateCancellation,DateCustomH,DateCustomI,DatecustomJ,DateFinished, DATE_FORMAT(DateOrder,'" . g_l('date', '[format][mysql]') . "') AS orddate, DATE_FORMAT(DateOrder,'%c%Y') AS mdate FROM " . SHOP_TABLE . ' GROUP BY IntOrderID ORDER BY IntID DESC');
 		while($this->db->next_record()){
 //added for #6786
 			$style = 'color:black;font:liberation_sansbold;';
@@ -74,9 +74,9 @@ function loadData() {
 				$style = 'color:black;';
 			}
 			$menu.= "  treeData.add({
-	id:'" . $this->db->f("IntOrderID") . "',
-	parentid:" . $this->db->f("mdate") . ",
-	text:'" . $this->db->f("IntOrderID") . ". " . g_l('modules_shop', '[bestellung]') . " " . $this->db->f("orddate") . "',
+	id:'" . $this->db->f('IntOrderID') . "',
+	parentid:" . $this->db->f('mdate') . ",
+	text:'" . $this->db->f('IntOrderID') . ', ' . ' ' . $this->db->f('orddate') . "',
 	typ:'shop',
 	checked:false,
 	contentType:'shop',
@@ -122,7 +122,7 @@ function loadData() {
 		}
 		$menu.='top.yearshop = ' . $year . ';
 			}';
-		return parent::getJSTreeCode().$ret . we_html_element::jsElement($menu);
+		return parent::getJSTreeCode() . $ret . we_html_element::jsElement($menu);
 	}
 
 }
