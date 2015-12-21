d6e84be71b8e35aeeb9119cc37775cdc26fa807c
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-12-21 11:20:16 +0000

    we_version for 6.4.4.1

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,8 +1,7 @@
 <?php
-	define('WE_VERSION', '6.4.4.0');
+	define('WE_VERSION', '6.4.4.1');
 	define('WE_ZFVERSION', '1.12.13'); // recommended version of the Zend Framework (bundled with webEdition)
-	define('WE_SVNREV', '10993');
+	define('WE_SVNREV', '$Rev$');
 	define('WE_VERSION_SUPP_VERSION', '0');
-	define('WE_VERSION_SUPP','release');
+	define('WE_VERSION_SUPP','');
 	define('WE_VERSION_BRANCH', 'trunk');
-	define("WE_VERSION_NAME","6.4.4");
