0bc77a5872be3bdf4d200c8ca4afca877db56515
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2015-12-21 17:10:16 +0000

    we_version for nightly

--- branches/mgallery/webEdition/we/include/we_version.php
+++ branches/mgallery/webEdition/we/include/we_version.php
@@ -1,7 +1,7 @@
 <?php
-define("WE_VERSION","6.4.9.6");
-define("WE_VERSION_SUPP","");
-define('WE_SVNREV', '11004');
+define("WE_VERSION","6.4.9.7");
+define("WE_VERSION_SUPP","nightly-build");
+define('WE_SVNREV', '$Rev$');
 define("WE_VERSION_SUPP_VERSION","0");
 define("WE_VERSION_BRANCH","mgallery");
-define("WE_VERSION_NAME","7.0 Beta");
+define("WE_VERSION_NAME","6.4.9.7 mGallery Nightly");
