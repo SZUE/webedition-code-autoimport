4c61c4a7ffbc21e0921d5e6fa0ccc1a1ee1f32f2
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-01-24 17:55:55 +0000

    latest changes

--- externals/additional/updateServer/htdocs/server/we/onlineInstallation/includes/includes.inc.php
+++ externals/additional/updateServer/htdocs/server/we/onlineInstallation/includes/includes.inc.php
@@ -12,6 +12,7 @@ require_once(LIVEUPDATE_SERVER_DIR . '/includes/classes/installationLog.class.ph
 require_once(LIVEUPDATE_SERVER_DIR . "/includes/classes/installerDownload.class.php");
 require_once(LIVEUPDATE_SERVER_DIR . '/includes/classes/languages.class.php');
 require_once(LIVEUPDATE_SERVER_DIR . '/includes/classes/license.class.php');
+require_once(LIVEUPDATE_SERVER_DIR . '/includes/classes/modules.class.php');
 require_once(LIVEUPDATE_SERVER_DIR . '/includes/classes/notification.class.php');
 require_once(LIVEUPDATE_SERVER_DIR . '/includes/classes/progressBar.class.php');
 require_once(LIVEUPDATE_SERVER_DIR . '/includes/classes/snippets.class.php');
