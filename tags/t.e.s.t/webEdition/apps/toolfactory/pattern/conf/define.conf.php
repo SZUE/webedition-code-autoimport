d804b7b479bd930d915bb5d23ecdb58fc2550477
we-org <we-org@5781a423-4729-0410-aacd-f88070b95415>
2016-01-28 11:23:43 +0000

    delete testtag

--- tags/t.e.s.t/webEdition/apps/toolfactory/pattern/conf/define.conf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if(isset($TABLECONSTANT) && isset($TABLENAME) && !empty($TABLECONSTANT) && !empty($TABLENAME)) {?>
-define("<?php echo $TABLECONSTANT;?>","<?php echo $TABLENAME;?>");
-
-<?php }?>
-
-define("<?php echo $ACTIVECONSTANT;?>",true);
