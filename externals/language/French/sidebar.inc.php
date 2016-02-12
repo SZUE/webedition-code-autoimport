3245bd903f40eab0e195674221d54ebed95f5bcd
we-org <we-org@5781a423-4729-0410-aacd-f88070b95415>
2016-02-12 19:00:17 +0000

    new lang files

--- externals/language/French/sidebar.inc.php
+++ externals/language/French/sidebar.inc.php
@@ -16,31 +16,26 @@ $l_sidebar=array(
 		),
 		'1'=>array(
 			'headline'=>'Manuals',
-			'icon'=>'documentation.gif',
 			'link'=>'http://documentation.webedition.org/wiki/de/start',
 			'text'=>'Ici vous trouvez l`informations de base pour l`utilisation et la structure de webEdition.',
 		),
 		'2'=>array(
 			'headline'=>'Resources d`aide supplementaire',
-			'icon'=>'help.gif',
 			'link'=>'javascript:top.we_cmd("help");',
 			'text'=>'Overview of farther instructions and references',
 		),
 		'3'=>array(
 			'headline'=>'How to proceed',
-			'icon'=>'firststepswizard.gif',
 			'link'=>'http://webedition.org/de/webedition-cms/dokumentation/tag-referenz/',
 			'text'=>'You can create your individual web site entirely from the scratch or access available elements and base layouts.',
 		),
 		'4'=>array(
 			'headline'=>'First-Steps-Wizard',
-			'icon'=>'firststepswizard.gif',
 			'link'=>'javascript:top.we_cmd(`openFirstStepsWizardMasterTemplate`);',
 			'text'=>'Use this wizard to install a ready-to-use base layouts. With "webEdition Online" you can install templates for special purposes at any time.',
 		),
 		'5'=>array(
 			'headline'=>'Demo web site',
-			'icon'=>'demopages.gif',
 			'link'=>'http://demo.en.webedition.info/',
 			'text'=>'These are entire web sites including example contents. You can import and freely edit these to fit your needs.',
 	),),
