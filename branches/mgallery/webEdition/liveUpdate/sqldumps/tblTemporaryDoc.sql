ae83bb042e0b0d4a2dd93ebd95c96b4ab8cc0d47
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-02-22 14:31:13 +0000

    Fix ,

--- branches/mgallery/webEdition/liveUpdate/sqldumps/tblTemporaryDoc.sql
+++ branches/mgallery/webEdition/liveUpdate/sqldumps/tblTemporaryDoc.sql
@@ -2,7 +2,7 @@ CREATE TABLE ###TBLPREFIX###tblTemporaryDoc (
   DocumentID bigint unsigned NOT NULL default '0',
   DocumentObject longtext NOT NULL,
   DocTable enum('tblFile','tblObjectFiles') NOT NULL,
-	saved TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	saved TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   Active tinyint unsigned NOT NULL default '0',
   PRIMARY KEY (`DocTable`,`DocumentID`,`Active`)
 ) ENGINE=MyISAM;
