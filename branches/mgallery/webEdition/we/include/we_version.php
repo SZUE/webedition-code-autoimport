839a61115de169f7c4e41329743f8c8ddbd35434
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2016-03-04 21:59:21 +0000

    reset we_version after build tests

--- branches/mgallery/webEdition/we/include/we_version.php
+++ branches/mgallery/webEdition/we/include/we_version.php
@@ -1,8 +1,7 @@
 <?php
-define("WE_VERSION","8.4.9.9");
+define("WE_VERSION","6.4.9.9");
 define("WE_VERSION_SUPP","nightly-build");
-define("WE_ZFVERSION","1.12.13"); // recommended version of the Zend Framework (bundled with webEdition)
-define("WE_SVNREV","11572");
+define('WE_SVNREV', '$Rev$');
 define("WE_VERSION_SUPP_VERSION","0");
 define("WE_VERSION_BRANCH","mgallery");
 define("WE_VERSION_NAME","6.4.9.9 mGallery Nightly");
