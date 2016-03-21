3dbe4f0a8bd9d2303b5e75c12983e349b5861671
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2016-03-21 23:50:01 +0000

    Fix #9389

--- branches/mgallery/webEdition/we/include/we_classes/listview/we_listview_document.class.php
+++ branches/mgallery/webEdition/we/include/we_classes/listview/we_listview_document.class.php
@@ -448,8 +448,8 @@ FROM ' . FILE_TABLE . ' WHERE ID=' . intval($id), $this->DB_WE, MYSQL_ASSOC)
 	}
 
 	function makeFieldCondition($name, $operation, $value){
-		return strstr($name, 'wedoc_') ? //Fix: #9389
-			'('. FILE_TABLE . '.' . ltrim($name, 'wedoc_') .' ' . $operation . ' ' . $value . ')' : 
+		return (strpos($name, 'WE_') == 0) ? //Fix: #9389
+			'('. FILE_TABLE . '.' . substr($name,3) .' ' . $operation . ' ' . $value . ')' : 
 			'(l.nHash=x\'' . md5($name) . '\' AND c.Dat ' . $operation . ' ' . $value . ')';
 	}
 
