d70ff83661cc1b43f1a3f3ac7fb48ffcc7e8fdc7
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-03-29 15:49:08 +0000

    Fix #0010452

--- branches/mgallery/webEdition/rpc/cmds/error/rpcTriggerJSErrorCmd.class.php
+++ branches/mgallery/webEdition/rpc/cmds/error/rpcTriggerJSErrorCmd.class.php
@@ -26,7 +26,7 @@ class rpcTriggerJSErrorCmd extends we_rpc_cmd{
 
 	function execute(){
 		if(isset($_REQUEST['we_cmd']) && function_exists('log_error_message')){
-			log_error_message(E_JS, $_REQUEST['we_cmd'], '', 0, true);
+			log_error_message(E_JS, print_r($_REQUEST['we_cmd'], true), '', 0, true);
 		}
 	}
 
