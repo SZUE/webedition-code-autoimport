f39dc875253edaad3978a9b5bf494e2e1d424b64
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2016-04-02 05:51:09 +0000

    Fix #10219

--- branches/mgallery/webEdition/we/include/we_modules/object/we_objectFile.class.php
+++ branches/mgallery/webEdition/we/include/we_modules/object/we_objectFile.class.php
@@ -3091,14 +3091,7 @@ class we_objectFile extends we_document{
 			return $path;
 		}
 		
-		if(in_workspace($pid, $ws1)){
-			$path = f('SELECT Path FROM ' . FILE_TABLE . ' WHERE Published>0 AND ContentType="' . we_base_ContentTypes::WEDOCUMENT . '" AND IsDynamic=1 AND Path LIKE "' . id_to_path(intval($pid), FILE_TABLE, $DB_WE) . '%" ORDER BY CHAR_LENGTH(Path) LIMIT 1', '', $DB_WE);
-			if($path){
-				return $path;
-			}
-		}
-		
-		if(in_workspace($pid, $ws2)){
+		if(in_workspace($pid, $ws1) || in_workspace($pid, $ws2)){
 			return f('SELECT Path FROM ' . FILE_TABLE . ' WHERE Published>0 AND ContentType="' . we_base_ContentTypes::WEDOCUMENT . '" AND IsDynamic=1 AND Path LIKE "' . id_to_path(intval($pid), FILE_TABLE, $DB_WE) . '%" ORDER BY CHAR_LENGTH(Path) LIMIT 1', '', $DB_WE);
 		}
 		return '';
