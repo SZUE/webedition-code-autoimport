8bdeedf88e6a768f5067885c4d5c087ed0a039a6
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2016-04-18 12:17:35 +0000

    tinymce: drag&drop from doc-tree to content

--- webEdition/js/wysiwyg/tinymce/plugins/weimage/editor_plugin.js
+++ webEdition/js/wysiwyg/tinymce/plugins/weimage/editor_plugin.js
@@ -1,7 +1,9 @@
 (function () {
 	tinymce.create('tinymce.plugins.WeimagePlugin', {
 		init: function (e, f) {
-			e.addCommand('mceWeimage', function () {
+			e.addCommand('mceWeimage', function (ui, dropID) {
+				dropID = dropID || 0;
+
 				e.isWeDataInitialized = false;
 				if (e.dom.getAttrib(e.selection.getNode(), 'class', '').indexOf('mceItem') != -1) {
 					return;
@@ -12,7 +14,7 @@
 				}
 
 				e.windowManager.open({
-					file: '/webEdition/dynamic/wysiwyg/imageDialog.php?we_dialog_args[editor]=tinyMce&we_dialog_args[src]=' + encodeURIComponent(wesrc) + '&we_dialog_args[cssclasses]=' + e.getParam('weClassNames_urlEncoded') + "&we_dialog_args[isFrontend]=" + e.getParam('weIsFrontend') + "&we_dialog_args[selectorStartID]=" + e.getParam('weImageStartID'),
+					file: '/webEdition/dynamic/wysiwyg/imageDialog.php?we_dialog_args[editor]=tinyMce&we_dialog_args[src]=' + encodeURIComponent(wesrc) + '&we_dialog_args[cssclasses]=' + e.getParam('weClassNames_urlEncoded') + "&we_dialog_args[isFrontend]=" + e.getParam('weIsFrontend') + "&we_dialog_args[selectorStartID]=" + e.getParam('weImageStartID') + "&we_dialog_args[fileID]=" + dropID + "&we_dialog_args[isPresetFromDnD]=" + (dropID ? 1 : 0),
 					popup_css: false,
 					width: 600 + parseInt(e.getLang('weimage.delta_width', 0)),
 					height: 610 + parseInt(e.getLang('weimage.delta_height', 0)),
