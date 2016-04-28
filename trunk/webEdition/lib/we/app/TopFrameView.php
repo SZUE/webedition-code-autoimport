d20241ba0d91ba0f2b1db17534f163a72f055839
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2016-04-28 13:14:33 +0000

    fix toolfactory pt.1

--- trunk/webEdition/lib/we/app/TopFrameView.php
+++ trunk/webEdition/lib/we/app/TopFrameView.php
@@ -35,7 +35,7 @@ class we_app_TopFrameView extends Zend_View{
 	 *
 	 * @var string
 	 */
-	const kEditorFrameset = 'self.resize.editor';
+	const kEditorFrameset = 'self.resize.right.editor';
 
 	/**
 	 * appName attribute
