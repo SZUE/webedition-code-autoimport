a6f1c9c01b0461667606cb3eaa80ccb46bb0c469
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-05-27 08:46:20 +0000

    #0010716

--- trunk/webEdition/liveUpdate/sqldumps/tblObjectFiles.sql
+++ trunk/webEdition/liveUpdate/sqldumps/tblObjectFiles.sql
@@ -35,7 +35,7 @@ CREATE TABLE ###TBLPREFIX###tblObjectFiles (
   Language varchar(5) default NULL,
   WebUserID bigint unsigned NOT NULL,
   PRIMARY KEY  (ID),
-  UNIQUE Path (Path),
+  UNIQUE KEY Path (Path),
   KEY WebUserID (WebUserID),
   KEY TableID (TableID),
   KEY Url (Url)
