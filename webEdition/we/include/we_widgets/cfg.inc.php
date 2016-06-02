4c570845e3eacc9183bc15eed5a69ca13019f449
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-06-02 14:26:17 +0000

    zu #0010731

--- webEdition/we/include/we_widgets/cfg.inc.php
+++ webEdition/we/include/we_widgets/cfg.inc.php
@@ -210,11 +210,11 @@ var oCfg={
 
 foreach($aPrefs as $type => $_prefs){
 	$jsPrefs .= "oCfg." . $type . "_props_={
-		width:" . $_prefs["width"] . ",
-		height:" . $_prefs["height"] . ",
+		width:" . intval($_prefs["width"]) . ",
+		height:" . intval($_prefs["height"]) . ",
 		res:" . $_prefs["res"] . ",
 		cls:'" . $_prefs["cls"] . "',
-		iDlgHeight:" . $_prefs["dlgHeight"] . "
+		iDlgHeight:" . intval($_prefs["dlgHeight"]) . "
 };";
 }
 
