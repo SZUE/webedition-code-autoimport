54c3fa580993de02d8ad252b182b6bd057c7c8a9
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-06-13 16:52:21 +0000

    some changes to textarea

--- webEdition/js/we_textarea.js
+++ webEdition/js/we_textarea.js
@@ -1,3 +1,5 @@
+/* global WE, top */
+
 /**
  *
  * webEdition CMS
@@ -27,24 +29,69 @@ weTextarea_count = 0;
 
 //FIXME: change/remove this!
 function we_textarea(name, value, cols, rows, width, height, autobr, autobrName, showAutobr, showRTF, language, classname, style, wrap, changehandler, xml, id, showSpell, origName) {
+	showSpell = false;
 	this.TAName = name;
 	this.name = "weTextarea" + (weTextarea_count++);
 	this.obj = this.name + "Object";
 	this.autobr = (autobr == "on") ? true : false;
-	this.nl2br = we_textarea_nl2br;
-	this.br2nl = we_textarea_br2nl;
-	this.appendText = we_textarea_appendText;
-	this.htmlspecialchars = we_textarea_htmlspecialchars;
-	this.ButtonNormal = we_textarea_ButtonNormal;
-	this.ButtonOverUp = we_textarea_ButtonOverUp;
-	this.ButtonOverDown = we_textarea_ButtonOverDown;
-	this.ButtonDown = we_textarea_ButtonDown;
+	this.autobrName = autobrName;
 	this.xml = xml;
 	this.id = id;
-	if (style.length) {
-		if (style.substring(style.length - 1, style.length) != ";") {
-			style += ";";
+
+	this.nl2br = function (i, xml) {
+		if (!xml) {
+			i = i.replace(/\r\n/g, "<br>");
+			i = i.replace(/\n/g, "<br>");
+			i = i.replace(/\r/g, "<br>");
+			return i.replace(/<br>/g, "<br>\n");
 		}
+		i = i.replace(/\r\n/g, "<br />");
+		i = i.replace(/\n/g, "<br />");
+		i = i.replace(/\r/g, "<br />");
+		return i.replace(/<br *\/>/g, "<br />\n");
+	};
+	this.br2nl = function (i) {
+		i = i.replace(/[\n\r]/g, "");
+		return i.replace(/<br *\/?>/gi, "\n");
+	};
+	this.appendText = function (text) {
+		this.form.elements[this.TAName].value += text;
+		this.form.elements["areatmp_" + this.TAName].value = (this.autobr ?
+						this.br2nl(this.form.elements[this.TAName].value) :
+						this.form.elements[this.TAName].value);
+	};
+	this.htmlspecialchars = function (i) {
+		return i.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(/'/g, "&#039;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	};
+	this.ButtonNormal = function (bt) {
+		bt.style.border = "0px groove";
+		bt.style.margin = "1px";
+	};
+	this.ButtonOverUp = function (bt) {
+		bt.style.margin = "0px";
+		bt.style.borderBottom = "#000000 solid 1px";
+		bt.style.borderLeft = "#CCCCCC solid 1px";
+		bt.style.borderRight = "#000000 solid 1px";
+		bt.style.borderTop = "#CCCCCC solid 1px";
+	};
+	this.ButtonOverDown = function (bt) {
+		bt.style.margin = "0px";
+		bt.style.borderBottom = "#CCCCCC solid 1px";
+		bt.style.borderLeft = "#000000 solid 1px";
+		bt.style.borderRight = "#CCCCCC solid 1px";
+		bt.style.borderTop = "#000000 solid 1px";
+	};
+	this.ButtonDown = function (bt) {
+		bt.style.margin = "0px";
+		bt.style.backgroundColor = "#dfdfdf";
+		bt.style.borderBottom = "#CCCCCC solid 1px";
+		bt.style.borderLeft = "#000000 solid 1px";
+		bt.style.borderRight = "#CCCCCC solid 1px";
+		bt.style.borderTop = "#000000 solid 1px";
+	};
+
+	if (style.length && style.substring(style.length - 1, style.length) != ";") {
+		style += ";";
 	}
 	if (width) {
 		style += "width:" + width + "px;";
@@ -55,70 +102,45 @@ function we_textarea(name, value, cols, rows, width, height, autobr, autobrName,
 	val = value ? value : "";
 	if (val) {
 		val = val.replace(/##\|n##/gi, "\n");
-		val = val.replace(/<##scr#ipt##/gi, "<script");
-		val = val.replace(/<\/##scr#ipt##/gi, "</script");
+		val = val.replace(/<##scr#ipt##/gi, "<script").replace(/<\/##scr#ipt##/gi, "</script");
 		val = val.replace(/##\|lt\;\?##/gi, "<?");
 	}
-	out = '<input type="hidden" name="' +
-					autobrName +
-					'" value="' +
-					(this.autobr ? 'on' : 'off') +
-					'"><table class="default" style="background-color: #F5F5F5;">';
-	if (showAutobr) {
-		out += '<tr><td><table class="default">' +
-						'<td><input type="checkbox" name="check' +
-						name +
-						'" id="check' +
-						name +
-						'" onClick="if(self.' + this.name + 'Object){' +
-						this.name +
-						'Object.translate(this);this.form.elements[\'' + autobrName + '\'].value=(this.checked ? \'on\' : \'off\');}"' +
-						(this.autobr ? ' checked' : '') +
-						'>&nbsp;</td><td style=" color:black;font-weight: bold; font-size: 10px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;cursor: pointer;" onClick="if(self.' + this.name + 'Object){var cb=document.getElementById(\'check' + name + '\');cb.checked=cb.checked ? false : true;' + this.name + 'Object.translate(cb);cb.form.elements[\'' + autobrName + '\'].value=(cb.checked ? \'on\' : \'off\');}">autobr</td>';
-	}
-
-	if (showAutobr && (showRTF || showSpell)) {
-		out += '<td style="color:black;font-weight: bold; font-size: 10px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif">&nbsp;</td><td><div unselectable="on" style="border-right: #999999 solid 1px; font-size: 0px; height:22px; width:2px;"></div></td><td style="color:black;font-weight: bold; font-size: 10px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif">&nbsp;</td>';
-	}
-
-	if (showSpell) {
-		out += '<td unselectable="on"><div unselectable="on">' +
-						'<img style="margin: 1px;width:23px;height:22px;" unselectable="on" src="/webEdition/images/wysiwyg/spellcheck.gif"' +
-						'onmouseover="if(self.' + this.name + 'Object){' + this.name + 'Object.ButtonOverUp(this);}"' +
-						'onmouseout="if(self.' + this.name + 'Object){' + this.name + 'Object.ButtonNormal(this);}"' +
-						'onmousedown="if(self.' + this.name + 'Object){' + this.name + 'Object.ButtonOverDown(this);}"' +
-						'onclick="new (WE().util.jsWindow)(window, \'' + WE().consts.dirs.WE_MODULES_DIR + 'spellchecker/weSpellchecker.php?editname=areatmp_' + encodeURI(name) + '\', "spellchechecker", -1, -1, 450, 500, true, true, true);"></div></td>';
-	}
-
-	if (showAutobr) {
-		out += '</table></td></tr>';
-	}
-
-	out += '<tr><td><textarea name="areatmp_' + name + '"' +
+	document.writeln(
+					'<input type="hidden" name="' + autobrName + '" value="' + (this.autobr ? 'on' : 'off') + '">' +
+					'<table class="default" style="background-color: #F5F5F5;">' +
+					(showAutobr ?
+									'<tr><td><table class="default"><tr>' +
+									'<td><input type="checkbox" name="check' + this.TAName + '" id="check' + this.TAName + '" onchange="' + this.name + 'Object.setAutoBr(this);"' + (this.autobr ? ' checked="checked"' : '') + '>&nbsp;</td>' +
+									'<td style="color:black;font-weight: bold; font-size: 10px; cursor: pointer;"><label for="check' + this.TAName + '">autobr</label></td>' :
+									"") +
+					(showAutobr && (showRTF || showSpell) ?
+									'<td style="color:black;font-weight: bold; font-size: 10px;">&nbsp;</td><td><div style="border-right: #999999 solid 1px; font-size: 0px; height:22px; width:2px;"></div></td><td>&nbsp;</td>' :
+									"") +
+					(showSpell ?
+									'<td><div>' +
+									'<img style="margin: 1px;width:23px;height:22px;" src="/webEdition/images/wysiwyg/spellcheck.gif"' +
+									'onmouseover="' + this.name + 'Object.ButtonOverUp(this);"' +
+									'onmouseout="' + this.name + 'Object.ButtonNormal(this);"' +
+									'onmousedown="' + this.name + 'Object.ButtonOverDown(this);"' +
+									'onclick="new (WE().util.jsWindow)(window, \'' + WE().consts.dirs.WE_MODULES_DIR + 'spellchecker/weSpellchecker.php?editname=areatmp_' + encodeURI(name) + '\', "spellchechecker", -1, -1, 450, 500, true, true, true);"></div></td>' :
+									"") +
+					(showAutobr ?
+									'</tr></table></td></tr>' :
+									"") +
+					'<tr><td><textarea name="areatmp_' + this.TAName + '"' +
 					'class="' + (classname ? classname + ' ' : '') + 'wetextarea wetextarea-' + origName + '"' +
 					(cols ? ' cols="' + cols + '"' : '') +
 					(wrap ? ' wrap="' + wrap + '"' : '') +
 					(rows ? ' rows="' + rows + '"' : '') +
 					(id ? ' id="' + id + '"' : '') +
-					(style ? ' style="' + style + '"' : '') +
-					' ' + changehandler + '="if (_EditorFrame && this.value != this.form.elements[\'' +
-					name + '\'].value){_EditorFrame.setEditorIsHot(true)};this.form.elements[\'' +
-					name +
-					'\'].value=(' +
-					this.name +
-					'Object.autobr ? ' +
-					this.name +
-					'Object.nl2br(this.value,' + (this.xml ? 'true' : 'false') + ') : this.value);" onblur="if(self.' + this.name + 'Object){this.form.elements[\'' +
-					name +
-					'\'].value=(' +
-					this.name + 'Object.autobr ? ' +
-					this.name +
-					'Object.nl2br(this.value,' + (this.xml ? 'true' : 'false') + ') : this.value);}">' +
+					(style ? ' style="' + style + '"' : '') + ' ' +
+					changehandler + '="self.' + this.name + 'Object.onchange(this);" onblur="self.' + this.name + 'Object.onblur(this);">' +
 					(val ? this.htmlspecialchars(this.autobr ? this.br2nl(val) : val) : '') +
 					'</textarea>' +
-					'<input type="hidden" name="' + name + '" value=""></td></tr></table>';
+					'<input type="hidden" name="' + this.TAName + '" value=""></td></tr></table>'
+					);
 	this.form = null;
-	document.writeln(out);
+
 	for (var i = 0; i < document.forms.length; i++) {
 		if (document.forms[i].elements[name]) {
 			this.form = document.forms[i];
@@ -129,85 +151,33 @@ function we_textarea(name, value, cols, rows, width, height, autobr, autobrName,
 		this.form.elements[name].value = val;
 	}
 
-	this.translate = we_textarea_translate;
+	this.translate = function (check) {
+		if (check.checked) {
+			this.autobr = true;
+			check.form.elements["areatmp_" + this.TAName].value = this.br2nl(check.form.elements[this.TAName].value);
+		} else {
+			this.autobr = false;
+			check.form.elements["areatmp_" + this.TAName].value = check.form.elements[this.TAName].value;
+		}
+	};
+
+	this.onblur = function (elem) {
+		elem.form.elements[this.TAName].value = (this.autobr ? this.nl2br(elem.value, this.xml) : elem.value);
+	};
+
+	this.onchange = function (elem) {
+		if (top._EditorFrame && elem.value !== elem.form.elements[this.TAName].value) {
+			top._EditorFrame.setEditorIsHot(true);
+		}
+		elem.form.elements[this.TAName].value = (this.autobr ? this.nl2br(elem.value, this.xml) : this.value);
+	};
+
+	this.setAutoBr = function (elem) {
+		this.translate(elem);
+		elem.form.elements[this.autobrName].value = (elem.checked ? 'on' : 'off');
+	};
 	//FIXME: do we need this as a global var?
 	window[this.obj] = this;
-}
-
-function we_textarea_translate(check) {
-	if (check.checked) {
-		this.autobr = true;
-		check.form.elements["areatmp_" + this.TAName].value = this.br2nl(check.form.elements[this.TAName].value);
-	} else {
-		this.autobr = false;
-		check.form.elements["areatmp_" + this.TAName].value = check.form.elements[this.TAName].value;
-	}
-}
-
-function we_textarea_appendText(text) {
-	this.form.elements[this.TAName].value += text;
-	if (this.autobr) {
-		this.form.elements["areatmp_" + this.TAName].value = this.br2nl(this.form.elements[this.TAName].value);
-	} else {
-		this.form.elements["areatmp_" + this.TAName].value = this.form.elements[this.TAName].value;
-	}
-}
-
-
-function we_textarea_nl2br(i, xml) {
-	if (!xml) {
-		i = i.replace(/\r\n/g, "<br>");
-		i = i.replace(/\n/g, "<br>");
-		i = i.replace(/\r/g, "<br>");
-		return i.replace(/<br>/g, "<br>\n");
-	} else {
-		i = i.replace(/\r\n/g, "<br />");
-		i = i.replace(/\n/g, "<br />");
-		i = i.replace(/\r/g, "<br />");
-		return i.replace(/<br *\/>/g, "<br />\n");
-	}
-}
-function we_textarea_br2nl(i) {
-	i = i.replace(/[\n\r]/g, "");
-	return i.replace(/<br *\/?>/gi, "\n");
-}
-
-function we_textarea_htmlspecialchars(i) {
-	i = i.replace(/&/g, "&amp;");
-	i = i.replace(/"/g, "&quot;");
-	i = i.replace(/'/g, "&#039;");
-	i = i.replace(/</g, "&lt;");
-	return i.replace(/>/g, "&gt;");
-}
-
-function we_textarea_ButtonNormal(bt) {
-	bt.style.border = "0px groove";
-	bt.style.margin = "1px";
-}
-
-function we_textarea_ButtonOverUp(bt) {
-	bt.style.margin = "0px";
-	bt.style.borderBottom = "#000000 solid 1px";
-	bt.style.borderLeft = "#CCCCCC solid 1px";
-	bt.style.borderRight = "#000000 solid 1px";
-	bt.style.borderTop = "#CCCCCC solid 1px";
-}
-
-function we_textarea_ButtonOverDown(bt) {
-	bt.style.margin = "0px";
-	bt.style.borderBottom = "#CCCCCC solid 1px";
-	bt.style.borderLeft = "#000000 solid 1px";
-	bt.style.borderRight = "#CCCCCC solid 1px";
-	bt.style.borderTop = "#000000 solid 1px";
-}
-
-function we_textarea_ButtonDown(bt) {
-	bt.style.margin = "0px";
-	bt.style.backgroundColor = "#dfdfdf";
-	bt.style.borderBottom = "#CCCCCC solid 1px";
-	bt.style.borderLeft = "#000000 solid 1px";
-	bt.style.borderRight = "#CCCCCC solid 1px";
-	bt.style.borderTop = "#000000 solid 1px";
 }
 
 //used for we:userInput
@@ -219,10 +189,10 @@ function open_wysiwyg_win() {
 			url += "&";
 	}
 
-	if (window.screen) {
+	/*if (window.screen) {
 		h = ((screen.height - 100) > screen.availHeight) ? screen.height - 100 : screen.availHeight;
 		w = screen.availWidth;
-	}
+	}*/
 	var wyw = Math.max(arguments[2], arguments[9]);
 	wyw = wyw ? wyw : 800;
 	var wyh = parseInt(arguments[3]) + parseInt(arguments[10]);
