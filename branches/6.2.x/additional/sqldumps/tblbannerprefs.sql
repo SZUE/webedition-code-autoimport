15d2efe561d59ea4b337ff39197a343b099c57af
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-06-22 13:00:15 +0000

    removed old, obsolete branch

--- branches/6.2.x/additional/sqldumps/tblbannerprefs.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE ###TBLPREFIX###tblbannerprefs (
-  pref_name varchar(255) NOT NULL default '',
-  pref_value varchar(255) NOT NULL default ''
-) ENGINE=MyISAM;
--- branches/6.2.x/webEdition/we/include/weTagWizard/we_tags/we_tag_order.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/webEdition/we/include/weTagWizard/classes/weTagData_textAttribute.class.php');
-
-$GLOBALS['weTagWizard']['weTagData']['needsEndtag'] = true;
-
-$GLOBALS['weTagWizard']['attribute']['id748_name'] = new weTagData_textAttribute('748', 'name', false, '');
-$GLOBALS['weTagWizard']['attribute']['id750_id'] = new weTagData_textAttribute('750', 'id', false, '');
-
-$GLOBALS['weTagWizard']['attribute']['id752_condition'] = new weTagData_textAttribute('752', 'condition', false, '');
