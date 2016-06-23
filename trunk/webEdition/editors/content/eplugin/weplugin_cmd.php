cb80785ea8bd60334c00264b017e376de79e3040
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-06-23 15:09:38 +0000

    prepare remove of we_init_doc.inc.php

--- trunk/webEdition/editors/content/eplugin/weplugin_cmd.php
+++ trunk/webEdition/editors/content/eplugin/weplugin_cmd.php
@@ -61,7 +61,7 @@ if (top.plugin.isLoaded && (typeof top.plugin.document.WePlugin.editSource == "f
 
 		break;
 	case "editFile":
-		$we_dt = isset($_SESSION['weS']['we_data'][$we_transaction]) ? $_SESSION['weS']['we_data'][$we_transaction] : "";
+		$we_dt = isset($_SESSION['weS']['we_data'][$we_transaction]) ? $_SESSION['weS']['we_data'][$we_transaction] : '';
 		include(WE_INCLUDES_PATH . 'we_editors/we_init_doc.inc.php');
 
 		$we_doc->we_initSessDat($we_dt);
