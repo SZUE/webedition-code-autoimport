a28e86862a3534d6f25a2efc6a5590257ff5fc57
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-07-11 07:45:28 +0000

    safety nachtrag

--- webEdition/apps/toolfactory/pattern/TOOLNAME.sql
+++ webEdition/apps/toolfactory/pattern/TOOLNAME.sql
@@ -3,7 +3,7 @@ CREATE TABLE `###TBLPREFIX###<?php echo $TABLENAMENOPREFIX?>` (
 `ParentID` BIGINT NOT NULL DEFAULT '0',
 `IsFolder` TINYINT NOT NULL DEFAULT '0',
 `ContentType` VARCHAR( 32 ) NOT NULL ,
-`Path` VARCHAR( 255 ) NOT NULL ,
+`Path` VARCHAR(255 ) NOT NULL ,
 `Text` VARCHAR( 255 ) NOT NULL,
  `Published` INT( 11 ) NOT NULL default '0',
  `Status` VARCHAR( 255 ) NOT NULL default ''
