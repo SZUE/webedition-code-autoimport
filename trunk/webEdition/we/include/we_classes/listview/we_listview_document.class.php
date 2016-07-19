21344b9cf22a370815359d7cce4b1ef136655725
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-07-19 09:52:08 +0000

    Fix listview document condition

--- trunk/webEdition/we/include/we_classes/listview/we_listview_document.class.php
+++ trunk/webEdition/we/include/we_classes/listview/we_listview_document.class.php
@@ -441,7 +441,7 @@ FROM ' . FILE_TABLE . ' WHERE ID=' . intval($id), $this->DB_WE, MYSQL_ASSOC)
 	private function makeFieldCondition($name, $operation, $value){
 		return (strpos($name, 'WE_') === 0) ? //Fix: #9389
 			'(' . FILE_TABLE . '.' . substr($name, 3) . ' ' . $operation . ' ' . $value . ')' :
-			'(l.nHash=x\'' . md5($name) . '\' AND c.Dat ' . $operation . ' ' . $value . ')';
+			'(l.nHash=x\'' . md5($name) . '\' AND IFNULL(c.Dat,c.BDID) ' . $operation . ' ' . $value . ')';
 	}
 
 	public function getCustomerRestrictionQuery($specificCustomersQuery, $classID, $mfilter, $listQuery){
