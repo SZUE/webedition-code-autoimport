61a9aa8ba7d5224d81047731387a04cfa01bc7e4
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-08-09 13:04:04 +0000

    #0010906

--- trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotWritten.inc.php
+++ trunk/webEdition/we/include/weTagWizard/we_tags/we_tag_ifNotWritten.inc.php
@@ -7,22 +7,18 @@ $this->Groups[] = 'if_tags';
 $this->Description = g_l('weTag', '[' . $tagName . '][description]', true);
 
 
-$error = new weTagData_selectAttribute('onerror', array(
-	new weTagDataOption('all'),
-	new weTagDataOption('nousername'),
-	new weTagDataOption('nopassword'),
-	new weTagDataOption('userexists'),
-	new weTagDataOption('passwordRule'),
-	), false, '');
-
-$this->TypeAttribute = new weTagData_typeAttribute('type', array(
-	new weTagDataOption('document', false, '', array(), array()),
-	new weTagDataOption('object', false, 'object', array(), array()),
-	new weTagDataOption('customer', false, 'customer', array($error), array($error)))
-	, false, '');
-
-
 $this->Attributes = array(
-	$error,
+	new weTagData_selectAttribute('type', array(
+		new weTagDataOption('document'),
+		new weTagDataOption('object'),
+		new weTagDataOption('customer'),
+		), false, ''),
+	new weTagData_selectAttribute('onerror', array(
+		new weTagDataOption('all'),
+		new weTagDataOption('nousername'),
+		new weTagDataOption('nopassword'),
+		new weTagDataOption('userexists'),
+		new weTagDataOption('passwordRule'),
+		), false, ''),
 	new weTagData_textAttribute('formname', false, '')
 );
