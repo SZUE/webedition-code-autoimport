4489174bb915901b623263e9618009fb5509d044
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-09-14 19:00:31 +0000

    eol

--- externals/additional/scripts/eol-style/eol.sh
+++ externals/additional/scripts/eol-style/eol.sh
@@ -4,9 +4,9 @@
 DIR=$1
 LOG=/tmp/log.txt
 
-find ${DIR} \( -path ${DIR}/webEdition/lib/Zend -o -path ${DIR}/webEdition/lib/phpMailer \) -prune -o -name \*.php -exec svn propset svn:keywords "Date Author Revision" {} \;> ${LOG}
-find ${DIR} -name \*.js -o -name \*.css -o -name \*.scss -exec svn propset svn:keywords "Date Author Revision" {} \;>> ${LOG}
-find ${DIR} -name \*.php -o -name \*.html -o -name \*.js -o -name \*.css -o -name \*.scss -o -name \*.java -o -name \*.xml -o -name \*.svg -exec svn propset svn:eol-style native {} \;>> ${LOG}
+find ${DIR} \( -path ${DIR}/webEdition/lib/phpMailer \) -prune -o -name \*.php -exec svn propset svn:keywords "Date Author Revision" {} \;> ${LOG}
+find ${DIR} \( -name *.js -o -name *.css -o -name *.scss \) -exec svn propset svn:keywords "Date Author Revision" {} \;>> ${LOG}
+find ${DIR} \( -name \*.php -o -name \*.html -o -name \*.js -o -name \*.css -o -name \*.scss -o -name \*.java -o -name \*.xml -o -name \*.svg \) -exec svn propset svn:eol-style native {} \;>> ${LOG}
 find ${DIR} -name .htaccess -exec svn propset svn:eol-style native {} \;>> ${LOG}
 find ${DIR} -name \*.sh -exec svn propset svn:eol-style LF {} \;>> ${LOG}
 find ${DIR} -name \*.sh -exec svn propset svn:executable ON {} \;>> ${LOG}
