82e066d881abd573b26bedb2685036351b9dc294
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-09-15 15:31:57 +0000

    less inline-js

--- branches/main-develop/webEdition/js/widgets/rss.js
+++ branches/main-develop/webEdition/js/widgets/rss.js
@@ -33,7 +33,6 @@ var _iInitRssCfgNumEntries = 0;
 var _sInitTbCfg = '';
 var _iInitTbTitlePers = 0;
 
-
 function isUrl(s) {
 	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 	return regexp.test(s);
