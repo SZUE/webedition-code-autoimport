50a9a26242fe37fe8710416f99548b9c67b602ce
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-09-16 11:14:54 +0000

    #0011017

--- branches/main-develop/webEdition/we/include/we_editors/we_editor_versions.inc.php
+++ branches/main-develop/webEdition/we/include/we_editors/we_editor_versions.inc.php
@@ -35,18 +35,18 @@ $foundItems = count($content);
 
 echo we_html_tools::getCalendarFiles() .
  $versionsView->getJS() .
- we_html_element::cssLink(CSS_DIR . 'we_versions.css', array('media' => 'screen')) .
- we_html_element::cssLink(CSS_DIR . 'we_versions_print.css', array('media' => 'print'));
+ we_html_element::cssLink(CSS_DIR . 'we_versions.css', ['media' => 'screen']) .
+ we_html_element::cssLink(CSS_DIR . 'we_versions_print.css', ['media' => 'print']);
 ?>
 </head>
 <body class="weEditorBody" onunload="doUnload()" onkeypress="javascript:if (event.keyCode == 13 || event.keyCode == 3)
 			search(true);" onload="init();" onresize="sizeScrollContent();">
 	<form name="we_form" action="" onsubmit="return false;" style="padding:0px;margin:0px;">
 		<?php
-		echo $versionsView->getHTMLforVersions(array(
-			array("html" => "<div id='searchTable'>" . $versionsView->getBodyTop() . '</div>'),
-			array("html" => "<div id='parametersTop'>" . $versionsView->getParameterTop($foundItems) . '</div>' . $versionsView->tblList($content, $versionsView->makeHeadLines()) . "<div id='parametersBottom'>" . $versionsView->getParameterBottom($foundItems) . "</div>")
-		)) .
+		echo $versionsView->getHTMLforVersions([
+				["html" => "<div id='searchTable'>" . $versionsView->getBodyTop() . '</div>'],
+				["html" => "<div id='parametersTop'>" . $versionsView->getParameterTop($foundItems) . '</div>' . $versionsView->tblList($content, $versionsView->makeHeadLines()) . "<div id='parametersBottom'>" . $versionsView->getParameterBottom($foundItems) . "</div>"]
+		]) .
 		we_html_element::htmlHidden("we_complete_request", 1);
 		?>
 	</form>
