f9e8ab4734e0d39e5d0ca2816f901525a8c93426
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-09-18 11:02:19 +0000

    #0003523

--- webEdition/liveUpdate/sqldumps/tblLock.sql
+++ webEdition/liveUpdate/sqldumps/tblLock.sql
@@ -9,6 +9,10 @@ CREATE TABLE ###TBLPREFIX###tblLock (
   sessionID char(64) NOT NULL default '',
   lockTime datetime NOT NULL,
   tbl enum('tblFile','tblObject','tblTemplates','tblObjectFiles','tblVFile') NOT NULL,
+	releaseRequestID int default NULL,
+	releaseRequestText text default NULL,
+	releaseRequestForce datetime NULL,
+	releaseRequestReply text default NULL,
   PRIMARY KEY (ID,tbl),
   KEY UserID (UserID,sessionID),
   KEY lockTime (lockTime)
