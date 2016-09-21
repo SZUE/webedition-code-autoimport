2afe991f861d429574eb7d06b5cd36fdb9d42539
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-09-21 12:53:54 +0000

    changed table def for tblObject

--- webEdition/liveUpdate/sqldumps/tblObject.sql
+++ webEdition/liveUpdate/sqldumps/tblObject.sql
@@ -4,6 +4,9 @@
 /* query separator */
 ###UPDATEDROPCOL(CacheLifeTime,###TBLPREFIX###tblObject)###
 /* query separator */
+###UPDATEDROPKEY(IsFolder,###TBLPREFIX###tblObject)###
+/* query separator */
+
 CREATE TABLE ###TBLPREFIX###tblObject (
   ID int unsigned NOT NULL auto_increment,
   ParentID int unsigned NOT NULL default '0',
@@ -17,29 +20,28 @@ CREATE TABLE ###TBLPREFIX###tblObject (
   CreatorID int unsigned NOT NULL default '0',
   ModifierID int unsigned NOT NULL default '0',
   RestrictOwners tinyint unsigned NOT NULL default '0',
-  Owners varchar(255) NOT NULL default '',
+  Owners text NOT NULL default '',
   OwnersReadOnly text NOT NULL,
   RestrictUsers tinyint unsigned NOT NULL default '0',
-  Users varchar(255) NOT NULL default '',
+  Users text NOT NULL default '',
   UsersReadOnly text NOT NULL,
   DefaultCategory text NOT NULL,
   DefaultParentID int unsigned NOT NULL default '0',
-  DefaultText varchar(255) NOT NULL default '',
+  DefaultText text NOT NULL default '',
   DefaultValues longtext NOT NULL,
-  DefaultDesc varchar(255) NOT NULL default '',
-  DefaultTitle varchar(255) NOT NULL default '',
-  DefaultKeywords varchar(255) NOT NULL default '',
-  DefaultUrl varchar(255) NOT NULL default '',
-  DefaultUrlfield0 varchar(255) NOT NULL DEFAULT '_',
-  DefaultUrlfield1 varchar(255) NOT NULL DEFAULT '_',
-  DefaultUrlfield2 varchar(255) NOT NULL DEFAULT '_',
-  DefaultUrlfield3 varchar(255) NOT NULL DEFAULT '_',
+  DefaultDesc tinytext NOT NULL default '',
+  DefaultTitle tinytext NOT NULL default '',
+  DefaultKeywords tinytext NOT NULL default '',
+  DefaultUrl tinytext NOT NULL default '',
+  DefaultUrlfield0 tinytext NOT NULL DEFAULT '_',
+  DefaultUrlfield1 tinytext NOT NULL DEFAULT '_',
+  DefaultUrlfield2 tinytext NOT NULL DEFAULT '_',
+  DefaultUrlfield3 tinytext NOT NULL DEFAULT '_',
   DefaultTriggerID int unsigned NOT NULL default '0',
   ClassName enum('we_object') NOT NULL default 'we_object',
   Workspaces varchar(1000) NOT NULL default '',
   DefaultWorkspaces varchar(1000) NOT NULL default '',
   Templates varchar(255) NOT NULL default '',
   PRIMARY KEY (ID),
-  UNIQUE KEY Path (Path),
-  KEY IsFolder (IsFolder)
+  UNIQUE KEY Path (Path)
 ) ENGINE=MyISAM;
