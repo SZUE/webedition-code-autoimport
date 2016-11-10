bbaadc4aee72a18aae345a6d59bb8cf82b4b8c8e
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2016-11-10 12:29:43 +0000

    small fix

--- trunk/webEdition/liveUpdate/templates/languages.inc.php
+++ trunk/webEdition/liveUpdate/templates/languages.inc.php
@@ -42,8 +42,7 @@ foreach($languages as $lng){
 	} else {
 		$lngBox = we_html_forms::checkbox($lng, false, 'deleteLanguages[]', $lng, true);
 	}
-	$languagesStr .= "
-	$lngBox";
+	$languagesStr .= '<div>' . $lngBox . '</div>';
 }
 
 $deletedLngs = $this->getData('deletedLngs');
