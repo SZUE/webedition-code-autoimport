ad0c330fb2c615a4eb64c53a3276020697684348
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2016-11-16 22:28:33 +0000

    missing file

---
+++ branches/main-develop/webEdition/js/we_tabs/weTabsHead.js
@@ -0,0 +1,27 @@
+/* global WE */
+
+/**
+ * webEdition CMS
+ *
+ * webEdition CMS
+ * $Rev: 12671 $
+ * $Author: mokraemer $
+ * $Date: 2016-08-29 20:35:39 +0200 (Mo, 29. Aug 2016) $
+ *
+ * This source is part of webEdition CMS. webEdition CMS is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the textfile
+ * webEdition/licenses/webEditionCMS/License.txt
+ *
+ * @category   webEdition
+ * @package    webEdition_base
+ * @license    http://www.gnu.org/copyleft/gpl.html  GPL
+ */
+
+var weTabs = new (WE().layout.we_tabs)(document, window);
