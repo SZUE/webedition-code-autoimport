b9d133ac4e00c6b1440e33b010780bf9af71c0b2
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2017-01-12 14:41:08 +0000

    Fix #11208

--- trunk/webEdition/we/include/we_modules/newsletter/we_newsletter_frames.class.php
+++ trunk/webEdition/we/include/we_modules/newsletter/we_newsletter_frames.class.php
@@ -686,7 +686,7 @@ top.content.treeData.add(top.content.node.prototype.rootEntry(\'' . $pid . '\',\
 	function getHTMLExtern($group){
 		$delallbut = we_html_button::create_button(we_html_button::DELETE_ALL, "javascript:we_cmd('del_all_files'," . $group . ")");
 		$wecmdenc4 = we_base_request::encCmd("opener.we_cmd('add_file',top.currentID,$group);");
-		$addbut = we_html_button::create_button(we_html_button::ADD, "javascript:we_cmd('browse_server','fileselect','" . we_base_ContentTypes::TEXT . "','/','" . $wecmdenc4 . "','',1);");
+		$addbut = we_html_button::create_button(we_html_button::ADD, "javascript:we_cmd('browse_server','fileselect','" . we_base_ContentTypes::TEXT . "','/','" . $wecmdenc4 . "','',0);");
 
 
 		$buttons = $delallbut . (permissionhandler::hasPerm('CAN_SELECT_EXTERNAL_FILES')) ? $addbut : '';
