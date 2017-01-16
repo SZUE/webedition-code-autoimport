7215c750c41f4672e0fbc5a7af71cccbc8d27a75
lukasimhof <lukasimhof@5781a423-4729-0410-aacd-f88070b95415>
2017-01-16 10:23:58 +0000

    resolve merge conflict

--- webEdition/we/include/we_editors/we_sfileselector_frameset.inc.php
+++ webEdition/we/include/we_editors/we_sfileselector_frameset.inc.php
@@ -143,7 +143,7 @@ function printFrameSet(){
 			], we_html_element::htmlDiv(['id' => 'fsheader'], printHeaderHTML(($cmd1 ? 1 : 0))) .
 			we_html_element::htmlIFrame('fsbody', 'about:blank', '', '', '', true) .
 			we_html_element::htmlDiv(['id' => 'fsfooter'], printFooterTable(($cmd1 ? 1 : 0), $filter, $currentName)) .
-			we_html_element::htmlIFrame('fscmd', WEBEDITION_DIR . 'we_cmd.php?we_cmd[0]=selectorBrowseCmd&ret=' . ($cmd1 ? 1 : 0) . '&filter=' . $filter . '&currentName=' . $currentName . '&selectOwn=' . $selectOwn, 'display:none;', '', '', false))
+			we_html_element::htmlIFrame('fscmd', WEBEDITION_DIR . 'we_cmd.php?we_cmd[0]=selectorBrowseCmd&ret=' . ($cmd1 ? 1 : 0) . '&filter=' . $filter . '&currentName=' . $currentName . '&selectInternal=' . $selectInternal, 'display:none;', '', '', false))
 	);
 }
 
