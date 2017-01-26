c95f27a23eea0ec07a1da5829aad6ac0e45de58c
we-org <we-org@5781a423-4729-0410-aacd-f88070b95415>
2017-01-26 14:00:08 +0000

    new lang files

---
+++ externals/language/Deutsch/apps.inc.php
@@ -0,0 +1,14 @@
+<?php
+/** Generated language file of webEdition CMS */
+$l_apps=array(
+	'toolfactory'=>array(
+		'name'=>'Anwendungsverwaltung',
+		
+		'perm'=>array(
+			'create'=>'Der Benutzer darf Anwendungen erstellen',
+			'delete'=>'Der Benutzer darf Anwendungen löschen',
+			'edit'=>'Der Benutzer darf Anwendungen bearbeiten',
+			'publish'=>'Der Benutzer darf Anwendungen aktivieren und deaktivieren',
+			'regenerateTOC'=>'Der Benutzer darf die Anwendungs-TOC neu generieren',
+			'use'=>'Der Benutzer darf die Anwendungsverwaltung nutzen',
+)));
---
+++ externals/language/Dutch/apps.inc.php
@@ -0,0 +1,14 @@
+<?php
+/** Generated language file of webEdition CMS */
+$l_apps=array(
+	'toolfactory'=>array(
+		'name'=>'Toolfactory',
+		
+		'perm'=>array(
+			'create'=>'The user is allowed to create new items in toolfactor',
+			'delete'=>'The user is allowed to delete items from toolfactory',
+			'edit'=>'The user is allowed to edit items toolfactory',
+			'publish'=>'The user is allowed to publish items toolfactory',
+			'regenerateTOC'=>'The user is allowed to regenerate the application toc.xml',
+			'use'=>'The user is allowed to use toolfactory',
+)));
