a1b1cee3bccd0060957abc75399da3efc6ff584e
we-org <we-org@5781a423-4729-0410-aacd-f88070b95415>
2017-02-06 12:54:57 +0000

    cli builder: we_version for new nightly

--- webEdition/we/include/we_version.php
+++ webEdition/we/include/we_version.php
@@ -1,8 +1,8 @@
 <?php
-define("WE_VERSION","7.0.2.3");
+define("WE_VERSION","7.0.3.1");
 define("WE_VERSION_SUPP","nightly");
 define("WE_ZFVERSION","1.12.13"); // recommended version of the Zend Framework (bundled with webEdition)
-define("WE_SVNREV","13258");
+define("WE_SVNREV","13315");
 define("WE_VERSION_SUPP_VERSION","0");
 define("WE_VERSION_BRANCH","trunk");
-define("WE_VERSION_NAME","webEdition 7.0.2.3 Nightly");
+define("WE_VERSION_NAME","webEdition 7.0.3.1 Nightly");
