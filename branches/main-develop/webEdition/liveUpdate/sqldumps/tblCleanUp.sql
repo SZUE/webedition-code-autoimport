7dd010e778705dec36a889d8607b4c1b16b6e2fb
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2017-03-08 22:05:56 +0000

    fix tbl def

--- branches/main-develop/webEdition/liveUpdate/sqldumps/tblCleanUp.sql
+++ branches/main-develop/webEdition/liveUpdate/sqldumps/tblCleanUp.sql
@@ -2,7 +2,7 @@
 /* query separator */
 
 CREATE TABLE ###TBLPREFIX###tblCleanUp (
-  Path varchar(1024) NOT NULL default '',
+  Path varchar(400) NOT NULL default '',
   `Date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY (Path),
   KEY `Date` (`Date`)
@@ -10,4 +10,3 @@ CREATE TABLE ###TBLPREFIX###tblCleanUp (
 
 /* query separator */
 ###UPDATEDROPKEY(Path,###TBLPREFIX###tblCleanUp)###
-
