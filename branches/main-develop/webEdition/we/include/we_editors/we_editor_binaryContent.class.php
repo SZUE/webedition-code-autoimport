765e4a2f627285419c4f0d547fffc93bac77410b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2017-03-14 21:58:43 +0000

    fix editor

--- branches/main-develop/webEdition/we/include/we_editors/we_editor_binaryContent.class.php
+++ branches/main-develop/webEdition/we/include/we_editors/we_editor_binaryContent.class.php
@@ -26,21 +26,21 @@ class we_editor_binaryContent extends we_editor_base{
 
 	public function show(){
 		return $this->getPage(we_html_multiIconBox::getHTML("weImgProp", [['icon' => "upload.gif",
-						"headline" => "",
-						"html" => $this->we_doc->formUpload(),
+					"headline" => "",
+					"html" => $this->we_doc->formUpload(),
+					'space' => we_html_multiIconBox::SPACE_MED2
+					],
+					['icon' => "attrib.gif",
+						"headline" => g_l('weClass', '[attribs]'),
+						"html" => $this->we_doc->formProperties(),
 						'space' => we_html_multiIconBox::SPACE_MED2
-							],
-							['icon' => "attrib.gif",
-								"headline" => g_l('weClass', '[attribs]'),
-								"html" => $this->we_doc->formProperties(),
-								'space' => we_html_multiIconBox::SPACE_MED2
-							],
-							['icon' => "meta.gif",
-								"headline" => g_l('weClass', '[metadata]'),
-								"html" => $this->we_doc->formMetaInfos() . $this->we_doc->formMetaData(),
-								'space' => we_html_multiIconBox::SPACE_MED2
-							]
-								], 20), we_html_element::jsScript(JS_DIR . 'multiIconBox.js'));
+					],
+					['icon' => "meta.gif",
+						"headline" => g_l('weClass', '[metadata]'),
+						"html" => $this->we_doc->formMetaInfos() . $this->we_doc->formMetaData(),
+						'space' => we_html_multiIconBox::SPACE_MED2
+					]
+					], 20), we_html_element::jsScript(JS_DIR . 'multiIconBox.js') . we_editor_script::get());
 	}
 
 }
