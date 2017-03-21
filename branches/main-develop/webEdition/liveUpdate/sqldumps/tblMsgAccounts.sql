5bd4391dba02e61ac2a488eba84b5bc3e22360ce
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2017-03-21 11:41:08 +0000

    drop tables

--- branches/main-develop/webEdition/liveUpdate/sqldumps/tblMsgAccounts.sql
+++ branches/main-develop/webEdition/liveUpdate/sqldumps/tblMsgAccounts.sql
@@ -1,13 +1,1 @@
-CREATE TABLE ###TBLPREFIX###tblMsgAccounts (
-  ID int unsigned NOT NULL auto_increment,
-  UserID int unsigned default NULL,
-  name varchar(255) NOT NULL default '',
-  msg_type int unsigned default NULL,
-  deletable tinyint unsigned NOT NULL default '0',
-  uri varchar(255) default NULL,
-  `user` varchar(255) default NULL,
-  pass varchar(255) default NULL,
-  update_interval smallint unsigned NOT NULL default '0',
-  ext varchar(255) default NULL,
-  PRIMARY KEY  (ID)
-) ENGINE=MyISAM;
+DROP TABLE IF EXISTS ###TBLPREFIX###tblMsgAccounts;
