0b89895362e46ebbc2da483beb513aee8fbc524b
mokraemer <mokraemer@5781a423-4729-0410-aacd-f88070b95415>
2017-04-05 16:29:45 +0000

    eol

--- webEdition/rpc/cmds/rpcRequestUnlockCmd.class.php
+++ webEdition/rpc/cmds/rpcRequestUnlockCmd.class.php
@@ -3,9 +3,9 @@
 /**
  * webEdition CMS
  *
- * $Rev: 13615 $
- * $Author: mokraemer $
- * $Date: 2017-03-20 18:04:19 +0100 (Mo, 20. Mär 2017) $
+ * $Rev$
+ * $Author$
+ * $Date$
  *
  * This source is part of webEdition CMS. webEdition CMS is
  * free software; you can redistribute it and/or modify
