fb02aa653b120dfdc4d3d62420ea7f2d2c630453
andreaswitt <andreaswitt@5781a423-4729-0410-aacd-f88070b95415>
2017-04-11 19:48:36 +0000

    Fix #11265 pt. 1

--- webEdition/we/include/we_tags/we_tag_url.inc.php
+++ webEdition/we/include/we_tags/we_tag_url.inc.php
@@ -48,9 +48,11 @@ function we_tag_url(array $attribs){
 				$objectID = ($id === 'self' || $id === 'top') ? $GLOBALS['we_obj']->ID : intval($id);
 				if(($getObject = getHash('SELECT Url,TriggerID FROM ' . OBJECT_FILES_TABLE . ' WHERE ID=' . intval($objectID)))){
 					$triggerDocPath = $triggerid ? id_to_path($triggerid) :
-                        (defined('WE_REDIRECTED_SEO') ? //webEdition object uses SEO-URL
-                            (we_objectFile::getNextDynDoc(($path = rtrim(substr(WE_REDIRECTED_SEO, 0, strripos(WE_REDIRECTED_SEO, $getObject['Url'])), '/') . DEFAULT_DYNAMIC_EXT), path_to_id(rtrim(substr(WE_REDIRECTED_SEO, 0, strripos(WE_REDIRECTED_SEO, $getObject['Url'])), '/')), $GLOBALS['we_obj']->Workspaces, $GLOBALS['we_obj']->ExtraWorkspacesSelected, $GLOBALS['DB_WE']) ?: id_to_path($getObject['TriggerID'])):
-                            parse_url(urldecode($_SERVER['REQUEST_URI']), PHP_URL_PATH)
+                        ($getObject['TriggerID'] ? id_to_path($getObject['TriggerID']) :
+                            (defined('WE_REDIRECTED_SEO') ? //webEdition object uses SEO-URL
+                                we_objectFile::getNextDynDoc(($path = rtrim(substr(WE_REDIRECTED_SEO, 0, strripos(WE_REDIRECTED_SEO, $getObject['Url'])), '/') . DEFAULT_DYNAMIC_EXT), path_to_id(rtrim(substr(WE_REDIRECTED_SEO, 0, strripos(WE_REDIRECTED_SEO, $getObject['Url'])), '/')), $GLOBALS['we_obj']->Workspaces, $GLOBALS['we_obj']->ExtraWorkspacesSelected, $GLOBALS['DB_WE']) :
+                                parse_url(urldecode($_SERVER['REQUEST_URI']), PHP_URL_PATH)
+                            )
                         );
 											
 					$path_parts = pathinfo($triggerDocPath);
